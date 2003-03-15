1be101cd00a81f5df190461a45c57fdf3f927757
mishoo <mishoo@users.sourceforge.net>
2003-03-15 12:48:11 +0000

    norwegian translation

---
+++ lang/calendar-no.js
@@ -0,0 +1,45 @@
+// ** I18N
+Calendar._DN = new Array
+("Søndag",
+ "Mandag",
+ "Tirsdag",
+ "Onsdag",
+ "Torsdag",
+ "Fredag",
+ "Lørdag",
+ "Søndag");
+Calendar._MN = new Array
+("Januar",
+ "Februar",
+ "Mars",
+ "April",
+ "Mai",
+ "Juni",
+ "Juli",
+ "August",
+ "September",
+ "Oktober",
+ "November",
+ "Desember");
+
+// tooltips
+Calendar._TT = {};
+Calendar._TT["TOGGLE"] = "Bytt første ukedag";
+Calendar._TT["PREV_YEAR"] = "Forrige året (trykk for meny)";
+Calendar._TT["PREV_MONTH"] = "Forrige månede (trykk for meny)";
+Calendar._TT["GO_TODAY"] = "Gå til dagens dato";
+Calendar._TT["NEXT_MONTH"] = "Neste månde (trykk for meny)";
+Calendar._TT["NEXT_YEAR"] = "Neste år (trykk for meny)";
+Calendar._TT["SEL_DATE"] = "Velg dag";
+Calendar._TT["DRAG_TO_MOVE"] = "Flytt vinduet";
+Calendar._TT["PART_TODAY"] = " (idag)";
+Calendar._TT["MON_FIRST"] = "Vis Mandag først";
+Calendar._TT["SUN_FIRST"] = "Vis Søndag først";
+Calendar._TT["CLOSE"] = "Lukk vinduet";
+Calendar._TT["TODAY"] = "I dag";
+
+// date formats
+Calendar._TT["DEF_DATE_FORMAT"] = "y-mm-dd";
+Calendar._TT["TT_DATE_FORMAT"] = "DD, d MM y";
+
+Calendar._TT["WK"] = "wk";
