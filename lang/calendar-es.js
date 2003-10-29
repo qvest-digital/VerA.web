ab93892e5dbbcaf883fb6bfbf7c9bda1b6dfa9f4
mishoo <mishoo@users.sourceforge.net>
2003-10-29 12:51:30 +0000

    updated translation, thanks to Jose Ma. Martinez Miralles

--- lang/calendar-es.js
+++ lang/calendar-es.js
@@ -24,7 +24,24 @@ Calendar._MN = new Array
 
 // tooltips
 Calendar._TT = {};
-Calendar._TT["TOGGLE"] = "Cambia el primer día de la semana";
+Calendar._TT["INFO"] = "Acerca del calendario";
+
+Calendar._TT["ABOUT"] =
+"Control DHTML de Fecha/Hora\n" +
+"(c) dynarch.com 2002-2003\n" + // don't translate this this ;-)
+"Puedes encontrar la última versión en: http://dynarch.com/mishoo/calendar.epl\n" +
+"Distribuido bajo GNU LGPL.  Más detalles en http://gnu.org/licenses/lgpl.html ." +
+"\n\n" +
+"Selección de fecha:\n" +
+"- Usa los botones \xab y \xbb para seleccionar el año\n" +
+"- Usa los botones " + String.fromCharCode(0x2039) + ", " + String.fromCharCode(0x203a) + " para seleccionar el mes\n" +
+"- Mantén el botón del ratón en cualquiera de los botones anteriores para una selección más rápida.";
+Calendar._TT["ABOUT_TIME"] = "\n\n" +
+"Selección de hora:\n" +
+"- Haz click en una de las partes de la hora para incrementarla\n" +
+"- o Mays-click para decrementarla\n" +
+"- o haz click y arrastra para una selección más rápida.";
+
 Calendar._TT["PREV_YEAR"] = "Año anterior (mantener para menu)";
 Calendar._TT["PREV_MONTH"] = "Mes anterior (mantener para menu)";
 Calendar._TT["GO_TODAY"] = "Ir a hoy";
@@ -37,6 +54,7 @@ Calendar._TT["MON_FIRST"] = "Mostrar lunes primero";
 Calendar._TT["SUN_FIRST"] = "Mostrar domingo primero";
 Calendar._TT["CLOSE"] = "Cerrar";
 Calendar._TT["TODAY"] = "Hoy";
+Calendar._TT["TIME_PART"] = "(Mays-)Click o arrastra para cambiar la hora";
 
 // date formats
 Calendar._TT["DEF_DATE_FORMAT"] = "dd-mm-yy";
