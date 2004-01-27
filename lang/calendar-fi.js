795967cfbdba3ce8e64b84909a17100a52b19f68
mishoo <mishoo@users.sourceforge.net>
2004-01-27 15:25:20 +0000

    updated

--- lang/calendar-fi.js
+++ lang/calendar-fi.js
@@ -1,13 +1,9 @@
-// ** I18N
+﻿// ** I18N
 
-// Calendar FI language
-// Author: Antti Tuppurainen, <antti.tuppurainen@systemprofes.fi>
+// Calendar FI language (Finnish, Suomi)
+// Author: Jarno Käyhkö, <gambler@phnet.fi>
 // Encoding: UTF-8
 // Distributed under the same terms as the calendar itself.
-
-// For translators: please use UTF-8 if possible.  We strongly believe that
-// Unicode is the answer to a real internationalized world.  Also please
-// include your contact information in the header, as can be seen above.
 
 // full day names
 Calendar._DN = new Array
@@ -19,18 +15,6 @@ Calendar._DN = new Array
  "Perjantai",
  "Lauantai",
  "Sunnuntai");
-
-// Please note that the following array of short day names (and the same goes
-// for short month names, _SMN) isn't absolutely necessary.  We give it here
-// for exemplification on how one can customize the short day names, but if
-// they are simply the first N letters of the full name you can simply say:
-//
-//   Calendar._SDN_len = N; // short day name length
-//   Calendar._SMN_len = N; // short month name length
-//
-// If N = 3 then this is not needed either since we assume a value of 3 if not
-// present, to be compatible with translation files that were written before
-// this feature.
 
 // short day names
 Calendar._SDN = new Array
@@ -50,8 +34,8 @@ Calendar._MN = new Array
  "Maaliskuu",
  "Huhtikuu",
  "Toukokuu",
- "Keso?=uu",
- "HeinC$uu",
+ "Kesäkuu",
+ "Heinäkuu",
  "Elokuu",
  "Syyskuu",
  "Lokakuu",
@@ -60,55 +44,55 @@ Calendar._MN = new Array
 
 // short month names
 Calendar._SMN = new Array
-("Tammi",
- "Helmi",
- "Maalis",
- "Huhti",
- "Touko",
- "KesC$",
- "HeinC$",
+("Tam",
+ "Hel",
+ "Maa",
+ "Huh",
+ "Tou",
+ "Kes",
+ "Hei",
  "Elo",
- "Syys",
- "Loka",
- "Marras",
- "Joulu");
+ "Syy",
+ "Lok",
+ "Mar",
+ "Jou");
 
 // tooltips
 Calendar._TT = {};
-Calendar._TT["INFO"] = "Tietoa kalenterista";
+Calendar._TT["INFO"] = "Tietoja kalenterista";
 
 Calendar._TT["ABOUT"] =
 "DHTML Date/Time Selector\n" +
 "(c) dynarch.com 2002-2003\n" + // don't translate this this ;-)
-"Viimeisin versio kalenterista: http://dynarch.com/mishoo/calendar.epl\n" +
-"Julkaistu GNU LGPL -lisenssillC$ Ehdot: http://gnu.org/licenses/lgpl.html." +
+"Uusin versio osoitteessa: http://dynarch.com/mishoo/calendar.epl\n" +
+"Julkaistu GNU LGPL lisenssin alaisuudessa. Lisätietoja osoitteessa http://gnu.org/licenses/lgpl.html" +
 "\n\n" +
-"PC$ivC$mC$C$rC$n valinta:\n" +
-"- KC$ytC$ \xab, \xbb -linkkejC$ vuoden valinnassa\n" +
-"- KC$ytC$ " + String.fromCharCode(0x2039) + ", " + String.fromCharCode(0x203a) + " -linkkejC$ kuukauden valinnassa\n" +
-"- PidC$ hiiren nappi alhaalla nopeuttaaksesi valintaa.";
+"Päivämäärä valinta:\n" +
+"- Käytä \xab, \xbb painikkeita valitaksesi vuosi\n" +
+"- Käytä " + String.fromCharCode(0x2039) + ", " + String.fromCharCode(0x203a) + " painikkeita valitaksesi kuukausi\n" +
+"- Pitämällä hiiren painiketta minkä tahansa yllä olevan painikkeen kohdalla, saat näkyviin valikon nopeampaan siirtymiseen.";
 Calendar._TT["ABOUT_TIME"] = "\n\n" +
-"Kellonajan valinta:\n" +
-"- Klikkaa mitC$ tahansa osaa kellosta suurentaaksesi lukua\n" +
-"- tai Shifti+klikkaus vC$hentC$C$ksesi sitC$" +
-"- tai klikkaa ja vedC$ nopeuttaksesi valintaa.";
+"Ajan valinta:\n" +
+"- Klikkaa kellonajan numeroita lisätäksesi aikaa\n" +
+"- tai pitämällä Shift-näppäintä pohjassa saat aikaa taaksepäin\n" +
+"- tai klikkaa ja pidä hiiren painike pohjassa sekä liikuta hiirtä muuttaaksesi aikaa nopeasti eteen- ja taaksepäin.";
 
-Calendar._TT["PREV_YEAR"] = "Edellinen vuosi (valikko)";
-Calendar._TT["PREV_MONTH"] = "Edellinen kuukausi (valikko)";
-Calendar._TT["GO_TODAY"] = "Siirry pC$ivC$C$n";
-Calendar._TT["NEXT_MONTH"] = "Seuraava kuukausi (valikko)";
-Calendar._TT["NEXT_YEAR"] = "Seuraava vuosi (valikko)";
-Calendar._TT["SEL_DATE"] = "Valitse pC$ivC$mC$C$rC$";
-Calendar._TT["DRAG_TO_MOVE"] = "VedC$ siirtC$C$ksesi";
-Calendar._TT["PART_TODAY"] = " (TC$mC$ pC$ivC$)";
-Calendar._TT["MON_FIRST"] = "NC$ytC$ maanantai ensimmC$isenC$";
-Calendar._TT["SUN_FIRST"] = "NC$ytC$ sunnuntai ensimmC$isenC$";
+Calendar._TT["PREV_YEAR"] = "Edell. vuosi (paina hetki, näet valikon)";
+Calendar._TT["PREV_MONTH"] = "Edell. kuukausi (paina hetki, näet valikon)";
+Calendar._TT["GO_TODAY"] = "Siirry tähän päivään";
+Calendar._TT["NEXT_MONTH"] = "Seur. kuukausi (paina hetki, näet valikon)";
+Calendar._TT["NEXT_YEAR"] = "Seur. vuosi (paina hetki, näet valikon)";
+Calendar._TT["SEL_DATE"] = "Valitse päivämäärä";
+Calendar._TT["DRAG_TO_MOVE"] = "Siirrä kalenterin paikkaa";
+Calendar._TT["PART_TODAY"] = " (tänään)";
+Calendar._TT["MON_FIRST"] = "Näytä maanantai ensimmäisenä";
+Calendar._TT["SUN_FIRST"] = "Näytä sunnuntai ensimmäisenä";
 Calendar._TT["CLOSE"] = "Sulje";
-Calendar._TT["TODAY"] = "TC$nC$C$n";
-Calendar._TT["TIME_PART"] = "(Shifti-)Klikkaus tai hiiren vetC$minen vaihtaa arvoa";
+Calendar._TT["TODAY"] = "Tänään";
+Calendar._TT["TIME_PART"] = "(Shift-) Klikkaa tai liikuta muuttaaksesi aikaa";
 
 // date formats
 Calendar._TT["DEF_DATE_FORMAT"] = "%d.%m.%Y";
-Calendar._TT["TT_DATE_FORMAT"] = "%a, %b %e";
+Calendar._TT["TT_DATE_FORMAT"] = "%d.%m.%Y";
 
-Calendar._TT["WK"] = "vko";
+Calendar._TT["WK"] = "Vko";
