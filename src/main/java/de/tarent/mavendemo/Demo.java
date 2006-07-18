da982ee4f0f90d3a46c99d8adacf5e315fd007c3
hendrik <hendrik@tarent.org>
2006-07-18 10:02:01 +0000

    Kopie des maven-Beispiels als Basis

---
+++ src/main/java/de/tarent/mavendemo/Demo.java
@@ -0,0 +1,34 @@
+package de.tarent.mavendemo;
+/*
+ * Copyright (c) tarent GmbH
+ * Bahnhofstrasse 13 . 53123 Bonn
+ * www.tarent.de . info@tarent.de
+ *
+ * Created on 06.02.2006
+ */
+
+/**
+ * Demo JavaDoc comment.
+ */
+public class Demo
+{
+
+    /**
+     * Demo is a demonstration method. 
+     */
+    public Demo()
+    {
+        super();
+    }
+
+    /**
+     * This is the main demo method.
+     * 
+     * @param args
+     */
+    public static void main(String[] args)
+    {
+        System.out.println("Demo!");
+    }
+
+}
