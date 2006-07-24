60b03b46572c8a70fa9d88c9e71f6d88c3dc441e
jens <jens@evolvis.org>
2006-07-24 14:58:43 +0000

    lizenz hinzugefŸgt

--- src/main/java/de/tarent/octopus/rpctunnel/OctopusRPCTunnel.java
+++ src/main/java/de/tarent/octopus/rpctunnel/OctopusRPCTunnel.java
@@ -1,9 +1,28 @@
-/*
- * Copyright (c) tarent GmbH
- * Bahnhofstrasse 13 . 53123 Bonn
- * www.tarent.de . info@tarent.de
+/* $Id$
  *
- * Created on 19.07.2006
+ * tarent-contact, Plattform-Independent Webservice-Based Contactmanagement
+ * Copyright (C) 2002 tarent GmbH
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ *
+ * tarent GmbH., hereby disclaims all copyright
+ * interest in the program 'tarent-octopus'
+ * (which makes passes at compilers) written
+ * by Hendrik Helwich. 
+ * signature of Elmar Geese, 1 June 2002
+ * Elmar Geese, CEO tarent GmbH
  */
 package de.tarent.octopus.rpctunnel;
 
@@ -21,12 +40,10 @@ import de.tarent.octopus.request.Octopus;
  *
  */
 public class OctopusRPCTunnel 
-{
+{	
     private TcCommonConfig octopusConfig = null;
     private Octopus octopus = null;
 	private OctopusRPCListener listener = null;
-	
-    public static final String OCTOPUS_ROLE = "octopus";
 	
 	private static OctopusRPCTunnel octTunnel = null;
 	
@@ -50,7 +67,7 @@ public class OctopusRPCTunnel
 				Class tunnelc = Class.forName("de.tarent.octopus.rpctunnel.RPCTunnel");
 				Class listc = Class.forName("de.tarent.octopus.rpctunnel.RPCListener");
 				Method reg = tunnelc.getMethod("registerListener", new Class[] {listc, String.class});
-				reg.invoke(this, new Object[] {listener, OCTOPUS_ROLE});
+				reg.invoke(this, new Object[] {listener, DefaultRoles.ROLE_OCTOPUS});
 			} catch (Exception e) {
 				throw new RPCTunnelUnavailableException();
 			}
