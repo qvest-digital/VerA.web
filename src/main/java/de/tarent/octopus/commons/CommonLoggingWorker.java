83f09c8831cd2370af6ccbb38c47dfb0595bc896
christoph <christoph@tarent.org>
2006-08-31 15:04:12 +0000

    add: inital version of a log manager who extend logging informations. try to parse logging.properties on same way as java.util.logging.

--- src/main/java/de/tarent/octopus/commons/CommonLoggingWorker.java
+++ src/main/java/de/tarent/octopus/commons/CommonLoggingWorker.java
@@ -2,19 +2,16 @@ package de.tarent.octopus.commons;
 
 import java.io.File;
 import java.io.FileInputStream;
-import java.lang.reflect.Method;
-import java.util.HashMap;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.Map;
-import java.util.logging.ConsoleHandler;
-import java.util.logging.FileHandler;
-import java.util.logging.Handler;
+import java.util.Properties;
 import java.util.logging.Level;
-import java.util.logging.LogManager;
 import java.util.logging.Logger;
-import java.util.logging.SocketHandler;
 
 import javax.jws.WebMethod;
 
+import de.tarent.commons.logging.LogManager;
 import de.tarent.commons.messages.Message;
 import de.tarent.commons.messages.MessageHelper;
 import de.tarent.octopus.content.annotation.Name;
@@ -23,8 +20,7 @@ import de.tarent.octopus.server.OctopusContext;
 
 /**
  * This octopus worker will be load the java.util.logging configuration
- * from a file ({@link #readConfiguration(Object, Object)} or from the
- * octopus module config ({@link #setAdditionalConfiguration(Map)}).
+ * from a file with the tarent-commons {@link LogManager}.
  * 
  * @see LogManager for more information.
  * 
@@ -41,6 +37,10 @@ public class CommonLoggingWorker {
 	public static Message FILE_NOT_FOUND;
 	/** Will be syserr'ed if an exception is thrown. (First parameter will be exception message.) */
 	public static Message UNEXPECTED_EXCEPTION;
+
+	static {
+		MessageHelper.init(CommonLoggingWorker.class.getName(), "de.tarent.commons.messages.LogManager");
+	}
 
 	/**
 	 * This octopus action re-configure the {@link LogManager} with the given
@@ -62,45 +62,42 @@ public class CommonLoggingWorker {
 			@Name("CONFIG:logging.property.file") @Optional(true) String configFilename) {
 		
 		try {
-			logInternal("Run appendLoggingProperties: Wlll reload java.util.logging configuration.");
-			logInternal("    contentFilename: " + contentFilename);
-			logInternal("     configFilename: " + configFilename);
-			
 			File modulePath = octopusContext.moduleRootPath();
 			
 			if (contentFilename != null) {
-				File file = new File(contentFilename);
-				if (file.exists()) {
-					logInternal(READ_CONFIGURATION_FILE.getMessage(file.getAbsoluteFile()));
-					LogManager.getLogManager().readConfiguration(new FileInputStream(file.getAbsoluteFile()));
-				} else {
-					file = new File(modulePath, contentFilename);
-					if (file.exists()) {
-						logInternal(READ_CONFIGURATION_FILE.getMessage(file.getAbsoluteFile()));
-						LogManager.getLogManager().readConfiguration(new FileInputStream(file.getAbsoluteFile()));
-					} else {
-						logInternal(FILE_NOT_FOUND.getMessage(file.getAbsoluteFile()));
-					}
+				if (!appendLoggingProperties(new File(modulePath, contentFilename))) {
+					appendLoggingProperties(new File(contentFilename));
 				}
 			} else if (configFilename != null) {
-				File file = new File(configFilename);
-				if (file.exists()) {
-					logInternal(READ_CONFIGURATION_FILE.getMessage(file.getAbsoluteFile()));
-					LogManager.getLogManager().readConfiguration(new FileInputStream(file.getAbsoluteFile()));
-				} else {
-					file = new File(modulePath, configFilename);
-					if (file.exists()) {
-						logInternal(READ_CONFIGURATION_FILE.getMessage(file.getAbsoluteFile()));
-						LogManager.getLogManager().readConfiguration(new FileInputStream(file.getAbsoluteFile()));
-					} else {
-						logInternal(FILE_NOT_FOUND.getMessage(file.getAbsoluteFile()));
-					}
+				if (!appendLoggingProperties(new File(modulePath, configFilename))) {
+					appendLoggingProperties(new File(configFilename));
 				}
 			} else {
-				logInternal(NO_PARAMETER_AVAILABLE.getMessage());
+				logger.log(Level.WARNING, NO_PARAMETER_AVAILABLE.getMessage());
 			}
 		} catch (Exception e) {
-			logInternal(UNEXPECTED_EXCEPTION.getMessage(e.getLocalizedMessage()), e);
+			logger.log(Level.SEVERE, UNEXPECTED_EXCEPTION.getMessage(e.getLocalizedMessage()), e);
+		}
+	}
+
+	protected boolean appendLoggingProperties(File file) throws IOException {
+		if (file.exists()) {
+			if (logger.isLoggable(Level.INFO))
+				logger.log(Level.INFO, READ_CONFIGURATION_FILE.getMessage(file.getAbsoluteFile()));
+			
+			InputStream is = new FileInputStream(file.getAbsoluteFile());
+			
+			// Do not call here java.util.logging.LogManager!
+			// This will reset all other logging configurations and that
+			// is really evil and can be have side effects to other
+			// software producer that are running in the same vm.
+			LogManager.getInstance().readConfiguration(is);
+			
+			return true;
+		} else {
+			if (logger.isLoggable(Level.INFO))
+				logger.log(Level.INFO, FILE_NOT_FOUND.getMessage(file.getAbsoluteFile()));
+			return false;
 		}
 	}
 
@@ -109,142 +106,29 @@ public class CommonLoggingWorker {
 			OctopusContext octopusContext,
 			@Name("CONFIG:logging.handler") @Optional(true) Map<String, Map<String, String>> configHandler,
 			@Name("CONFIG:logging.logger") @Optional(true) Map<String, Map<String, String>> configLogger) {
-		logInternal("Run appendLoggingHandler: Wlll append java.util.logging configuration.");
-		logInternal("    configHandler: " + configHandler);
-		logInternal("    configLogger : " + configLogger);
 		
-		Map<String, Handler> handlerPool = new HashMap<String, Handler>();
+		LogManager logManager = LogManager.getInstance();
 		
 		if (configHandler != null) {
 			for (Map.Entry<String, Map<String, String>> entry : configHandler.entrySet()) {
-				handlerPool.put(entry.getKey(), getHandler(octopusContext, entry.getValue()));
+				String handlername = entry.getKey();
+				Properties properties = new Properties();
+				properties.putAll(entry.getValue());
+				
+				logManager.setHandler(handlername, properties, null);
 			}
 		}
 		
 		if (configLogger != null) {
 			for (Map.Entry<String, Map<String, String>> entry : configLogger.entrySet()) {
-				setLogger(entry.getKey(), entry.getValue(), handlerPool);
-			}
-		}
-	}
-
-	protected Handler getHandler(OctopusContext octopusContext, Map<String, String> configuration) {
-		try {
-			String classname = configuration.get("class");
-			String level = configuration.get("level");
-			
-			Handler handler = null;
-			
-			if (classname.equals("java.util.logging.ConsoleHandler")) {
-				handler = new ConsoleHandler();
-			} else if (classname.equals("java.util.logging.FileHandler")) {
-				String pattern = configuration.get("pattern");
-				int limit = getParameter(configuration, "limit", 0);
-				int count = getParameter(configuration, "limit", 1);
-				boolean a = getParameter(configuration, "limit", false);
-				handler = new FileHandler(pattern, limit, count, a);
-			} else if (classname.equals("java.util.logging.SocketHandler")) {
-				String host = getParameter(configuration, "host", "127.0.0.1");
-				int port = getParameter(configuration, "port", 0);
-				handler =  new SocketHandler(host, port);
-			} else {
-				Class clazz = Class.forName(classname);
-				Method setter = null;
-				handler = (Handler)clazz.newInstance();
+				String loggername = entry.getKey();
 				
-				for (Map.Entry<String, String> entry : configuration.entrySet()) {
-					String key = entry.getKey();
-					String value = octopusContext.moduleConfig().substituteVars(entry.getValue());
-					
-					if (!(key.equals("class") || key.equals("level"))) {
-						setter = clazz.getMethod("set" +
-								key.substring(0, 1).toUpperCase() +
-								key.substring(1),
-								new Class[] { String.class });
-						if (setter != null) {
-							setter.invoke(handler, new Object[] { value });
-						} else {
-							logInternal("No setter for " + key + " found.");
-						}
-					}
-				}
-			}
-			
-			if (level != null && level.length() != 0)
-				handler.setLevel(Level.parse(level));
-			
-			return handler;
-			
-		} catch (Exception e) {
-			logInternal(UNEXPECTED_EXCEPTION.getMessage(e.getLocalizedMessage()), e);
-			return null;
-		}
-	}
-
-	protected void setLogger(String name, Map<String, String> configuration, Map<String, Handler> handlerPool) {
-		Logger logger = Logger.getLogger(name);
-		
-		String level = configuration.get("level");
-		String handlers = configuration.get("handler");
-		
-		if (level != null && level.length() != 0)
-			logger.setLevel(Level.parse(level));
-		
-		if (handlers != null) {
-			for (String h : handlers.split("[,;]")) {
-				Handler handler = handlerPool.get(h.trim());
-				if (handler != null) {
-					logger.addHandler(handler);
-				}
+				Properties properties = new Properties();
+				properties.setProperty(loggername + ".level", entry.getValue().get("level"));
+				properties.setProperty(loggername + ".handlers", entry.getValue().get("handlers"));
+				
+				logManager.readConfiguration(properties);
 			}
 		}
-	}
-
-	protected String getParameter(Map<String, String> configuration, String key, String def) {
-		String result = configuration.get(key);
-		if (result != null && result.trim().length() != 0)
-			return result;
-		else
-			return def;
-	}
-
-	protected int getParameter(Map<String, String> configuration, String key, int def) {
-		try {
-			return Integer.parseInt(configuration.get(key));
-		} catch (Exception e) {
-			return def;
-		}
-	}
-
-	protected boolean getParameter(Map<String, String> configuration, String key, boolean def) {
-		try {
-			return Boolean.parseBoolean(configuration.get(key));
-		} catch (Exception e) {
-			return def;
-		}
-	}
-
-	/**
-	 * This method log internal parameters of this configuration worker
-	 * out of the error stream of your webapplication server.
-	 * 
-	 * @param message Message string
-	 */
-	protected void logInternal(String message) {
-		logger.log(Level.CONFIG, message);
-	}
-
-	/**
-	 * This method log internal exceptions of this configuration worker
-	 * out of the error stream of your webapplication server.
-	 * 
-	 * @param e Exception
-	 */
-	protected void logInternal(String message, Exception e) {
-		logger.log(Level.SEVERE, message, e);
-	}
-
-	static {
-		MessageHelper.init();
 	}
 }
