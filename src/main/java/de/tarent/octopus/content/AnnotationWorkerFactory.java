ecb02394dcfded48cbe7202cc8f3e7aa1c8ba597
christoph <christoph@tarent.org>
2007-06-11 13:33:12 +0000

    add / update GPL2 disclaimer and tarent copyright information

--- src/main/java/de/tarent/octopus/content/AnnotationWorkerFactory.java
+++ src/main/java/de/tarent/octopus/content/AnnotationWorkerFactory.java
@@ -1,28 +1,26 @@
-/* $Id: AnnotationWorkerFactory.java,v 1.7 2007/03/21 13:16:22 amaier Exp $
- * 
- * tarent-octopus, Webservice Data Integrator and Applicationserver
- * Copyright (C) 2002 tarent GmbH
- * 
+/*
+ * tarent-octopus annotation extension,
+ * an opensource webservice and webapplication framework (annotation extension)
+ * Copyright (c) 2006-2007 tarent GmbH
+ *
  * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License
- * as published by the Free Software Foundation; either version 2
- * of the License, or (at your option) any later version.
- * 
+ * modify it under the terms of the GNU General Public License,version 2
+ * as published by the Free Software Foundation.
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
- * 
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301, USA.
+ *
  * tarent GmbH., hereby disclaims all copyright
- * interest in the program 'tarent-octopus'
- * (which makes passes at compilers) written
- * by Sebastian Mancke
- * signature of Elmar Geese, 1 June 2002
- * Elmar Geese, CEO tarent GmbH
+ * interest in the program 'tarent-octopus annotation extension'
+ * Signature of Elmar Geese, 11 June 2007
+ * Elmar Geese, CEO tarent GmbH.
  */
 
 package de.tarent.octopus.content;
