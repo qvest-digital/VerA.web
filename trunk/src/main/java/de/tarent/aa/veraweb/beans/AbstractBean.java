a265340482612b1e064121fd37061c347aed8b95
Christoph Jerolimov <jerolimov@gmx.de>
2007-08-07 09:04:21 +0000

    Publish VerA.web on veraweb.evolis.org (Initial version.) as GPL 2.
    git-svn-id: svn+ssh://evolvis.org/scmrepos/svn/veraweb@10 293e2372-0500-4244-b9aa-80dbbde7b6ea

--- trunk/src/main/java/de/tarent/aa/veraweb/beans/AbstractBean.java
+++ trunk/src/main/java/de/tarent/aa/veraweb/beans/AbstractBean.java
@@ -1,3 +1,28 @@
+/*
+ * VerA.web,
+ * Veranstaltungsmanagment VerA.web
+ * Copyright (c) 2005-2007 tarent GmbH
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License,version 2
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301, USA.
+ *
+ * tarent GmbH., hereby disclaims all copyright
+ * interest in the program 'VerA.web'
+ * Signature of Elmar Geese, 7 August 2007
+ * Elmar Geese, CEO tarent GmbH.
+ */
+
 package de.tarent.aa.veraweb.beans;
 
 import de.tarent.octopus.PersonalConfigAA;
