78e60b16a8292f8b1c0a4bfb2edb5593ee7331a2
Carsten Klein <c.klein@tarent.de>
2008-02-18 15:58:46 +0000

    continued work on the next version release 1.2.0
    - 2.10.4 begun implementing the reporting feature
    
    
    git-svn-id: svn+ssh://evolvis.org/scmrepos/svn/veraweb@49 293e2372-0500-4244-b9aa-80dbbde7b6ea

---
+++ trunk/src/main/java/de/tarent/aa/veraweb/worker/ChangeLogReportsWorker.java
@@ -0,0 +1,112 @@
+/*
+ * veraweb,
+ * Veranstaltungsmanagment veraweb
+ * Copyright (c) 2005-2007 tarent GmbH
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License,version 2
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301, USA.
+ *
+ * tarent GmbH., hereby disclaims all copyright
+ * interest in the program 'veraweb'
+ * Signature of Elmar Geese, 21 November 2007
+ * Elmar Geese, CEO tarent GmbH.
+ */
+
+/* $Id: WorkAreaWorker.java,v 1.0 2008/02/14 13:00:00 cklein Exp $ */
+package de.tarent.aa.veraweb.worker;
+
+import java.io.IOException;
+
+import de.tarent.aa.veraweb.beans.WorkArea;
+import de.tarent.dblayer.sql.clause.Clause;
+import de.tarent.dblayer.sql.clause.Order;
+import de.tarent.dblayer.sql.statement.Select;
+import de.tarent.octopus.custom.beans.Bean;
+import de.tarent.octopus.custom.beans.BeanException;
+import de.tarent.octopus.custom.beans.veraweb.ListWorkerVeraWeb;
+import de.tarent.octopus.server.OctopusContext;
+
+/**
+ * The class ChangeLogReportsWorker is a concrete
+ * worker for operations on the {@link ChangeLogReport}
+ * bean in collaboration with the {@link ChangeLogEntry}
+ * entity bean.
+ * 
+ * @author cklein
+ * @since 1.2.0
+ * @see de.tarent.aa.veraweb.beans.ChangeLogReport
+ * @see de.tarent.octopus.customs.beans.ChangeLogEntry
+ */
+public class ChangeLogReportsWorker extends ListWorkerVeraWeb
+{
+	/**
+	 * Constructs a new instance of this.
+	 */
+	public ChangeLogReportsWorker()
+	{
+		// TODO
+		super( "ChangeLogReport" );
+	}
+
+	protected void extendWhere( OctopusContext cntx, Select select )
+		throws BeanException, IOException
+	{
+		Clause clause = getWhere( cntx );
+		if ( clause != null )
+		{
+			select.where( clause );
+		}
+	}
+
+	protected void extendColumns( OctopusContext cntx, Select select )
+		throws BeanException, IOException
+	{
+		if ( cntx.requestContains( "order" ) )
+		{
+			String order = cntx.requestAsString( "order" );
+			if ( "name".equals( order ) )
+			{
+				select.orderBy( Order.asc( order ) );
+				cntx.setContent( "order", order );
+			} else if ( "flags".equals( order ) )
+			{
+				select.orderBy( Order.asc( order ).andAsc( "name" ) );
+				cntx.setContent( "order", order );
+			}
+		}
+	}
+
+	protected void extendAll( OctopusContext cntx, Select select )
+		throws BeanException, IOException
+	{
+		Clause clause = getWhere( cntx );
+		if ( clause != null )
+		{
+			select.where( clause );
+		}
+	}
+
+	protected Clause getWhere( OctopusContext cntx )
+		throws BeanException
+	{
+		Clause clause = null;
+		//Clause clause = Expr.equal( "deleted", "f" );
+		return clause;
+	}
+
+	protected void saveBean(OctopusContext cntx, Bean bean) throws BeanException, IOException
+	{
+		throw new RuntimeException( "Change log entries cannot be modified. Not implemented." );
+	}
+}
