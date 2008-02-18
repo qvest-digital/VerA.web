63ae62838fb5b45bdbd28dabf54aa510d5bd8e3d
mpelzer <mpelzer@users.evolvis.org>
2008-02-18 16:39:27 +0000

    new exception that will be thrown if update or delete methods are called for entites that do not exist in database

---
+++ src/main/java/de/tarent/dblayer/persistence/EntityOperationException.java
@@ -0,0 +1,25 @@
+package de.tarent.dblayer.persistence;
+
+/** exception that will be thrown if there is a problem during
+ * operations (insert, update, delete) on entities that are not
+ * SQL related
+ * 
+ * @author Martin Pelzer, tarent GmbH
+ *
+ */
+public class EntityOperationException extends Exception {
+
+	private static final long serialVersionUID = -3529977685324692869L;
+
+	
+
+	public EntityOperationException() {
+        super();
+    }
+
+	
+    public EntityOperationException(String message) {
+        super(message);
+    }
+	
+}
