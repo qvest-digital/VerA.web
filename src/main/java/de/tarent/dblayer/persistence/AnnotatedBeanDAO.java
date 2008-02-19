60d1effbe8c803d869c103ebb1b79c6959f71629
mpelzer <mpelzer@users.evolvis.org>
2008-02-19 08:27:58 +0000

    new constructor which get db mapping as parameter for easier use

--- src/main/java/de/tarent/dblayer/persistence/AnnotatedBeanDAO.java
+++ src/main/java/de/tarent/dblayer/persistence/AnnotatedBeanDAO.java
@@ -1,5 +1,7 @@
 package de.tarent.dblayer.persistence;
 
+import de.tarent.dblayer.engine.DBAccess;
+
 /** data access object for annotated beans. simplifies the
  * handling of DAOs a bit (but just a little bit...)
  * 
@@ -11,6 +13,19 @@ public class AnnotatedBeanDAO extends AbstractDAO {
 	public AnnotatedBeanDAO(Class bean) {
 		super(bean);
 		this.setEntityFactory(new DefaultAnnotatedBeanEntityFactory(bean));
+	}
+	
+	
+	public AnnotatedBeanDAO(Class bean, AbstractDBMapping dbMapping) {
+		super(bean);
+		this.setEntityFactory(new DefaultAnnotatedBeanEntityFactory(bean));
+		
+		// initialize DB mapping and set to this DAO
+		dbMapping.setDBContext(DBAccess.getContextWithoutConnection());
+		dbMapping.setBeanName(bean);
+		dbMapping.configureMapping();
+		dbMapping.init();
+		setDbMapping(dbMapping);
 	}
 	
 }
