4b37d56873a9c12f11a8eddf01e23db39e12a5c6
Thomas Schmitz <t.schmitz@tarent.de>
2009-01-05 02:13:14 +0000

    release version 1.2.2
    git-svn-id: svn+ssh://evolvis.org/scmrepos/svn/veraweb/tags/1.2.2@166 293e2372-0500-4244-b9aa-80dbbde7b6ea

---
+++ trunk/veraweb-war/src/main/java/de/tarent/octopus/beans/veraweb/ListWorkerVeraWeb.java
@@ -0,0 +1,71 @@
+/*
+ * veraweb,
+ * Veranstaltungsmanagment veraweb
+ * Copyright (c) 2005-2007 tarent GmbH
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License,version 2
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301, USA.
+ *
+ * tarent GmbH., hereby disclaims all copyright
+ * interest in the program 'veraweb'
+ * Signature of Elmar Geese, 21 November 2007
+ * Elmar Geese, CEO tarent GmbH.
+ */
+
+/* $Id: ListWorkerVeraWeb.java,v 1.1 2007/06/20 11:56:52 christoph Exp $ */
+package de.tarent.octopus.beans.veraweb;
+
+import de.tarent.octopus.beans.BeanListWorker;
+import de.tarent.octopus.beans.Database;
+import de.tarent.octopus.beans.Request;
+import de.tarent.octopus.server.OctopusContext;
+
+/**
+ * Diese Klasse konkretisiert den abstrakten Basis-Worker {@link BeanListWorker}
+ * auf die Benutzung der VerA.web-spezifischen Implementierungen von
+ * {@link Database} und {@link Request} hin.
+ */
+public class ListWorkerVeraWeb extends BeanListWorker {
+    //
+    // Konstruktoren
+    //
+    /**
+     * Dieser Konstruktor gibt den Namen der zugrunde liegenden Bean weiter.
+     */
+    protected ListWorkerVeraWeb(String beanName) {
+        super(beanName);
+    }
+
+    //
+    // Oberklasse BeanListWorker
+    //
+	/**
+     * Diese Methode liefert eine {@link DatabaseVeraWeb}-Instanz
+     * 
+	 * @see BeanListWorker#getDatabase(OctopusContext)
+	 */
+	@Override
+    protected Database getDatabase(OctopusContext cntx) {
+		return new DatabaseVeraWeb(cntx);
+	}
+
+	/**
+     * Diese Methode liefert eine {@link RequestVeraWeb}-Instanz.
+	 * @see BeanListWorker#getRequest(OctopusContext)
+	 */
+	@Override
+    protected Request getRequest(OctopusContext cntx) {
+		return new RequestVeraWeb(cntx);
+	}
+}
---
+++ trunk/veraweb-war/src/test/java/de/tarent/octopus/custom/beans/ProfileLogger.java
@@ -0,0 +1,80 @@
+/*
+ * veraweb,
+ * Veranstaltungsmanagment veraweb
+ * Copyright (c) 2005-2007 tarent GmbH
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License,version 2
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301, USA.
+ *
+ * tarent GmbH., hereby disclaims all copyright
+ * interest in the program 'veraweb'
+ * Signature of Elmar Geese, 21 November 2007
+ * Elmar Geese, CEO tarent GmbH.
+ */
+
+/*
+ * $Id: ProfileLogger.java,v 1.1 2007/06/20 11:56:52 christoph Exp $
+ * 
+ * Created on 18.11.2005
+ */
+package de.tarent.octopus.custom.beans;
+
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
+
+/**
+ * Diese Klasse stellt Methoden zum Loggen von Profiling-Informationen
+ * zur Verfügung.
+ * 
+ * @author mikel
+ */
+public final class ProfileLogger {
+    //
+    // Konstruktoren
+    //
+    /**
+     * Dieser Konstruktor setzt den Startpunkt auf jetzt.
+     */
+    public ProfileLogger() {
+        super();
+        last = System.currentTimeMillis();
+    }
+    
+    //
+    // öffentliche Methoden
+    //
+    /**
+     * Diese Methode loggt die Millisekunden seit dem letzten Aufruf
+     * mit dem übergebenen String als Information.
+     * 
+     * @param string Log-Eintrag-Info
+     */
+    public void log(final String string) {
+        final long now = System.currentTimeMillis();
+        logger.log(level, "[" + (now - last) + "ms] " + string);
+        last = now;
+    }
+
+    //
+    // geschützte Member
+    //
+    /** letzter Messpunkt */
+    long last = 0;
+    
+    /** Log-Level */
+    Level level = Level.INFO;
+    
+    /** Logger */
+    Logger logger = Logger.getLogger(ProfileLogger.class.getName());
+}
