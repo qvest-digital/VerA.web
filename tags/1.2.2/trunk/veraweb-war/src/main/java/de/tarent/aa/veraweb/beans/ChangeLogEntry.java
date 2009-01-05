34eb28333d06a38694282392676131cee02ca9d2
Thomas Schmitz <t.schmitz@tarent.de>
2009-01-05 02:13:14 +0000

    release version 1.2.2
    git-svn-id: svn+ssh://evolvis.org/scmrepos/svn/veraweb@166 293e2372-0500-4244-b9aa-80dbbde7b6ea

---
+++ tags/1.2.2/trunk/veraweb-war/src/main/java/de/tarent/aa/veraweb/beans/ChangeLogEntry.java
@@ -0,0 +1,103 @@
+/*
+ * veraweb,
+ * Veranstaltungsmanagment veraweb
+ * Copyright (c) 2005-2008 tarent GmbH
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License,version 2
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301, USA.
+ *
+ * tarent GmbH., hereby disclaims all copyright
+ * interest in the program 'veraweb'
+ * Signature of Elmar Geese, 21 November 2007
+ * Elmar Geese, CEO tarent GmbH.
+ */
+
+/*
+ * $Id: ChangeLogEntry.java,v 1.2 2008/02/12 12:00:00 cklein Exp $
+ * 
+ * Created on 12.02.2008
+ */
+package de.tarent.aa.veraweb.beans;
+
+import java.util.Date;
+
+import de.tarent.octopus.PersonalConfigAA;
+import de.tarent.octopus.beans.BeanException;
+import de.tarent.octopus.server.OctopusContext;
+import de.tarent.octopus.server.PersonalConfig;
+
+/**
+ * The bean class ChangeLogEntry represents a single entry
+ * in the newly introduced change logging feature.
+ * 
+ * Each changelog entry stores information on the change
+ * action, which is one of insert, delete or update and
+ * the attributes of the object that were changed by the
+ * user who committed the action.
+ * 
+ * For now, changelogging is enabled for the following
+ * entities {see Person}, {see Guest}, and {see Event}.  
+ * 
+ * @see de.tarent.octopus.beans.veraweb.BeanChangeLogger
+ * 
+ * @author cklein
+ * @since 1.2.0
+ */
+public class ChangeLogEntry extends AbstractBean
+{
+	public Integer	id;
+	public String	username;
+	public String	objectname;
+	public String	objecttype;
+	public Integer	objectid;
+	public String	op;
+	public String	attributes;
+	public Date		created;
+
+	/**
+	 * Creates a new instance of this.
+	 */
+	public ChangeLogEntry()
+	{
+		super();
+	}
+
+	/**
+	 * Only admins may read the entity beans from the table.
+	 */
+	@Override
+    public void checkRead( OctopusContext cntx ) throws BeanException
+	{
+		checkGroup( cntx, PersonalConfigAA.GROUP_ADMIN );
+	}
+
+	/**
+	 * Anonymous user group requires write access to the
+	 * entity bean, since a background service will be responsible
+	 * for purging old entries from change log. The service runs
+	 * with the priviledge of the anonymous user group.
+	 */
+	@Override
+    public void checkWrite( OctopusContext cntx ) throws BeanException
+	{
+		try
+		{
+			checkGroup( cntx, PersonalConfig.GROUP_ANONYMOUS );
+		}
+		catch( BeanException e )
+		{
+			checkGroup( cntx, PersonalConfig.GROUP_USER );
+		}
+	}
+}
---
+++ tags/1.2.2/trunk/veraweb-war/src/main/java/de/tarent/aa/veraweb/beans/Function.java
@@ -0,0 +1,72 @@
+/*
+ * veraweb,
+ * Veranstaltungsmanagment veraweb
+ * Copyright (c) 2005-2007 tarent GmbH
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License,version 2
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301, USA.
+ *
+ * tarent GmbH., hereby disclaims all copyright
+ * interest in the program 'veraweb'
+ * Signature of Elmar Geese, 21 November 2007
+ * Elmar Geese, CEO tarent GmbH.
+ */
+
+/*
+ * Created on 23.02.2005
+ */
+package de.tarent.aa.veraweb.beans;
+
+import de.tarent.octopus.PersonalConfigAA;
+import de.tarent.octopus.beans.BeanException;
+import de.tarent.octopus.server.OctopusContext;
+
+public class Function extends AbstractBean {
+	public Integer id;
+	public String name;
+
+	@Override
+    public void verify() throws BeanException {
+		if (name == null || name.length() == 0)
+			addError("Sie müssen der Funktion einen Namen geben.");
+	}
+
+    /**
+     * Diese Methode testet, ob im aktuellen Kontext diese Bohne gelesen werden
+     * darf.<br>
+     * Test ist, ob der Benutzer Standard-Reader ist.
+     * 
+     * @param cntx Octopus-Kontext
+     * @throws BeanException Wenn im angegebenen Kontext diese Bohne nicht gelesen werden darf.
+     * @see de.tarent.aa.veraweb.beans.AbstractBean#checkRead(de.tarent.octopus.server.OctopusContext)
+     */
+    @Override
+    public void checkRead(OctopusContext cntx) throws BeanException {
+        checkGroup(cntx, PersonalConfigAA.GROUP_READ_STANDARD);
+    }
+
+    /**
+     * Diese Methode testet, ob im aktuellen Kontext diese Bohne geschrieben
+     * werden darf.<br>
+     * Test ist, ob der Benutzer Teil-Admin ist.
+     * 
+     * @param cntx Octopus-Kontext
+     * @throws BeanException Wenn im angegebenen Kontext diese Bohne nicht geschrieben werden darf.
+     * @see de.tarent.aa.veraweb.beans.AbstractBean#checkWrite(de.tarent.octopus.server.OctopusContext)
+     */
+    @Override
+    public void checkWrite(OctopusContext cntx) throws BeanException {
+        checkGroup(cntx, PersonalConfigAA.GROUP_PARTIAL_ADMIN);
+    }
+}
