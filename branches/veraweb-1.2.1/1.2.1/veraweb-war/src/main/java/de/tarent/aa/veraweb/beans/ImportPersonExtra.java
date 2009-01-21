5c9b8493d5902b61a2371c436dcfbc977267138e
Thomas Schmitz <t.schmitz@tarent.de>
2009-01-21 15:53:13 +0000

    branch for "Auswärtiges Amt" bugfixing (2009-01-21)
    git-svn-id: svn+ssh://evolvis.org/scmrepos/svn/veraweb@171 293e2372-0500-4244-b9aa-80dbbde7b6ea

---
+++ branches/veraweb-1.2.1/1.2.1/veraweb-war/src/main/java/de/tarent/aa/veraweb/beans/ImportPersonExtra.java
@@ -0,0 +1,46 @@
+/*
+ * veraweb,
+ * Veranstaltungsmanagment veraweb
+ * Copyright (c) 2005-2007 tarent GmbH
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License,version 2
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301, USA.
+ *
+ * tarent GmbH., hereby disclaims all copyright
+ * interest in the program 'veraweb'
+ * Signature of Elmar Geese, 21 November 2007
+ * Elmar Geese, CEO tarent GmbH.
+ */
+
+/*
+ * $Id: ImportPersonExtra.java,v 1.1 2007/06/20 11:56:51 christoph Exp $
+ * 
+ * Created on 15.09.2005
+ */
+package de.tarent.aa.veraweb.beans;
+
+/**
+ * Diese Schnittstelle markiert Beans, die Ergänzungen zu ImportPersons darstellen. 
+ * 
+ * @author mikel
+ */
+public interface ImportPersonExtra {
+    /**
+     * Diese Methode assoziiert diese Importpersonenergänzung mit ihrer Importperson;
+     * dies bedeutet insbesondere die Übernahme deren ID als Fremdschlüssel auf sie. 
+     * 
+     * @param person Importperson, mit der dieses Extra assoziiert werden soll.
+     */
+    void associateWith(ImportPerson person);
+}
