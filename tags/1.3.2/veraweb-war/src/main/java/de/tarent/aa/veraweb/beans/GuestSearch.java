a3adb6e431b8ac44fc37186d2983872597f2ef1c
Thomas Schmitz <t.schmitz@tarent.de>
2009-10-05 13:37:01 +0000

    Tagged release version 1.3.2
    git-svn-id: svn+ssh://evolvis.org/scmrepos/svn/veraweb@284 293e2372-0500-4244-b9aa-80dbbde7b6ea

---
+++ tags/1.3.2/veraweb-war/src/main/java/de/tarent/aa/veraweb/beans/GuestSearch.java
@@ -0,0 +1,48 @@
+/*
+ * veraweb,
+ * Veranstaltungsmanagment veraweb
+ * Copyright (c) 2005-2007 tarent GmbH
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License,version 2
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301, USA.
+ *
+ * tarent GmbH., hereby disclaims all copyright
+ * interest in the program 'veraweb'
+ * Signature of Elmar Geese, 21 November 2007
+ * Elmar Geese, CEO tarent GmbH.
+ */
+
+/*
+ * Created on 07.04.2005
+ */
+package de.tarent.aa.veraweb.beans;
+
+/**
+ * Bean zum Filtern einer G‰steliste und navigieren
+ * innerhalb der Detailansicht.
+ * 
+ * Entspricht keiner Datenbank-Tabelle und wird ausschlieﬂlich
+ * in der Session gehalten oder aus dem Request geladen.
+ */
+public class GuestSearch extends AbstractBean {
+	public Integer event;
+	public Integer offset;
+	public Integer count;
+	/** Gibt an ob nach Platz / Reserve gefiltert werden soll. */
+	public Integer reserve;
+	/** Gibt an ob nach Offene / Zusagen / Absagen gefiltert werden soll. */
+	public Integer invitationstatus;
+	/** Gibt an in welcher Sortierreihenfolge die G‰steliste angezeigt werden soll.*/
+	public String listorder; // orderno, lastname, firstname, email
+}
---
+++ tags/1.3.2/veraweb-war/src/main/java/de/tarent/aa/veraweb/utils/ExportHelper.java
@@ -0,0 +1,74 @@
+/*
+ * veraweb,
+ * Veranstaltungsmanagment veraweb
+ * Copyright (c) 2005-2007 tarent GmbH
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License,version 2
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301, USA.
+ *
+ * tarent GmbH., hereby disclaims all copyright
+ * interest in the program 'veraweb'
+ * Signature of Elmar Geese, 21 November 2007
+ * Elmar Geese, CEO tarent GmbH.
+ */
+
+/*
+ * $Id: ExportHelper.java,v 1.1 2007/06/20 11:56:52 christoph Exp $
+ * 
+ * Created on 07.02.2006
+ */
+package de.tarent.aa.veraweb.utils;
+
+/**
+ * Diese Klasse enthält statische Hilfsmethoden für die Behandlung von
+ * Export -Dateinamen und -Content-Typen. Diese werden hier zentral
+ * "entstandardisiert" um sicherzustellen das diese vom Browser
+ * als Download angeboten werden statt diese z.B. als Active-X-Controll
+ * einzubinden.
+ */
+public class ExportHelper {
+	/**
+	 * Erweitert die Standard-Dateiendung um den Zusatz <code>.export</code>.
+	 * 
+	 * @param extension Original Dateiendung
+	 * @return angepasste Dateiendung
+	 */
+	static public String getExtension(String extension) {
+		return extension;
+//		return extension.endsWith(".export") ? extension : extension + ".export";
+	}
+
+	/**
+	 * Erweitert den Standard-Dateinamen um den Zusatz <code>.export</code>.
+	 * 
+	 * @param filename Original Dateiname
+	 * @return angepassten Dateinamen
+	 */
+	static public String getFilename(String filename) {
+		return filename;
+//		return filename.endsWith(".export") ? filename : filename + ".export";
+	}
+
+	/**
+	 * Ersetzt den Standard-Content-Type durch den allgemeinen Standard
+	 * für beliebige Datenströme: <code>application/octet-stream</code>
+	 * 
+	 * @param contentType Original Content-Type
+	 * @return angepassten Content-Type
+	 */
+	static public String getContentType(String contentType) {
+		return contentType;
+//		return "application/octet-stream";
+	}
+}
