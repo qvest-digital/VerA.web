13c658a601c3dcc13a9b3f0b2f0e53c7de413d5d
cklein <cklein@users.evolvis.org>
2010-02-24 13:31:39 +0000

    finalizing changes for veraweb specific release, updated pom dependencies
    the actual changes included are are wrapper mechanism using dynamic proxies that will circumvent a
    possible configuration problem with postgresql where there are timestamps with timezone information
    stored in the database that have a different timezone than the postgresql server and the webserver
    accessing the dbms. in that case, whenever a timestamp was instantiated from the database, the time
    would shift everytime that the data was accessed.

---
+++ src/main/java/de/tarent/dblayer/engine/proxy/CallableStatementProxyInvocationHandler.java
@@ -0,0 +1,62 @@
+package de.tarent.dblayer.engine.proxy;
+
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
+import java.sql.CallableStatement;
+import java.sql.ResultSet;
+import java.util.Calendar;
+
+public class CallableStatementProxyInvocationHandler extends PreparedStatementProxyInvocationHandler
+{
+	private ResultSet resultSet;
+
+	public CallableStatementProxyInvocationHandler( CallableStatement callableStatement )
+	{
+		super( callableStatement );
+	}
+
+	public Object invoke( Object proxy, Method method, Object[] args ) throws Throwable
+	{
+		Object result = null;
+
+		String methodName = method.getName();
+		if ( "getObject".equals( methodName ) && args.length == 1 )
+		{
+			this.fetchResultSet();
+			if ( args[ 0 ] instanceof String )
+			{
+				result = this.resultSet.getObject( ( String ) args[ 0 ] );
+			}
+			else
+			{
+				result = this.resultSet.getObject( ( Integer ) args[ 0 ] );
+			}
+		}
+		else if ( "getTimestamp".equals( methodName ) )
+		{
+			this.fetchResultSet();
+			if ( args[ 0 ] instanceof String )
+			{
+				result = this.resultSet.getTimestamp( ( String ) args[ 0 ], args.length == 2 ? ( Calendar ) args[ 1 ] : null );
+			}
+			else
+			{
+				result = this.resultSet.getTimestamp( ( Integer ) args[ 0 ], args.length == 2 ? ( Calendar ) args[ 1 ] : null );
+			}
+		}
+		else
+		{
+			result = super.invoke( proxy, method, args );
+		}
+
+		return result;
+	}
+
+	private void fetchResultSet() throws Throwable
+	{
+		if ( this.resultSet == null )
+		{
+			this.resultSet = ( ResultSet ) Proxy.newProxyInstance( this.getClass().getClassLoader(), new Class[] { ResultSet.class }, new ResultSetProxyInvocationHandler( this.statement.getResultSet() ) );
+		}
+	}
+}
