737ad4b2eacc257c9eb0b11bc502a645e1bc29d1
Valentin But <v.but@tarent.de>
2013-04-11 13:26:06 +0000

    added initial page definition and abstract step definitions
    git-svn-id: svn+ssh://evolvis.org/scmrepos/svn/veraweb/trunk@397 293e2372-0500-4244-b9aa-80dbbde7b6ea

---
+++ veraweb-system-test/src/test/java/de/tarent/aa/veraweb/cucumber/pagedefinitions/PageDefinition.java
@@ -0,0 +1,154 @@
+package de.tarent.aa.veraweb.cucumber.pagedefinitions;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.support.ByIdOrName;
+
+import de.tarent.aa.veraweb.cucumber.env.GlobalConfig;
+import de.tarent.aa.veraweb.cucumber.utils.NameUtil;
+
+/**
+ * {@link Enum} for all pages.
+ * 
+ * @author Michael Kutz, tarent Solutions GmbH
+ * @author Valentin But (v.but@tarent.de), tarent solutions GmbH
+ */
+public enum PageDefinition {
+
+	/** 
+	 * Anmeldungsseite 
+	 */
+	ANMELDUNGSSEITE("ShowLogin",
+			new ElementDefinition("Benutzername-Feld", "input.username"),
+			new ElementDefinition("Passwort-Feld", "input.password"),
+			new ElementDefinition("In Vertretung Anmelden-Checkbox", "input.loginasproxy", false),
+			new ElementDefinition("Anmelden-Button", "button.login"),
+			new ElementDefinition("FelderLeeren-Button", "button.clearFields"),
+			new ElementDefinition("Infobox-Titel", "text.infoTitle"),
+			new ElementDefinition("Infobox-Text", "text.infoText")),
+
+	/** 
+	 * Aufgabenübersichtsseite 
+	 */
+	AUFGABENUEBERSICHTSEITE(
+	        new ElementDefinition("Löschen-Button", "button.delete"),
+            new ElementDefinition("Zurück-Button", "button.back")),
+	        
+    /** 
+     * Aufgabendetailseite
+     */
+    AUFGABEDETAILSEITE(
+            new ElementDefinition("Titel-Feld", "input.title"),
+            new ElementDefinition("Beschreibung-Feld", "input.description", false),
+            new ElementDefinition("Startdatum-Feld", "input.startdate", false),
+            new ElementDefinition("Enddatum-Feld", "input.enddate", false),
+            new ElementDefinition("Fortschrittsgrad-Button", "input.degreeOfCompletion", false),
+            new ElementDefinition("Priorität-Button", "input.priority", false),
+            new ElementDefinition("Veranwortliche Person-Button", "input.responsiblePerson", false),
+            new ElementDefinition("Speichern-Button", "button.save"),
+            new ElementDefinition("Zurück-Button", "button.back"));
+
+	/** 
+	 * URL of the page relative to the {@link GlobalConfig#getDnsaBaseUrl()}. 
+	 */
+	public final String url;
+
+	/** 
+	 * All {@link PageElementDefinition}s mapped to their name. 
+	 */
+	private final Map<String, ElementDefinition> elementMap = new HashMap<String, PageDefinition.ElementDefinition>();
+
+	/** 
+	 * All {@link PageElementDefinition}s. 
+	 */
+	public final ElementDefinition[] elements;
+
+	private PageDefinition(String url, ElementDefinition... elements) {
+		this.url = url;
+		this.elements = elements;
+		for (ElementDefinition element : elements) {
+			ElementDefinition previous = this.elementMap.put(element.name, element);
+			if (previous != null) {
+				throw new IllegalArgumentException(String.format(
+						"The element %1$s was added before. Element names must be unique.", element.name));
+			}
+		}
+	}
+
+	private PageDefinition(ElementDefinition... elements) {
+		this((String) null, elements);
+	}
+
+	public static PageDefinition pageForName(String pageName) {
+		return valueOf(NameUtil.nameToEnumName(pageName));
+	}
+
+	public ElementDefinition elementForName(String elementName) {
+		if (!this.elementMap.containsKey(elementName)) {
+			throw new IllegalArgumentException(
+					String.format("Page %1$s has no element %2$s.", this.name(), elementName));
+		}
+		return this.elementMap.get(elementName);
+	}
+
+	/**
+	 * Class to define a page element.
+	 * 
+	 * @author Michael Kutz, tarent Solutions GmbH
+	 */
+	public static class ElementDefinition {
+
+		/**
+		 * A name for the element that may be used in cucumber feature definitions. Will be used to resolve the element
+		 * by {@link PageDefinition#elementForName(String)}.
+		 */
+		public final String name;
+
+		/** 
+		 * The {@link By} criteria to find the element. 
+		 */
+		public final By by;
+
+		/** 
+		 * True if the element is required to be present on its page. 
+		 */
+		public final boolean required;
+
+		/**
+		 * Standard constructor.
+		 * 
+		 * @param name
+		 *            the {@link #name} {@link String}.
+		 * @param idOrName
+		 *            the elemet's ID or name attribute value. Will be used to create the element's {@link #by} object
+		 *            to find the actual element on an actual page.
+		 * @param required
+		 *            the {@link #required} {@link Boolean}.
+		 */
+		private ElementDefinition(final String name, final String idOrName, final boolean required) {
+			this.name = name;
+			this.by = new ByIdOrName(idOrName);
+			this.required = required;
+		}
+
+		/**
+		 * Convenience constructor for required elements (sets {@link #required} to {@code true}).
+		 * 
+		 * @param name
+		 *            the {@link #name} {@link String}.
+		 * @param idOrName
+		 *            the elemet's ID or name attribute value. Will be used to create the element's {@link #by} object
+		 *            to find the actual element on an actual page.
+		 */
+		private ElementDefinition(final String name, final String idOrName) {
+			this(name, idOrName, true);
+		}
+
+		@Override
+		public String toString() {
+			return "ElementDefinition [name=" + name + ", by=" + by + ", required=" + required + "]";
+		}
+	}
+}
---
+++ veraweb-system-test/src/test/java/de/tarent/aa/veraweb/cucumber/stepdefinitions/AbstractStepDefinitions.java
@@ -0,0 +1,206 @@
+package de.tarent.aa.veraweb.cucumber.stepdefinitions;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import org.joda.time.DateMidnight;
+import org.joda.time.format.DateTimeFormat;
+import org.joda.time.format.DateTimeFormatter;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import de.tarent.aa.veraweb.cucumber.env.GlobalConfig;
+import de.tarent.aa.veraweb.cucumber.pagedefinitions.PageDefinition;
+import de.tarent.aa.veraweb.cucumber.pagedefinitions.PageDefinition.ElementDefinition;
+import de.tarent.aa.veraweb.selenium.AdvancedWebDriver;
+
+/**
+ * Super class for all {@link AbstractStepDefinitions}.
+ * 
+ * @author Michael Kutz, tarent Solutions GmbH
+ * @author Valentin But (v.but@tarent.de), tarent solutions GmbH
+ */
+public abstract class AbstractStepDefinitions {
+    
+	protected static PageDefinition page;
+
+	/**
+	 * Default {@link DateTimeFormatter}.
+	 */
+	protected static final DateTimeFormatter DEFAULT_DATETIME_FORMATTER = DateTimeFormat.forPattern("dd.MM.yyyy");
+
+	/**
+	 * Injected {@link GlobalConfig}.
+	 */
+	@Autowired
+	protected GlobalConfig config;
+
+	/**
+	 * Injected {@link AdvancedWebDriver}.
+	 */
+	@Autowired
+	protected WebDriver driver;
+
+	/**
+	 * Enters the given {@code value} into the given {@code elementDefinition}.
+	 * 
+	 * @param elementDefinition
+	 *            the {@link ElementDefinition} of the field that should be filled.
+	 * @param value
+	 *            the {@link String} the field should be filled with.
+	 */
+	protected void whenFillField(String value, ElementDefinition elementDefinition) {
+		WebElement element = findPageElement(elementDefinition);
+		element.clear();
+		element.sendKeys(value);
+	}
+
+	/**
+	 * Clears the given {@code elementDefinition}.
+	 * 
+	 * @param elementDefinition
+	 *            the {@link ElementDefinition} of the field that should be cleared.
+	 */
+	protected void whenNotFillField(ElementDefinition elementDefinition) {
+		findPageElement(elementDefinition).clear();
+	}
+
+	/**
+	 * Enters the given {@code date} into the given {@code elementDefinition}.
+	 * 
+	 * @param elementDefinition
+	 *            the {@link ElementDefinition} of the field that should be filled.
+	 * @param date
+	 *            the {@link DateMidnight} the field should be filled with.
+	 */
+	protected void whenFillField(DateMidnight date, ElementDefinition elementDefinition) {
+		whenFillField(date.toString(DEFAULT_DATETIME_FORMATTER), elementDefinition);
+	}
+
+	/**
+	 * Fails if the current page does not match the given {@code pageDefinition}, sets the {@link #page} otherwise.
+	 * 
+	 * @param pageDefinition
+	 *            the {@link PageDefinition} that should match the current page.
+	 */
+	protected void thenAtPage(PageDefinition pageDefinition) {
+		assertTrue(pageDefinition.url == null || driver.getCurrentUrl().endsWith(pageDefinition.url));
+		thenElementsPresent(pageDefinition.elements);
+		page = pageDefinition;
+	}
+
+	/**
+	 * Will {@link #fail()} if the given {@code elementDefinition} can not be found on the current page.
+	 * 
+	 * @param elementDefinition
+	 *            the {@link ElementDefinition} of the required element.
+	 */
+	protected void thenElementPresent(ElementDefinition elementDefinition) {
+		try {
+			findPageElement(elementDefinition);
+		} catch (NoSuchElementException e) {
+			throw new IllegalStateException(String.format(
+					"Required element \"%1$s\" cound not be found by %2$s at \"%3$s\".", elementDefinition,
+					elementDefinition.by, driver.getCurrentUrl()), e);
+		}
+	}
+
+	/**
+	 * Will {@link #fail()} if the given {@code elementDefinition} can be found on the current page.
+	 * 
+	 * @param elementDefinition
+	 *            the {@link ElementDefinition} of the element that should not be present.
+	 */
+	protected void thenElementNotPresent(ElementDefinition elementDefinition) {
+		assertTrue(driver.findElements(elementDefinition.by).isEmpty());
+	}
+
+	/**
+	 * Will {@link #fail()} if not all given {@code elementDefinitions} can be found on the current page.
+	 * 
+	 * @param elementDefinitions
+	 *            {@link ElementDefinition}s of all required elements.
+	 */
+	protected void thenElementsPresent(ElementDefinition... elementDefinitions) {
+		boolean allRequiredFound = true;
+		StringBuilder messageBuilder = new StringBuilder();
+		for (ElementDefinition elementDefinition : elementDefinitions) {
+			try {
+				findPageElement(elementDefinition);
+			} catch (NoSuchElementException e) {
+				messageBuilder.append(String.format("\n - \"%1$s\" %2$s.", elementDefinition, elementDefinition.by));
+				allRequiredFound = !elementDefinition.required;
+			}
+		}
+
+		if (!allRequiredFound) {
+			new IllegalStateException(String.format("Not all required elements were found at %1$s: %2$s",
+					driver.getCurrentUrl(), messageBuilder.toString()));
+		}
+	}
+
+	/**
+	 * Navigates the {@link #driver} to the given {@link PageDefinition#url} and sets {@link #page}.
+	 * 
+	 * @param pageDefinition
+	 *            the {@link PageDefinition} of the page to navigate to.
+	 */
+	protected void whenNavigateToPage(PageDefinition pageDefinition) {
+		if (pageDefinition.url == null) {
+			throw new IllegalArgumentException("The given pageDefinition " + pageDefinition
+					+ " has no URL an can therefor not be accessed directly!");
+		}
+		driver.get(config.getVerawebBaseUrl() + "/" + pageDefinition.url);
+
+		page = pageDefinition;
+	}
+
+	/**
+	 * Clicks the given {@code elementDefinition}.
+	 * 
+	 * @param elementDefinition
+	 *            the {@link ElementDefinition} that should be clicked.
+	 */
+	protected void whenClickElement(ElementDefinition elementDefinition) {
+		findPageElement(elementDefinition).click();
+	}
+
+	/**
+	 * Fails unless the given {@code elementDefinition}'s text equals the given {@code expectedText}.
+	 * 
+	 * @param elementDefinition
+	 *            the {@link ElementDefinition} whose text should be equal to the given {@code expectedText}.
+	 * @param expectedText
+	 *            the expected text.
+	 */
+	protected void thenElementTextEquals(ElementDefinition elementDefinition, String expectedText) {
+		String acturalText = findPageElement(elementDefinition).getText();
+		assertEquals(expectedText, acturalText);
+	}
+
+	/**
+	 * Fails unless the given {@code elementDefinition}'s text matches the given {@code expression}.
+	 * 
+	 * @param elementDefinition
+	 *            the {@link ElementDefinition} whose text should match the given {@code expression}.
+	 * @param expression
+	 *            the regular expression that should be matched by the element's text.
+	 */
+	protected void thenElementTextMatches(ElementDefinition elementDefinition, String expression) {
+		String acturalText = findPageElement(elementDefinition).getText();
+		assertTrue(acturalText.matches(expression));
+	}
+
+	/**
+	 * Returns the {@link WebElement} defined by the given {@link ElementDefinition}.
+	 * 
+	 * @param elementDefinition
+	 *            the {@link ElementDefinition} to look for.
+	 * @return the {@link WebElement} for the given {@link ElementDefinition}.
+	 */
+	private WebElement findPageElement(ElementDefinition elementDefinition) {
+		return driver.findElement(elementDefinition.by);
+	}
+}
