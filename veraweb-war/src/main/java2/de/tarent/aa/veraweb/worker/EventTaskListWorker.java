6808f3a86c77f68c06537ca715adad9a7d647dda
Patrick Apel <p.apel@tarent.de>
2013-04-18 14:43:08 +0000

    Deletion method in worker and velocity template
    git-svn-id: svn+ssh://evolvis.org/scmrepos/svn/veraweb/trunk@484 293e2372-0500-4244-b9aa-80dbbde7b6ea

--- veraweb-war/src/main/java2/de/tarent/aa/veraweb/worker/EventTaskListWorker.java
+++ veraweb-war/src/main/java2/de/tarent/aa/veraweb/worker/EventTaskListWorker.java
@@ -2,16 +2,19 @@ package de.tarent.aa.veraweb.worker;
 
 import java.io.IOException;
 import java.security.acl.LastOwnerException;
-import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 
 import de.tarent.aa.veraweb.beans.Person;
 import de.tarent.aa.veraweb.beans.Task;
+import de.tarent.dblayer.sql.clause.Clause;
 import de.tarent.dblayer.sql.clause.Expr;
 import de.tarent.dblayer.sql.clause.WhereList;
 import de.tarent.dblayer.sql.statement.Select;
 import de.tarent.octopus.beans.BeanException;
 import de.tarent.octopus.beans.Database;
+import de.tarent.octopus.beans.TransactionContext;
 import de.tarent.octopus.beans.veraweb.DatabaseVeraWeb;
 import de.tarent.octopus.beans.veraweb.ListWorkerVeraWeb;
 import de.tarent.octopus.server.OctopusContext;
@@ -39,6 +42,7 @@ public class EventTaskListWorker extends ListWorkerVeraWeb {
         super("Task");
     }
     
+    @Override
     public List<Task> showList(OctopusContext cntx) throws IOException, BeanException {
         List<Task> list = super.showList(cntx);
 
@@ -56,8 +60,7 @@ public class EventTaskListWorker extends ListWorkerVeraWeb {
         }
         
         return list;
-    }
-    
+    }  
     
     @Override
     protected void extendWhere(OctopusContext cntx, Select select) throws BeanException {
@@ -66,5 +69,43 @@ public class EventTaskListWorker extends ListWorkerVeraWeb {
         Integer eventId = Integer.valueOf(strEventId);
         where.addAnd(Expr.equal("fk_event", eventId));
         select.where(where);
+    }
+    
+    /**
+     * Überprüft ob eine Aufgabe nicht mindestens ein Jahr in der
+     */
+    @Override
+    protected int removeSelection(OctopusContext cntx, List errors, List selection, TransactionContext context) throws BeanException, IOException {
+       
+        int count = 0;
+        if (selection == null || selection.size() == 0) return count;
+        Database database = context.getDatabase();
+
+        Task task = (Task)database.createBean("Task");
+        Clause clause = Expr.in("pk", selection);
+        Select select = database.getSelectIds(task).where(clause);
+
+        if(!selection.isEmpty()) {
+            try
+            {
+            
+                Map data;
+                for (Iterator it = database.getList(select, context).iterator(); it.hasNext(); ) {
+                    data = (Map)it.next();
+                    task.id = (Integer)data.get("id");
+                    if (removeBean(cntx, task, context)) {
+                        selection.remove(task.id);
+                        count++;
+                    }
+                }
+                context.commit();
+            }
+            catch ( BeanException e )
+            {
+                context.rollBack();
+                throw new BeanException( "Die Aufgabe(n) konnten nicht gelöscht werden.", e );
+            }
+        }
+        return count; 
     }
 }
