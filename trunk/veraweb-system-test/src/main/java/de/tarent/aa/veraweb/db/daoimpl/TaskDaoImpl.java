ea93c5813086d5a873b48bc90f47c4df3c0e29bb
Valentin But <v.but@tarent.de>
2013-04-18 17:05:48 +0000

    reset also related SEQUENCE after deleting all entries from db table 'veraweb.ttask'
    git-svn-id: svn+ssh://evolvis.org/scmrepos/svn/veraweb@500 293e2372-0500-4244-b9aa-80dbbde7b6ea

--- trunk/veraweb-system-test/src/main/java/de/tarent/aa/veraweb/db/daoimpl/TaskDaoImpl.java
+++ trunk/veraweb-system-test/src/main/java/de/tarent/aa/veraweb/db/daoimpl/TaskDaoImpl.java
@@ -1,5 +1,7 @@
 package de.tarent.aa.veraweb.db.daoimpl;
 
+import javax.persistence.Query;
+
 import org.springframework.stereotype.Component;
 
 import de.tarent.aa.veraweb.db.dao.TaskDao;
@@ -8,5 +10,14 @@ import de.tarent.aa.veraweb.db.entity.Task;
 @Component(value = "TaskDao")
 public class TaskDaoImpl extends GenericDaoImpl<Task, Long> implements TaskDao {
 
-
+    @Override
+    public int deleteAll() {
+        int count = super.deleteAll();
+        
+        Query q = em.createNativeQuery("ALTER SEQUENCE veraweb.ttask_pk_seq MINVALUE 1 START 1 RESTART 1");
+        q.executeUpdate();
+        
+        return count;
+        
+    }
 }
