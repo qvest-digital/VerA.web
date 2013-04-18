9313cdfc122cb87e5c787e893573534f076f5271
Hendrik Helwich <h.helwich@tarent.de>
2013-04-18 15:49:02 +0000

    add whenFillMask()
    git-svn-id: svn+ssh://evolvis.org/scmrepos/svn/veraweb/trunk@494 293e2372-0500-4244-b9aa-80dbbde7b6ea

--- veraweb-system-test/src/test/java/de/tarent/aa/veraweb/cucumber/stepdefinitions/FormStepDefinitions.java
+++ veraweb-system-test/src/test/java/de/tarent/aa/veraweb/cucumber/stepdefinitions/FormStepDefinitions.java
@@ -1,6 +1,8 @@
 package de.tarent.aa.veraweb.cucumber.stepdefinitions;
 
+import cucumber.annotation.de.Angenommen;
 import cucumber.annotation.de.Wenn;
+import de.tarent.aa.veraweb.cucumber.data.AufgabeData;
 import de.tarent.aa.veraweb.cucumber.data.LoginData;
 import de.tarent.aa.veraweb.cucumber.utils.NameUtil;
 
@@ -12,8 +14,15 @@ public class FormStepDefinitions extends AbstractStepDefinitions {
         whenFillFields(loginData.valuesForPageFields);
     }
     
+    @Angenommen("^ich fülle die Maske mit \"([^\"]*)\" aus$")
+    public void whenFillMask(String name) throws Throwable {
+    	AufgabeData data = AufgabeData.forName(NameUtil.nameToEnumName(name));
+        whenFillFields(data.valuesForPageFields);
+    }
+    
     @Wenn("^ich die Checkbox zur Aufgabe \"([^\"]+)\" auswähle$")
     public void whenClickCheckboxInTable(String name) {
         
     }
+    
 }
