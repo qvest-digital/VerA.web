4057c3a0b133ae19bebdaaffeea6b1d20c12d803
Patrick Apel <p.apel@tarent.de>
2013-05-10 10:06:13 +0000

    Implementation of LocationDetailWorker and editions of the view template etc.
    git-svn-id: svn+ssh://evolvis.org/scmrepos/svn/veraweb@528 293e2372-0500-4244-b9aa-80dbbde7b6ea

--- trunk/veraweb-war/src/main/javaOld/de/tarent/aa/veraweb/beans/Location.java
+++ trunk/veraweb-war/src/main/javaOld/de/tarent/aa/veraweb/beans/Location.java
@@ -19,16 +19,201 @@
  */
 package de.tarent.aa.veraweb.beans;
 
+import java.text.Collator;
+import java.util.ArrayList;
+import java.util.List;
+
 import de.tarent.octopus.PersonalConfigAA;
 import de.tarent.octopus.beans.BeanException;
 import de.tarent.octopus.server.OctopusContext;
 
-public class Location extends AbstractBean implements OrgUnitDependent {
+public class Location extends AbstractBean implements OrgUnitDependent, Comparable<Location> {
+    
+    /**
+     * PK.
+     */
 	public Integer id;
+	
+	 /**
+     * Organisation unit.
+     */
 	public Integer orgunit;
+	
+	 /**
+     * Name of location.
+     */
 	public String name;
+	
+	 /**
+     * Contact person.
+     */
+	public String contactperson;
+	
+	 /**
+     * address.
+     */
+	public String address;
+	
+    /**
+     * zip code.
+     */
+    public String zip;
+    
+     /**
+     * location.
+     */
+    public String location;
+	
+	 /**
+     * Call number.
+     */
+	public String callnumber;
+	
+	 /**
+     * Fax number.
+     */
+	public String faxnumber;
+	
+	 /**
+     * E-Mail address.
+     */
+	public String email;
+	
+	 /**
+     * Comment.
+     */
+	public String comment;
+	
+	 /**
+     * URL.
+     */
+	public String url;
+	
+	 /**
+     * GPS data.
+     */
+	public String gpsdata;
+	
+	 /**
+     * Room number.
+     */
+	public String roomnumber;
+	
 
-	@Override
+	public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getOrgunit() {
+        return orgunit;
+    }
+
+    public void setOrgunit(Integer orgunit) {
+        this.orgunit = orgunit;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getContactperson() {
+        return contactperson;
+    }
+
+    public void setContactperson(String contactperson) {
+        this.contactperson = contactperson;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCallnumber() {
+        return callnumber;
+    }
+
+    public void setCallnumber(String callnumber) {
+        this.callnumber = callnumber;
+    }
+
+    public String getFaxnumber() {
+        return faxnumber;
+    }
+
+    public void setFaxnumber(String faxnumber) {
+        this.faxnumber = faxnumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getGpsdata() {
+        return gpsdata;
+    }
+
+    public void setGpsdata(String gpsdata) {
+        this.gpsdata = gpsdata;
+    }
+
+    public String getRoomnumber() {
+        return roomnumber;
+    }
+
+    public void setRoomnumber(String roomnumber) {
+        this.roomnumber = roomnumber;
+    }
+    
+
+    public String getZip() {
+        return zip;
+    }
+
+    public void setZip(String zip) {
+        this.zip = zip;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    @Override
     public void verify() throws BeanException {
 		if (name == null || name.length() == 0)
 			addError("Sie müssen einen Namen eingeben.");
@@ -59,5 +244,35 @@ public class Location extends AbstractBean implements OrgUnitDependent {
     @Override
     public void checkWrite(OctopusContext cntx) throws BeanException {
         checkGroup(cntx, PersonalConfigAA.GROUP_PARTIAL_ADMIN);
+    }
+
+
+    public int compareTo(Location location) {
+        int result = 0;     
+        
+        Collator collator = Collator.getInstance(java.util.Locale.GERMAN);
+        collator.setStrength(Collator.SECONDARY);
+        
+        List<Integer> comparedList = new ArrayList<Integer>();
+        
+        comparedList.add(collator.compare(this.address == null ? "" : this.address, location.address == null ? "" : location.address));
+        comparedList.add(collator.compare(this.callnumber == null ? "" : this.callnumber, location.callnumber == null ? "" : location.callnumber));
+        comparedList.add(collator.compare(this.comment == null ? "" : this.comment, location.comment == null ? "" : location.comment));
+        comparedList.add(collator.compare(this.contactperson == null ? "" : this.contactperson, location.contactperson == null ? "" : location.contactperson));
+        comparedList.add(collator.compare(this.email == null ? "" : this.email, location.email == null ? "" : location.email));
+        comparedList.add(collator.compare(this.faxnumber == null ? "" : this.faxnumber, location.faxnumber == null ? "" : location.faxnumber));        
+        comparedList.add(collator.compare(this.gpsdata == null ? "" : this.gpsdata, location.gpsdata == null ? "" : location.gpsdata));
+        comparedList.add(collator.compare(this.location == null ? "" : this.location, location.location == null ? "" : location.location));
+        comparedList.add(collator.compare(this.name == null ? "" : this.name, location.name == null ? "" : location.name));
+        comparedList.add(collator.compare(this.roomnumber == null ? "" : this.roomnumber, location.roomnumber == null ? "" : location.roomnumber));
+        //comparedList.add(collator.compare(this.title, location.title));
+        comparedList.add(collator.compare(this.url == null ? "" : this.url, location.url == null ? "" : location.url));
+        comparedList.add(collator.compare(this.zip == null ? "" : this.zip, location.zip == null ? "" : location.zip));
+        
+        if(comparedList.contains(1) || comparedList.contains(-1)) { //It contains 1 or -1 so it is not equal
+            return 1; //For this implementation it does not matter what value returns. Can be changed for future reasons.
+        }
+        
+        return result;
     }
 }
