04eeb5fa3c9170e1e16aa24642220b529994ab1b
Hendrik Helwich <h.helwich@tarent.de>
2013-05-27 10:29:54 +0000

    fix JS tab bug in IE9
    => $.each() in IE9 wraps strings in "this"
    
    git-svn-id: svn+ssh://evolvis.org/scmrepos/svn/veraweb/trunk@720 293e2372-0500-4244-b9aa-80dbbde7b6ea

--- veraweb-war/src/main/webapp/scripts/vera-tabs.js
+++ veraweb-war/src/main/webapp/scripts/vera-tabs.js
@@ -39,7 +39,7 @@ $(function () { // on DOM ready
      */
     var updateContent = function () {
         // update visibility of content
-        var contentIds = []; // sctive content ids
+        var contentIds = []; // active content ids
         $('[vera-tab].active').each(function () { // iterate active tabs
             contentIds.push($(this).attr(ATTR_TAB)); // add id of current active tab
         });
@@ -47,19 +47,21 @@ $(function () { // on DOM ready
             contentIds.push($(this).val());  // add id of current active selection
         });
         var activeTabContent = $();
-        $('[' + ATTR_CONTENT + ']').each(function () { // iterate all managed content
+        var all = $('[' + ATTR_CONTENT + ']');
+        all.each(function () { // iterate all managed content
             var showContent = true; // true if all ids in the content field are active
-            $.each($(this).attr(ATTR_CONTENT).split(' '), function () {
-                if ($.inArray(this, contentIds) === -1) {  // content field contains an id which is not active
+            var ccids = $(this).attr(ATTR_CONTENT).split(' ');
+            for (var i = 0; i < ccids.length; i += 1) { // do not use $.each here => IE9 jQuery bug
+                if ($.inArray(ccids[i], contentIds) === -1) {  // content field contains an id which is not active
                     showContent = false;
                 }
-            });
+            };
             if (showContent) {
                 activeTabContent = $(this).add(activeTabContent);
             }
         });
         // hide inactive content
-        $('[' + ATTR_CONTENT + ']').not(activeTabContent).hide();
+        all.not(activeTabContent).hide();
         // show active content
         activeTabContent.show();
     };
