07b2a8f1035f375416e57b5fe9ea72a48d3d8781
Valentin But <v.but@tarent.de>
2013-06-24 07:05:37 +0000

    corrected deleting/saving behavior in color view
    git-svn-id: svn+ssh://evolvis.org/scmrepos/svn/veraweb/trunk@1266 293e2372-0500-4244-b9aa-80dbbde7b6ea

--- veraweb-war/src/main/javaOld/de/tarent/aa/veraweb/worker/ColorWorker.java
+++ veraweb-war/src/main/javaOld/de/tarent/aa/veraweb/worker/ColorWorker.java
@@ -109,7 +109,7 @@ public class ColorWorker {
 		Color color3 = (Color)request.getBean("Color", "color3");
 		Color color4 = (Color)request.getBean("Color", "color4");
 		
-		removeColor(database);
+//		removeColor(database);
 		saveColor(database, new Integer(1), color1, errors);
 		saveColor(database, new Integer(2), color2, errors);
 		saveColor(database, new Integer(3), color3, errors);
@@ -135,7 +135,7 @@ public class ColorWorker {
 			if (color.isCorrect()) {
 				database.saveBean(color);
 			} else {
-				database.execute(database.getDelete(color));
+//				database.execute(database.getDelete(color));
 				errors.addAll(color.getErrors());
 			}
 		}
