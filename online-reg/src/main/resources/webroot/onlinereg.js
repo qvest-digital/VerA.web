2447f31b0f9f626403d761e0c555e1ff3004ba9a
Martin Ley <m.ley@tarent.de>
2014-07-22 08:42:28 +0000

    added "login" function
    git-svn-id: svn+ssh://evolvis.org/scmrepos/svn/veraweb/trunk@1582 293e2372-0500-4244-b9aa-80dbbde7b6ea

--- online-reg/src/main/resources/webroot/onlinereg.js
+++ online-reg/src/main/resources/webroot/onlinereg.js
@@ -5,3 +5,39 @@
 $('.navbar-lower').affix({
     offset: {top: 100}
 });
+
+
+
+var onlineRegApp = angular.module('onlineRegApp', [ 'ngRoute', 'ui.bootstrap' ]);
+
+
+
+onlineRegApp.config(function ($routeProvider) {
+
+    $routeProvider.when('/login', {
+        templateUrl: 'partials/login.html',
+        controller: 'LoginController'
+    }).when('/welcome', {
+        templateUrl: 'partials/welcome.html',
+        controller: 'WelcomeController'
+    }).otherwise({
+            redirectTo: '/login'
+        });
+});
+
+onlineRegApp.controller('LoginController', function ($scope, $location) {
+
+
+    $scope.login = function () {
+        console.log("logging in.");
+        $location.path("/welcome");
+    }
+
+
+});
+
+onlineRegApp.controller('WelcomeController', function ($scope) {
+
+
+
+});
