a39999c5bf1f5f7b49a0f9df5617f6fd1352e3db
Martin Ley <m.ley@tarent.de>
2014-07-29 12:21:39 +0000

    added wiremock data, EventResource
    git-svn-id: svn+ssh://evolvis.org/scmrepos/svn/veraweb/trunk@1594 293e2372-0500-4244-b9aa-80dbbde7b6ea

--- online-reg/src/main/java/org/evolvis/veraweb/onlinereg/Config.java
+++ online-reg/src/main/java/org/evolvis/veraweb/onlinereg/Config.java
@@ -1,6 +1,8 @@
 package org.evolvis.veraweb.onlinereg;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.yammer.dropwizard.client.HttpClientConfiguration;
+import com.yammer.dropwizard.client.JerseyClientConfiguration;
 import com.yammer.dropwizard.config.Configuration;
 import com.yammer.dropwizard.db.DatabaseConfiguration;
 import lombok.Data;
@@ -13,7 +15,13 @@ import javax.validation.constraints.NotNull;
 public class Config extends Configuration {
 
     @JsonProperty
-    private String myConfigValue;
+    private String verawebEndpoint;
+
+    @Valid
+    @NotNull
+    @JsonProperty
+    private JerseyClientConfiguration jerseyClientConfiguration = new JerseyClientConfiguration();
+
     @Valid
     @NotNull
     @JsonProperty
