6532538e3c0cf02256ac3faf1007963f3b6f5077
Sevilay Temiz <s.temiz@tarent.de>
2014-08-05 09:01:22 +0000

    js
    git-svn-id: svn+ssh://evolvis.org/scmrepos/svn/veraweb/trunk@1622 293e2372-0500-4244-b9aa-80dbbde7b6ea

---
+++ online-reg/src/test/js/karma.conf.js
@@ -0,0 +1,37 @@
+module.exports = function (config) {
+  config.set({
+    basePath: '../../',
+
+    files: [
+        'main/resources/webroot/jquery-1.10.2.min.js',
+        'main/resources/webroot/angular.min.js',
+        'main/resources/webroot/angular-route.min.js',
+        'main/resources/webroot/ui-bootstrap-tpls.min.js',
+        'main/resources/webroot/moment.min.js',
+        'main/resources/webroot/onlinereg.js',
+        'test/js/lib/angular-mocks.js',
+        'test/js/unit/*.js',
+    ],
+
+    frameworks: ['jasmine'],
+
+    autoWatch: true,
+
+    browsers: ['Firefox'],
+
+    junitReporter: {
+      outputFile: '../target/karma_unit.xml',
+      suite: 'unit'
+    },
+
+    preprocessors: {
+      // ..
+      'js/*.js': 'coverage'         // (1)
+    },
+ 
+    coverageReporter: {
+      type : 'html',                // (2)
+      dir : '../target/karma-coverage'
+    }
+  });
+};
