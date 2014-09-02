7f42e40cada98ed3b938c1767f3b904ec5a3aa0b
Martin Ley <m.ley@tarent.de>
2014-09-02 13:10:17 +0000

    added tests
    git-svn-id: svn+ssh://evolvis.org/scmrepos/svn/veraweb/trunk@1683 293e2372-0500-4244-b9aa-80dbbde7b6ea

---
+++ src/main/java/org/evolvis/veraweb/onlinereg/entities/Config.java
@@ -0,0 +1,28 @@
+package org.evolvis.veraweb.onlinereg.entities;
+
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ * Created by mley on 02.09.14.
+ */
+@Data
+@XmlRootElement
+@Entity
+@Table(name = "tconfig")
+@NamedQueries(value = {
+        @NamedQuery(name = "Config.find", query = "SELECT c FROM Config c where cname = :key")
+})
+public class Config {
+
+    @Id
+    private int pk;
+    private String cname;
+    private String cvalue;
+}
