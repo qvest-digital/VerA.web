eb95205d3140589ac9f2e91d1985ded85c3758ed
Атанас Александров <a.alexandrov@tarent.de>
2014-10-07 06:01:08 +0000

    Pfade angepasst nachdem die Ordnerstruktur geändert wurde
    git-svn-id: svn+ssh://evolvis.org/scmrepos/svn/veraweb/trunk@1724 293e2372-0500-4244-b9aa-80dbbde7b6ea

--- src/test/js/karma.conf.js
+++ src/test/js/karma.conf.js
@@ -3,12 +3,12 @@ module.exports = function (config) {
     basePath: '../../',
 
     files: [
-        'main/resources/webroot/jquery-1.10.2.min.js',
-        'main/resources/webroot/angular.min.js',
-        'main/resources/webroot/angular-route.min.js',
-        'main/resources/webroot/ui-bootstrap-tpls.min.js',
-        'main/resources/webroot/moment.min.js',
-        'main/resources/webroot/onlinereg.js',
+        'main/resources/webroot/scripts/jquery-1.10.2.min.js',
+        'main/resources/webroot/scripts/angular.min.js',
+        'main/resources/webroot/scripts/angular-route.min.js',
+        'main/resources/webroot/scripts/ui-bootstrap-tpls.min.js',
+        'main/resources/webroot/scripts/moment.min.js',
+        'main/resources/webroot/scripts/onlinereg.js',
         'test/js/lib/angular-mocks.js',
         'test/js/unit/*.js',
     ],
