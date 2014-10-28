0e429ee133b34325c8cab65df3f706126b002fa5
Атанас Александров <a.alexandrov@tarent.de>
2014-10-28 13:32:12 +0000

    Filter für Meine Veranstaltungen eingefügt
    git-svn-id: svn+ssh://evolvis.org/scmrepos/svn/veraweb/trunk@1760 293e2372-0500-4244-b9aa-80dbbde7b6ea

--- vw-onlinereg-common/src/main/java/org/evolvis/veraweb/onlinereg/entities/Event.java
+++ vw-onlinereg-common/src/main/java/org/evolvis/veraweb/onlinereg/entities/Event.java
@@ -6,6 +6,8 @@ import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.NamedNativeQueries;
+import javax.persistence.NamedNativeQuery;
 import javax.persistence.NamedQueries;
 import javax.persistence.NamedQuery;
 import javax.persistence.Table;
@@ -21,12 +23,20 @@ import java.util.Date;
 @Table(name = "tevent")
 @NamedQueries({
         @NamedQuery(name = "Event.list", query =
-                "SELECT e FROM Event e " +
-                    "where (CURRENT_TIMESTAMP < e.datebegin OR CURRENT_TIMESTAMP < e.dateend) " +
-                    "AND e.eventtype LIKE 'Offene Veranstaltung'"),
+            "SELECT e FROM Event e " +
+                "where (CURRENT_TIMESTAMP < e.datebegin OR CURRENT_TIMESTAMP < e.dateend) " +
+                "AND e.eventtype LIKE 'Offene Veranstaltung'"),
         @NamedQuery(name = "AllEvents.list", query = "SELECT e FROM Event e"),
         @NamedQuery(name = "Event.getEvent", query = "SELECT e FROM Event e where e.pk = :pk")
 })
+@NamedNativeQueries({
+        @NamedNativeQuery(name="Event.list.userevents", query = "SELECT e.* FROM tevent e " +
+                "JOIN tguest g on g.fk_event = e.pk " +
+                "JOIN tuser tu on tu.pk = g.fk_person " +
+                    "where (CURRENT_TIMESTAMP < e.datebegin OR CURRENT_TIMESTAMP < e.dateend) " +
+                    "AND e.eventtype LIKE 'Offene Veranstaltung' " +
+                    "AND tu.username LIKE :username", resultClass=Event.class)})
+
 public class Event {
 	
     @Id
