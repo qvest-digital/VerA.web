3d6298d26c40a4195c0d029cf6dd5bfa724763a7
Jon Nunez Alvarez <j.nunez-alvarez@tarent.de>
2014-11-26 13:55:40 +0000

    Delegierten registrierung - WIP
    git-svn-id: svn+ssh://evolvis.org/scmrepos/svn/veraweb/trunk@1796 293e2372-0500-4244-b9aa-80dbbde7b6ea

--- src/main/java/org/evolvis/veraweb/onlinereg/event/DelegationResource.java
+++ src/main/java/org/evolvis/veraweb/onlinereg/event/DelegationResource.java
@@ -1,15 +1,26 @@
 package org.evolvis.veraweb.onlinereg.event;
 
 import lombok.extern.java.Log;
+
+import org.evolvis.veraweb.onlinereg.Config;
 import org.evolvis.veraweb.onlinereg.entities.Guest;
+import org.evolvis.veraweb.onlinereg.entities.Person;
+import org.evolvis.veraweb.onlinereg.osiam.OsiamClient;
+import org.hibernate.Query;
+import org.hibernate.Session;
+import org.osiam.resources.scim.User;
+
+import com.sun.jersey.api.client.Client;
+import com.sun.jersey.api.client.WebResource;
 
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -20,16 +31,40 @@ import java.util.List;
 @Log
 public class DelegationResource {
 
+	private Config config;
+    private Client client;
+	
     @GET
     @Path("/{uuid}")
     public List<Guest> showRegisterView(@PathParam("uuid") String uuid) throws IOException {
         return null;
     }
 
-    @GET
+    @POST
     @Path("/{uuid}/register")
-    public List<Guest> registerDelegateForEvent(@PathParam("uuid") String uuid) throws IOException {
-        return null;
+    public String registerDelegateForEvent(@PathParam("uuid") String uuid,@QueryParam("username") String username,
+            @QueryParam("vorname") String vorname,
+            @QueryParam("nachname") String nachname) throws IOException {
+    	
+    	if (!username.matches("\\w+")) {
+            return "INVALID_USERNAME";
+        }
+    	
+    	OsiamClient osiamClient = config.getOsiam().getClient(client);
+        String accessToken = osiamClient.getAccessToken("GET POST");
+    	
+    	User user = osiamClient.getUser(accessToken, username);
+        if (user != null) {
+            return "USER_EXISTS";
+        }
+    	
+    	// TODO Store in tperson
+    	WebResource r = client.resource(config.getVerawebEndpoint() + "/rest/person/");
+        r = r.queryParam("username", username).queryParam("firstname", vorname).queryParam("lastname", nachname);
+        Person person = r.post(Person.class);
+        
+        
+        return "OK";
     }
 
     @GET
