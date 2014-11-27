c36d88f3666621378bf6be569fa1afc71182ca1b
Jon Nunez Alvarez <j.nunez-alvarez@tarent.de>
2014-11-27 16:19:33 +0000

    Renaming calls on Java
    git-svn-id: svn+ssh://evolvis.org/scmrepos/svn/veraweb/trunk@1814 293e2372-0500-4244-b9aa-80dbbde7b6ea

--- src/main/java/org/evolvis/veraweb/onlinereg/event/DelegationResource.java
+++ src/main/java/org/evolvis/veraweb/onlinereg/event/DelegationResource.java
@@ -1,17 +1,7 @@
 package org.evolvis.veraweb.onlinereg.event;
 
-import lombok.extern.java.Log;
-
-import org.evolvis.veraweb.onlinereg.Config;
-import org.evolvis.veraweb.onlinereg.entities.Guest;
-import org.evolvis.veraweb.onlinereg.entities.Person;
-import org.evolvis.veraweb.onlinereg.osiam.OsiamClient;
-import org.hibernate.Query;
-import org.hibernate.Session;
-import org.osiam.resources.scim.User;
-
-import com.sun.jersey.api.client.Client;
-import com.sun.jersey.api.client.WebResource;
+import java.io.IOException;
+import java.util.List;
 
 import javax.ws.rs.FormParam;
 import javax.ws.rs.GET;
@@ -21,8 +11,18 @@ import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
-import java.io.IOException;
-import java.util.List;
+
+import lombok.Getter;
+import lombok.extern.java.Log;
+
+import org.evolvis.veraweb.onlinereg.Config;
+import org.evolvis.veraweb.onlinereg.entities.Guest;
+import org.evolvis.veraweb.onlinereg.entities.Person;
+import org.evolvis.veraweb.onlinereg.osiam.OsiamClient;
+import org.osiam.resources.scim.User;
+
+import com.sun.jersey.api.client.Client;
+import com.sun.jersey.api.client.WebResource;
 
 /**
  * Created by Atanas Alexandrov, tarent solutions GmbH
@@ -34,18 +34,35 @@ public class DelegationResource {
 
 	private Config config;
     private Client client;
-	
-    @GET
+
+    /**
+     * Empty constructor
+     */
+    public DelegationResource() {
+	}
+    
+    /**
+     * Constructor using parameters - required in Main class
+     * 
+     * @param config
+     * @param client
+     */
+    public DelegationResource(Config config, Client client) {
+		super();
+		this.config = config;
+		this.client = client;
+	}
+
+	@GET
     @Path("/{uuid}")
     public List<Guest> showRegisterView(@PathParam("uuid") String uuid) throws IOException {
         return null;
     }
 
-    
-    @Path("/{uuid}/register")
-    public String registerDelegateForEvent(@PathParam("uuid") String uuid,@FormParam("username") String username,
-    		@FormParam("vorname") String vorname,
-    		@FormParam("nachname") String nachname) throws IOException {
+    @POST
+    @Path("/{uuid}/registerdelegiert")
+    public String registerDelegateForEvent(@PathParam("uuid") String uuid,@QueryParam("nachname") String nachname,@QueryParam("username") String username,
+    		@QueryParam("vorname") String vorname) throws IOException {
     	
     	if (!username.matches("\\w+")) {
             return "INVALID_USERNAME";
@@ -60,17 +77,30 @@ public class DelegationResource {
         }
     	
     	// TODO Store in tperson
-    	WebResource r = client.resource(config.getVerawebEndpoint() + "/rest/person/");
-        r = r.queryParam("username", username).queryParam("firstname", vorname).queryParam("lastname", nachname);
-        Person person = r.post(Person.class);
+    	insertIntoTPerson(nachname, username, vorname);
         
         
         return "OK";
     }
 
+
     @GET
     @Path("/{uuid}/remove/{userid}")
     public List<Guest> removeDelegateFromEvent(@PathParam("uuid") String uuid, @PathParam("userid") Long userid) throws IOException {
         return null;
+    }
+    
+    /**
+     * Includes a new person in the database - Table "tperson"
+     * 
+     * @param nachname String
+     * @param username String
+     * @param vorname String
+     */
+    private void insertIntoTPerson(String nachname, String username,
+    		String vorname) {
+    	WebResource r = client.resource(config.getVerawebEndpoint() + "/rest/person/");
+    	r = r.queryParam("username", username).queryParam("firstname", vorname).queryParam("lastname", nachname);
+    	Person person = r.post(Person.class);
     }
 }
