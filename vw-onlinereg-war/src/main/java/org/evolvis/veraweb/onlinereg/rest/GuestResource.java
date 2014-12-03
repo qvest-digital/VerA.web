1a97886a8c549605403c7261164bacd38434b53f
Атанас Александров <a.alexandrov@tarent.de>
2014-12-03 08:57:08 +0000

    Delegationsanmeldung durchführen
    git-svn-id: svn+ssh://evolvis.org/scmrepos/svn/veraweb/trunk@1858 293e2372-0500-4244-b9aa-80dbbde7b6ea

--- vw-onlinereg-war/src/main/java/org/evolvis/veraweb/onlinereg/rest/GuestResource.java
+++ vw-onlinereg-war/src/main/java/org/evolvis/veraweb/onlinereg/rest/GuestResource.java
@@ -26,7 +26,6 @@ import javax.ws.rs.core.MediaType;
 @Produces(MediaType.APPLICATION_JSON)
 public class GuestResource extends AbstractResource{
 
-
     @GET
     @Path("/{eventId}/{userId}")
     public Guest getGuest(@PathParam("eventId") int eventId, @PathParam("userId") int userId) {
@@ -50,13 +49,13 @@ public class GuestResource extends AbstractResource{
             query.setInteger("eventId", eventId);
             query.setInteger("userId", userId);
 
-            Guest g = (Guest) query.uniqueResult();
+            Guest guest = (Guest) query.uniqueResult();
+            guest.setInvitationstatus(invitationstatus);
+            guest.setNotehost(notehost);
 
-            g.setInvitationstatus(invitationstatus);
-            g.setNotehost(notehost);
-            session.update(g);
+            session.update(guest);
             session.flush();
-            return g;
+            return guest;
         } finally {
             session.close();
         }
@@ -67,7 +66,7 @@ public class GuestResource extends AbstractResource{
     public Guest findEventIdByDelegation(@PathParam("uuid") String uuid) {
     	Session session = openSession();
         try {
-            Query query = session.getNamedQuery("Guest.findEventIdByDelegation");
+            Query query = session.getNamedQuery("Guest.findEventIdByDelegationUUID");
             query.setString("uuid", uuid);
             Guest guest = (Guest) query.uniqueResult();
             return guest;
@@ -75,6 +74,7 @@ public class GuestResource extends AbstractResource{
             session.close();
         }
     }
+
     @GET
     @Path("/exist/{uuid}")
     public Boolean existEventIdByDelegation(@PathParam("uuid") String uuid) {
@@ -93,16 +93,16 @@ public class GuestResource extends AbstractResource{
     }
     
     @POST
-    @Path("/{uuid}/einladen")
+    @Path("/{uuid}/register")
     public Guest addGuestToEvent(@PathParam("uuid") String uuid, @QueryParam("eventId") String eventId, @QueryParam("userId") String userId,
     		@QueryParam("invitationstatus") String invitationstatus, @QueryParam("gender") String gender) {
 		Session session = openSession();
 		try { 
-			Guest g = initGuest(uuid,eventId, userId, invitationstatus, gender);
-			session.save(g);
+			Guest guest = initGuest(uuid,eventId, userId, invitationstatus, gender);
+            session.save(guest);
 			session.flush();
 			     
-			return g;
+			return guest;
 		} finally {
 			session.close();
 		}
@@ -112,23 +112,25 @@ public class GuestResource extends AbstractResource{
      * Initialize guest with event information 
      */
     private Guest initGuest(String uuid, String eventId, String userId, String invitationstatus, String gender) {
-        Guest g = new Guest();
-        g.setDelegation(uuid);
-        g.setFk_person(Integer.parseInt(userId));
-        g.setFk_event(Integer.parseInt(eventId));
-        g.setInvitationstatus(Integer.parseInt(invitationstatus));
-        g.setNotehost("");
+        Guest guest = new Guest();
+        guest.setDelegation(uuid);
+        guest.setFk_person(Integer.parseInt(userId));
+        guest.setFk_event(Integer.parseInt(eventId));
+        guest.setInvitationstatus(Integer.parseInt(invitationstatus));
+        guest.setNotehost("");
+        setGender(gender, guest);
+
+        return guest;
+    }
+
+    private void setGender(String gender, Guest guest) {
         if (gender.equalsIgnoreCase("Herr")) {
-        	g.setGender("m");
-        	g.setGender_p("m");
+        	guest.setGender("m");
+        	guest.setGender_p("m");
         }
         else {
-        	g.setGender("w");
-        	g.setGender_p("w");
+        	guest.setGender("w");
+        	guest.setGender_p("w");
         }
-    	
-    	return g;
     }
-    
-    
 }
