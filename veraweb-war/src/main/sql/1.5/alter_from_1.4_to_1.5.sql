09316cbd9d17524266e5525c9ec9ae213b4f42c0
Max Marche <m.marche@tarent.de>
2014-12-03 11:39:18 +0000

    link old and new orgunits with an categorie
    git-svn-id: svn+ssh://evolvis.org/scmrepos/svn/veraweb/trunk@1868 293e2372-0500-4244-b9aa-80dbbde7b6ea

--- veraweb-war/src/main/sql/1.5/alter_from_1.4_to_1.5.sql
+++ veraweb-war/src/main/sql/1.5/alter_from_1.4_to_1.5.sql
@@ -12,6 +12,41 @@ alter table tguest add column delegation varchar(255);
 alter table tguest add column osiam_login varchar(255);
 
 /* ---------------------------------------------------------------------- */
+/* Trigger for link mandant with categorie                                */
+/* ---------------------------------------------------------------------- */
+CREATE FUNCTION linkOrgUnitWithCategorie()
+        RETURNS TRIGGER AS $BODY$
+        BEGIN
+                INSERT INTO veraweb.tcategorie (fk_event, fk_orgunit, catname, flags, rank) VALUES (NULL, NEW.PK, 'Pressevertreter', NULL, NULL);
+                RETURN NEW;
+        END;
+        $BODY$ LANGUAGE plpgsql;
+
+CREATE TRIGGER createCategorieOnUnitInsert
+        AFTER INSERT ON veraweb.torgunit
+        FOR EACH ROW EXECUTE PROCEDURE test();
+        
+/* ---------------------------------------------------------------------- */
+/* Mirgate old OrgUnits                                                   */
+/* ---------------------------------------------------------------------- */        
+CREATE FUNCTION migrateOrgUnits() RETURNS integer AS $BODY$
+	DECLARE
+	    unLinked RECORD;
+	BEGIN
+
+	FOR unLinked IN SELECT pk FROM veraweb.torgunit WHERE pk NOT IN( SELECT fk_orgunit FROM veraweb.tcategorie ) LOOP
+        
+        execute format('INSERT INTO veraweb.tcategorie (fk_event, fk_orgunit, catname, flags, rank) VALUES (NULL, %s, %s, NULL, NULL);', unlinked, quote_literal(E'Pressevertreter'));
+
+    END LOOP;
+
+    RETURN 1;
+END;
+$BODY$ LANGUAGE plpgsql;
+
+SELECT migrateOrgUnits();
+DROP FUNCTION migrateOrgUnits();
+/* ---------------------------------------------------------------------- */
 /* Update schema version                                                  */
 /* ---------------------------------------------------------------------- */
 
