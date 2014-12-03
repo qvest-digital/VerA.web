20285d5e689f80d4d5e046481a5a88c092efb779
Jon Nunez Alvarez <j.nunez-alvarez@tarent.de>
2014-12-03 17:26:12 +0000

    New Resource for press
    git-svn-id: svn+ssh://evolvis.org/scmrepos/svn/veraweb/trunk@1888 293e2372-0500-4244-b9aa-80dbbde7b6ea

---
+++ src/main/java/org/evolvis/veraweb/onlinereg/event/MediaResource.java
@@ -0,0 +1,153 @@
+package org.evolvis.veraweb.onlinereg.event;
+
+import java.io.IOException;
+import java.net.SocketTimeoutException;
+import java.util.List;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
+
+import org.evolvis.veraweb.onlinereg.Config;
+import org.evolvis.veraweb.onlinereg.entities.Guest;
+import org.evolvis.veraweb.onlinereg.entities.Person;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.sun.jersey.api.client.Client;
+import com.sun.jersey.api.client.ClientHandlerException;
+import com.sun.jersey.api.client.UniformInterfaceException;
+import com.sun.jersey.api.client.WebResource;
+
+import lombok.extern.java.Log;
+
+/**
+ * @author jnunez
+ */
+@Path("/media")
+@Produces(MediaType.APPLICATION_JSON)
+@Log
+public class MediaResource {
+
+
+    private static final TypeReference<Boolean> BOOLEAN = new TypeReference<Boolean>() {};
+	
+    /**
+     * Jackson Object Mapper
+     */
+    private final ObjectMapper mapper = new ObjectMapper();
+    private Config config;
+    private Client client;
+    
+    /**
+     * Base path of all resources.
+     */
+    private static final String BASE_RESOURCE = "/rest";
+    
+    public MediaResource() {
+    }
+
+	public MediaResource(Config config, Client client) {
+		super();
+		this.config = config;
+		this.client = client;
+	}
+    
+	@GET
+    @Path("/{uuid}")
+    public List<Person> getGuests(@PathParam("uuid") String uuid) throws IOException {
+		return null;
+    }
+	
+    @POST
+    @Path("/{uuid}/register")
+    public String registerDelegateForEvent(
+            @PathParam("uuid") String uuid,
+            @QueryParam("nachname") String nachname,
+    		@QueryParam("vorname") String vorname,
+            @QueryParam("gender") String gender,
+            @QueryParam("email") String email,
+    		@QueryParam("address") String address,
+    		@QueryParam("plz") String plz,
+    		@QueryParam("city") String city,
+            @QueryParam("country") String country) throws IOException {
+
+        Boolean delegationIsFound = checkForExistingPressEvent(uuid);
+//
+//        if(delegationIsFound) {
+//            return handleDelegationFound(uuid, nachname, vorname, gender);
+//        } else {
+//            return "WRONG_DELEGATION";
+//        }
+        if (delegationIsFound) {
+        	
+        	return "OK";
+        } else {
+        	return "WRONG_EVENT";
+        }
+        
+    }
+	
+    @POST
+    @Path("/{uuid}/remove/{userid}")
+    public List<Guest> removeDelegateFromEvent(@PathParam("uuid") String uuid, @PathParam("userid") Long userid) throws IOException {
+        return null;
+    }
+    
+
+    private Boolean checkForExistingPressEvent(String uuid) throws IOException {
+    	return readResource(path("event","exist", uuid), BOOLEAN);
+    }
+    
+    
+    
+    /**
+     * Reads the resource at given path and returns the entity.
+     *
+     * @param path path
+     * @param type TypeReference of requested entity
+     * @param <T>  Type of requested entity
+     * @return requested resource
+     * @throws IOException
+     */
+    private <T> T readResource(String path, TypeReference<T> type) throws IOException {
+        WebResource resource;
+        try {
+            resource = client.resource(path);
+            String json = resource.get(String.class);
+            return mapper.readValue(json, type);
+        } catch (ClientHandlerException che) {
+            if (che.getCause() instanceof SocketTimeoutException) {
+                //FIXME some times open, pooled connections time out and generate errors
+                log.warning("Retrying request to " + path + " once because of SocketTimeoutException");
+                resource = client.resource(path);
+                String json = resource.get(String.class);
+                return mapper.readValue(json, type);
+            } else {
+                throw che;
+            }
+
+        } catch (UniformInterfaceException uie) {
+            log.warning(uie.getResponse().getEntity(String.class));
+            throw uie;
+        }
+    }
+
+    /**
+     * Constructs a path from vera.web endpint, BASE_RESOURCE and given path fragmensts.
+     *
+     * @param path path fragments
+     * @return complete path as string
+     */
+    private String path(Object... path) {
+        String r = config.getVerawebEndpoint() + BASE_RESOURCE;
+        for (Object p : path) {
+            r += "/" + p;
+        }
+        return r;
+    }
+}
