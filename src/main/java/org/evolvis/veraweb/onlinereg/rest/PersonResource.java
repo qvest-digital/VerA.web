cc0f071d16a735bd4537962ceae15a732badfea6
Jon Nunez Alvarez <j.nunez-alvarez@tarent.de>
2014-12-04 13:12:51 +0000

    Services updated for press site
    git-svn-id: svn+ssh://evolvis.org/scmrepos/svn/veraweb/trunk@1898 293e2372-0500-4244-b9aa-80dbbde7b6ea

--- src/main/java/org/evolvis/veraweb/onlinereg/rest/PersonResource.java
+++ src/main/java/org/evolvis/veraweb/onlinereg/rest/PersonResource.java
@@ -35,6 +35,28 @@ public class PersonResource extends AbstractResource {
             session.close();
         }
     }
+    
+    @POST
+    @Path("/press")
+    public Person createPersonPress(@QueryParam("username") String username,
+                               @QueryParam("firstname") String firstName,
+                               @QueryParam("lastname") String lastname, 
+                               @QueryParam("gender") String gender,
+                               @QueryParam("email") String email,
+                       		   @QueryParam("address") String address,
+                       		   @QueryParam("plz") String plz,
+                       		   @QueryParam("city") String city,
+                               @QueryParam("country") String country) {
+    	
+    	
+        Session session = openSession();
+        try {
+            Person person = handleCreatePersonPress(username, firstName, lastname,gender, email, address, plz, city, country, session);
+            return person;
+        } finally {
+            session.close();
+        }
+    }
 
     @GET
     @Path("/{uuid}")
@@ -56,8 +78,19 @@ public class PersonResource extends AbstractResource {
             // user already exists
             return null;
         }
-
         persistPerson(username, firstName, lastname, session);
+        Person person = (Person) query.uniqueResult();
+        return person;
+    }
+
+    private Person handleCreatePersonPress(String username, String firstName, String lastname, String gender,
+            String email, String address, String plz, String city, String country, Session session) {
+        Query query = getSelectPersonByUsernameQuery(username, session);
+        if (!query.list().isEmpty()) {
+            // user already exists
+            return null;
+        }
+        persistPersonPress(username, firstName, lastname, gender, email, address, plz, city, country, session);
         Person person = (Person) query.uniqueResult();
         return person;
     }
@@ -81,6 +114,31 @@ public class PersonResource extends AbstractResource {
         p.setLastName(lastname);
         p.setUsername(username);
         p.setFk_orgunit(0);
+        return p;
+    }
+
+    private Person persistPersonPress(String username, String firstName, String lastname, String gender,
+            String email, String address, String plz, String city, String country, Session session) {
+        Person p = initPersonPress(username, firstName, lastname, gender, email, address, plz, city, country);
+        session.persist(p);
+        session.flush();
+        return p;
+    }
+
+    private Person initPersonPress(String username, String firstName, String lastname,  String gender,
+            String email, String address, String plz, String city, String country) {
+        Person p = new Person();
+        p.setFirstName(firstName);
+        p.setLastName(lastname);
+        p.setUsername(username);
+        p.setFk_orgunit(0);
+        p.setSex_a_e1(gender);
+        p.setMail_a_e1(email);
+        p.setStreet_a_e1(address);
+        p.setZipcode_a_e1(plz);
+        p.setCity_a_e1(city);
+        p.setCountry_a_e1(country);
+        
         return p;
     }
 
