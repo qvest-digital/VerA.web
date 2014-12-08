e65501ec40f86100d351686e2c677cbd136e2b43
Jon Nunez Alvarez <j.nunez-alvarez@tarent.de>
2014-12-08 16:49:40 +0000

    New entity Category
    git-svn-id: svn+ssh://evolvis.org/scmrepos/svn/veraweb/trunk@1920 293e2372-0500-4244-b9aa-80dbbde7b6ea

---
+++ src/main/java/org/evolvis/veraweb/onlinereg/entities/Category.java
@@ -0,0 +1,44 @@
+package org.evolvis.veraweb.onlinereg.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.NamedNativeQueries;
+import javax.persistence.NamedNativeQuery;
+import javax.persistence.Table;
+import javax.xml.bind.annotation.XmlRootElement;
+
+import lombok.Data;
+
+/**
+ * @author jnunez
+ */
+@Data
+@XmlRootElement
+@Entity
+@Table(name = "tcategorie")
+@NamedNativeQueries({
+	@NamedNativeQuery(name = "Category.findIdByCatname", query = "SELECT c.pk FROM tcategorie c where catname=:pcatname limit 1 ")
+})
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int pk;
+    private String catname;
+    
+	public int getPk() {
+		return pk;
+	}
+	public void setPk(int pk) {
+		this.pk = pk;
+	}
+	public String getCatname() {
+		return catname;
+	}
+	public void setCatname(String catname) {
+		this.catname = catname;
+	}
+    
+}
