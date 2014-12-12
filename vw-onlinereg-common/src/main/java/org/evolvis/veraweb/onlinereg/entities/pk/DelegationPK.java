533c5883d9e5c5287d36be7a722c8beff2db9199
Jon Nunez Alvarez <j.nunez-alvarez@tarent.de>
2014-12-12 11:52:57 +0000

    PK class for hibernate and Delegation.java
    git-svn-id: svn+ssh://evolvis.org/scmrepos/svn/veraweb/trunk@1971 293e2372-0500-4244-b9aa-80dbbde7b6ea

--- vw-onlinereg-common/src/main/java/org/evolvis/veraweb/onlinereg/entities/pk/DelegationPK.java
+++ vw-onlinereg-common/src/main/java/org/evolvis/veraweb/onlinereg/entities/pk/DelegationPK.java
@@ -11,8 +11,21 @@ import javax.persistence.Embeddable;
 @Embeddable()
 public class DelegationPK implements Serializable{
 	
+	/**
+	 * Serial
+	 */
+	private static final long serialVersionUID = 1L;
 	private int fk_guest;
 	private int fk_delegation_field;
+	
+	public DelegationPK() {
+	}
+	
+	public DelegationPK(int fk_guest, int fk_delegation_field) {
+		super();
+		this.fk_guest = fk_guest;
+		this.fk_delegation_field = fk_delegation_field;
+	}
 	
 	public int getFk_guest() {
 		return fk_guest;
