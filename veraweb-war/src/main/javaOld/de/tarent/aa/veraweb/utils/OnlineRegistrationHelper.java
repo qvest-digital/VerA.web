fd03896a40d35d65169e106e1af47a982281d210
Stefan Weiz <s.weiz@tarent.de>
2015-02-27 15:26:08 +0100

    Changes variables and extends java-doc

--- veraweb-war/src/main/javaOld/de/tarent/aa/veraweb/utils/OnlineRegistrationHelper.java
+++ veraweb-war/src/main/javaOld/de/tarent/aa/veraweb/utils/OnlineRegistrationHelper.java
@@ -4,18 +4,21 @@ import de.tarent.octopus.server.OctopusContext;
 
 /**
  * Helper to play with the Online-Application Configuration
- * 
+ *
+ * @Return true = activated
+ * @Return false = deactivated
+ *
  * @author jnunez
  */
 public class OnlineRegistrationHelper {
-	
-	private static String VWOR_PARAM = "vwor.activated";
-	private static String VWOR_VALUE_TRUE = "yes";
-	
+
+	private static final String VWOR_PARAM = "vwor.activated";
+	private static final String VWOR_VALUE_TRUE = "true";
+
 	public static Boolean isOnlineregActive(OctopusContext cntx) {
-		
+
 		String active = cntx.moduleConfig().getParam(VWOR_PARAM);
-		
+
 		if (active != null && VWOR_VALUE_TRUE.equals(active)) {
 			return true;
 		}
