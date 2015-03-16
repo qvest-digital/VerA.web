8e443b7069f4646dc27d7b4315179f45b69dd9d0
Jon Nunez Alvarez <j.nunez-alvarez@tarent.de>
2015-03-16 16:32:43 +0100

    Unit Testing without mockito/without duplicates

--- veraweb-war/src/test/java/de/tarent/aa/veraweb/utils/OnlineRegistrationHelperTest.java
+++ veraweb-war/src/test/java/de/tarent/aa/veraweb/utils/OnlineRegistrationHelperTest.java
@@ -4,9 +4,8 @@ import junit.framework.TestCase;
 import org.junit.Before;
 import org.junit.Test;
 
-public class OnlineRegistrationHelperTest extends TestCase {
 
-    private OnlineRegistrationHelper helper;
+public class OnlineRegistrationHelperTest extends TestCase {
 
     /**
      * At least one digit
@@ -17,16 +16,64 @@ public class OnlineRegistrationHelperTest extends TestCase {
 
     @Before
     public void setUp() {
-        helper = new OnlineRegistrationHelper();
     }
 
     @Test
     public void testGeneratePassword() throws Exception {
         for (int i = 0; i < 1000; i++) {
-            String password = helper.generatePassword();
+            String password = OnlineRegistrationHelper.generatePassword();
             assertEquals(8, password.length());
 
             assertTrue(password.matches(CONDITIONS));
         }
     }
+    
+    @Test
+    public void testNullvaluesUsernameGenerator() throws Exception {
+    	String username = OnlineRegistrationHelper.generateUsername(null, null, null);
+    	assertNull(username);
+    }
+
+    @Test
+    public void testNullFirstname() throws Exception {
+    	String lastname = "Reuter";
+    	String username = OnlineRegistrationHelper.generateUsername(null, lastname, null);
+    	assertNull(username);
+    }
+    
+    @Test
+    public void testNullLastname() throws Exception {
+    	String firstname = "Karen";
+    	String username = OnlineRegistrationHelper.generateUsername(firstname, null, null);
+    	assertNull(username);
+    }
+    
+    @Test
+    public void testGenerateUsernameWithoutNumberAndOneLastname() throws Exception {
+    	String firstname = "Karin";
+    	String lastname = "Schneider";
+    	String username = OnlineRegistrationHelper.generateUsername(firstname, lastname, null);
+    	
+    	assertEquals("kschne", username);
+    }
+    
+    @Test
+    public void testGenerateUsernameWithoutNumberAndTwoLastname() throws Exception {
+    	String firstname = "Javier";
+    	String lastname = "Lopez Casas";
+    	String username = OnlineRegistrationHelper.generateUsername(firstname, lastname, null);
+    	
+    	assertEquals("jlopez", username);
+    }
+
+    @Test
+    public void testGenerateUsernameWithoutNumberAndShortNames() throws Exception {
+    	String firstname = "Lie";
+    	String lastname = "Yin";
+    	String username = OnlineRegistrationHelper.generateUsername(firstname, lastname, null);
+    	
+    	assertEquals("lyin", username);
+    }
+    
+    
 }
