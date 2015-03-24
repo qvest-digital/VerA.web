639008c05663ac36e0bafb2971f2ae9011b4e7fe
Atanas Alexandrov <a.alexandrov@tarent.de>
2015-03-24 13:17:32 +0100

    Story [#171] Entity eingefügt

---
+++ src/main/java/org/evolvis/veraweb/onlinereg/entities/LinkUUID.java
@@ -0,0 +1,59 @@
+package org.evolvis.veraweb.onlinereg.entities;
+
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ * @author Atanas Alexandrov, tarent solutions GmbH
+ */
+
+@Data
+@XmlRootElement
+@Entity
+@Table(name = "tguest")
+@NamedQueries({
+        @NamedQuery(name = "LinkUUID.getUserIdByUUID", query = "SELECT l FROM LinkUUID l where uuid = :uuid"),
+})
+public class LinkUUID {
+    private Long id;
+    private String uuid;
+    private String linktype;
+    private Integer personid;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUuid() {
+        return uuid;
+    }
+
+    public void setUuid(String uuid) {
+        this.uuid = uuid;
+    }
+
+    public String getLinktype() {
+        return linktype;
+    }
+
+    public void setLinktype(String linktype) {
+        this.linktype = linktype;
+    }
+
+    public Integer getPersonid() {
+        return personid;
+    }
+
+    public void setPersonid(Integer personid) {
+        this.personid = personid;
+    }
+}
