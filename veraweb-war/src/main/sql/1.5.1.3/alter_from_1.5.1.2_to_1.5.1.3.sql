07a1ac92c5005d08bff02f1ac3da240f3458e568
Jon Nunez Alvarez <j.nunez-alvarez@tarent.de>
2015-03-24 09:19:23 +0100

    DB-Schema updated - new table 'link_uuid'

---
+++ veraweb-war/src/main/sql/1.5.1.3/alter_from_1.5.1.2_to_1.5.1.3.sql
@@ -0,0 +1,29 @@
+/* ---------------------------------------------------------------------- */
+/* Create sequence "link_uuid_pk_seq"			                          */
+/* ---------------------------------------------------------------------- */
+CREATE SEQUENCE veraweb.link_uuid_pk_seq INCREMENT 1 MINVALUE 1 MAXVALUE 9223372036854775807 START 1 CACHE 1;
+
+/* ---------------------------------------------------------------------- */
+/* Create table "link_uuid"						                          */
+/* ---------------------------------------------------------------------- */
+
+CREATE TABLE veraweb.link_uuid (
+    pk INTEGER DEFAULT nextval('link_uuid_pk_seq') NOT NULL,
+    uuid INTEGER DEFAULT 0,
+    type INTEGER DEFAULT 0,
+    fk_person INTEGER DEFAULT 0,
+    CONSTRAINT link_uuid_pkey PRIMARY KEY (pk)
+) WITH OIDS;
+
+/* ---------------------------------------------------------------------- */
+/* Foreign key constraints                                                */
+/* ---------------------------------------------------------------------- */
+
+ALTER TABLE veraweb.link_uuid ADD CONSTRAINT link_uuid_fkey_person 
+    FOREIGN KEY (fk_person) REFERENCES veraweb.tperson (pk) ON DELETE RESTRICT ON UPDATE RESTRICT;
+
+/* ---------------------------------------------------------------------- */
+/* Update schema version                                                  */
+/* ---------------------------------------------------------------------- */
+
+UPDATE veraweb.tconfig SET cvalue = '2015-03-24' WHERE cname = 'SCHEMA_VERSION';
