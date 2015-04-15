9b25e0c78a1b4a4807b18444d17ae4e45a9a233e
Atanas Alexandrov <a.alexandrov@tarent.de>
2015-04-15 12:55:41 +0200

    Bugfix: Pressekategorie für Mandant doppelt erstellt

--- files/alter_from_1.5.1.3_to_1.5.1.5.1.sql
+++
@@ -1,7 +0,0 @@
--- Remove foreign key constraints (Hotfix)
-
-ALTER TABLE veraweb.tcategorie DROP CONSTRAINT mandant_categ_unique;
-
--- Update schema version
-
-UPDATE veraweb.tconfig SET cvalue = '2015-04-14' WHERE cname = 'SCHEMA_VERSION';
