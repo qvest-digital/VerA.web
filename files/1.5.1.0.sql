400172049b3332eb4a713113ee05d49c05d73ab8
Thorsten Glaser <t.glaser@tarent.de>
2015-04-22 12:47:13 +0200

    both “CREATE OR REPLACE FUNCTION” and “ALTER FUNCTION … OWNER TO” are (should be…) idempotent, so running 1.5.1.0 twice does no harm ⇒ pseudo-bump 1.5.1.0 schema version

--- files/1.5.1.0.sql
+++ files/1.5.1.0.sql
@@ -1,7 +1,3 @@
---
--- WARNING! 1.5.0.4 and 1.5.1.0 have the same SCHEMA_VERSION!
---
-
 /* ---------------------------------------------------------------------- */
 /* Alter table "tdoctype"                                                 */
 /* ---------------------------------------------------------------------- */
@@ -31,4 +27,4 @@ ALTER FUNCTION veraweb.umlaut_fix(character varying) OWNER TO veraweb;
 /* Update schema version                                                  */
 /* ---------------------------------------------------------------------- */
 
-UPDATE veraweb.tconfig SET cvalue = '2015-02-24' WHERE cname = 'SCHEMA_VERSION';
+UPDATE veraweb.tconfig SET cvalue = '2015-02-25' WHERE cname = 'SCHEMA_VERSION';
