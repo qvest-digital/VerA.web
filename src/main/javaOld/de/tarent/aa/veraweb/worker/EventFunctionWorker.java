d73bcf53df71737f93b35e7a80d69858104e27f9
Atanas Alexandrov <a.alexandrov@tarent.de>
2015-04-23 10:40:30 +0200

    Story [#395] W.i.P. Delegationsanmeldung um Funktions-/Amtsbezeichnung und Kategorie erweitern

--- src/main/javaOld/de/tarent/aa/veraweb/worker/EventFunctionWorker.java
+++ src/main/javaOld/de/tarent/aa/veraweb/worker/EventFunctionWorker.java
@@ -1,18 +1,24 @@
 package de.tarent.aa.veraweb.worker;
 
 import de.tarent.aa.veraweb.beans.Event;
+import de.tarent.aa.veraweb.beans.EventDoctype;
+import de.tarent.aa.veraweb.beans.Guest;
 import de.tarent.dblayer.sql.clause.Expr;
 import de.tarent.dblayer.sql.clause.Order;
 import de.tarent.dblayer.sql.statement.Select;
+import de.tarent.octopus.beans.Bean;
 import de.tarent.octopus.beans.BeanException;
 import de.tarent.octopus.beans.Database;
+import de.tarent.octopus.beans.TransactionContext;
 import de.tarent.octopus.beans.veraweb.DatabaseVeraWeb;
 import de.tarent.octopus.beans.veraweb.ListWorkerVeraWeb;
 import de.tarent.octopus.server.OctopusContext;
 import org.apache.log4j.Logger;
 
 import java.io.IOException;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 
 /**
  * @author Atanas Alexandrov, tarent solutions GmbH
@@ -38,6 +44,21 @@ public class EventFunctionWorker extends ListWorkerVeraWeb {
     @Override
     protected void extendWhere(OctopusContext cntx, Select select) throws BeanException {
         select.where(Expr.equal("fk_event", getEvent(cntx).id));
+    }
+
+    @Override
+    protected void saveBean(OctopusContext cntx, Bean bean, TransactionContext context) throws BeanException, IOException {
+
+        Database database = context.getDatabase();
+        super.saveBean(cntx, bean, context);
+        List list =
+                database.getList(
+                        database.getSelectIds(new Guest()).
+                                where(Expr.equal("fk_event", ((EventDoctype)bean).event)), context);
+        GuestWorker worker = WorkerFactory.getGuestWorker(cntx);
+        for (Iterator it = list.iterator(); it.hasNext(); ) {
+            worker.refreshDoctypes(cntx, database, context, (Integer)((Map)it.next()).get("id"));
+        }
     }
 
     private Event getEvent(OctopusContext cntx) {
