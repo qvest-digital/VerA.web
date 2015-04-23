2f8f6c386b08913300510469bd4bdd4b8e530a5c
Atanas Alexandrov <a.alexandrov@tarent.de>
2015-04-23 15:11:03 +0200

    Story [#395] Kategorie kann jetzt hinzugefügt werden

--- files/1.5.1.9.sql
+++ files/1.5.1.9.sql
@@ -1,12 +1,21 @@
--- Create table "tevent_functions"
+-- Create table "tevent_function"
 
 CREATE TABLE veraweb.tevent_function (
   pk serial NOT NULL,
   fk_event int4 NOT NULL DEFAULT 0,
   fk_function int4 NOT NULL DEFAULT 0,
-  CONSTRAINT tevent_doctype_pkey PRIMARY KEY (pk)
+  CONSTRAINT tevent_function_pkey PRIMARY KEY (pk)
+) WITH OIDS;
+
+-- Create table "tevent_category"
+
+CREATE TABLE veraweb.tevent_category (
+  pk serial NOT NULL,
+  fk_event int4 NOT NULL DEFAULT 0,
+  fk_category int4 NOT NULL DEFAULT 0,
+  CONSTRAINT tevent_category_pkey PRIMARY KEY (pk)
 ) WITH OIDS;
 
 -- Update schema version
 
-UPDATE veraweb.tconfig SET cvalue = '2015-04-22' WHERE cname = 'SCHEMA_VERSION';
+UPDATE veraweb.tconfig SET cvalue = '2015-04-23' WHERE cname = 'SCHEMA_VERSION';
---
+++ src/main/javaOld/de/tarent/aa/veraweb/worker/EventCategoryWorker.java
@@ -0,0 +1,58 @@
+package de.tarent.aa.veraweb.worker;
+
+import de.tarent.aa.veraweb.beans.Event;
+import de.tarent.dblayer.sql.clause.Expr;
+import de.tarent.dblayer.sql.clause.Order;
+import de.tarent.dblayer.sql.statement.Select;
+import de.tarent.octopus.beans.Bean;
+import de.tarent.octopus.beans.BeanException;
+import de.tarent.octopus.beans.TransactionContext;
+import de.tarent.octopus.beans.veraweb.ListWorkerVeraWeb;
+import de.tarent.octopus.server.OctopusContext;
+import org.apache.log4j.Logger;
+
+import java.io.IOException;
+
+/**
+ * @author Atanas Alexandrov, tarent solutions GmbH
+ */
+public class EventCategoryWorker extends ListWorkerVeraWeb {
+
+    private static final Logger LOGGER = Logger.getLogger(EventCategoryWorker.class.getCanonicalName());
+
+    public EventCategoryWorker() {
+        super("EventCategory");
+    }
+
+    @Override
+    protected void extendAll(OctopusContext cntx, Select select) throws BeanException, IOException {
+        select.where(Expr.equal("tevent_category.fk_event", getEvent(cntx).id));
+    }
+
+    @Override
+    protected void extendColumns(OctopusContext cntx, Select select) throws BeanException {
+        select.join("veraweb.tcategorie", "tevent_category.fk_category", "tcategorie.pk");
+        select.selectAs("tcategorie.catname", "name");
+        select.orderBy(Order.asc("name"));
+    }
+
+    @Override
+    protected void extendWhere(OctopusContext cntx, Select select) throws BeanException {
+        select.where(Expr.equal("tevent_category.fk_event", getEvent(cntx).id));
+    }
+
+    @Override
+    protected void saveBean(OctopusContext octopusContext, Bean bean, TransactionContext context) {
+        try {
+            super.saveBean(octopusContext, bean, context);
+        } catch (BeanException e) {
+            LOGGER.error("Fehler beim speichern der neuen Kategorie", e);
+        } catch (IOException e) {
+            LOGGER.error("Fehler beim speichern der neuen Kategorie", e);
+        }
+    }
+
+    private Event getEvent(OctopusContext cntx) {
+        return (Event)cntx.contentAsObject("event");
+    }
+}
