dd60356403f12dbec91b950f895e6b5e3056f803
Stefan Weiz <s.weiz@tarent.de>
2015-05-08 09:25:50 +0200

    Refactors Adds java-doc

--- src/main/java/org/evolvis/veraweb/onlinereg/event/DelegationResource.java
+++ src/main/java/org/evolvis/veraweb/onlinereg/event/DelegationResource.java
@@ -83,7 +83,7 @@ public class DelegationResource {
     private static final TypeReference<Boolean> BOOLEAN = new TypeReference<Boolean>() {};
     private static final TypeReference<List<Person>> GUEST_LIST = new TypeReference<List<Person>>() {};
     private static final TypeReference<List<OptionalFieldValue>> FIELDS_LIST = new TypeReference<List<OptionalFieldValue>>() {};
-    private static final TypeReference<List<String>> NAME_LIST = new TypeReference<List<String>>() {};
+    private static final TypeReference<List<String>> CATEGORY_LIST = new TypeReference<List<String>>() {};
     private static final TypeReference<List<String>> FUNCTION_LIST = new TypeReference<List<String>>() {};
 
 
@@ -133,6 +133,14 @@ public class DelegationResource {
 		return readResource(path("person", uuid), GUEST_LIST);
     }
 
+	/**
+	 * Get category names of event
+	 *
+	 * @param uuid
+	 *
+	 * @return List with categories
+	 * @throws IOException
+	 */
 	@GET
     @Path("/fields/list/category/{uuid}")
     public List<String> getCategories(@PathParam("uuid") String uuid) throws IOException {
@@ -140,9 +148,17 @@ public class DelegationResource {
         WebResource resource = client.resource(path("guest", uuid));
         Integer eventId = resource.get(Guest.class).getFk_event();
 
-		return readResource(path("category","fields","list", eventId), NAME_LIST);
+		return readResource(path("category", "fields", "list", eventId), CATEGORY_LIST);
     }
 
+	/**
+	 * Get function names of event
+	 *
+	 * @param uuid
+	 *
+	 * @return List with functions
+	 * @throws IOException
+	 */
 	@GET
     @Path("/fields/list/function/{uuid}")
     public List<String> getFunctions(@PathParam("uuid") String uuid) throws IOException {
@@ -151,7 +167,7 @@ public class DelegationResource {
     	WebResource resource = client.resource(path("guest", uuid));
     	Integer eventId = resource.get(Guest.class).getFk_event();
 
-		return readResource(path("category", "fields","list", "function", eventId), FUNCTION_LIST);
+		return readResource(path("function", "fields", "list", "function", eventId), FUNCTION_LIST);
     }
 
     /**
