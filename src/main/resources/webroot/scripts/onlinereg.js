a157916ece11195fb7505ed296f209f9317e8380
Atanas Alexandrov <a.alexandrov@tarent.de>
2015-05-11 09:28:30 +0200

    Sprachauswahl jetzt auch unter Chrome/Chromium möglich

--- src/main/resources/webroot/scripts/onlinereg.js
+++ src/main/resources/webroot/scripts/onlinereg.js
@@ -92,6 +92,7 @@ onlineRegApp.directive('equals', function() {
 });
 
 onlineRegApp.controller('LanguageSelectController', function ($scope, $translate) {
+  $scope.langKey = 'de_DE';
   $scope.changeLang = function (key) {
 	$translate.use(key).then(function (key) {
 	  console.log("Sprache zu " + key + " gewechselt.");
