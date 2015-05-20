62a3cd7d95c4955f1f32dde7fac4b657e069a54d
Jon Nunez Alvarez <j.nunez-alvarez@tarent.de>
2015-05-20 17:33:30 +0200

    Story [#415] Allowing entities OptionalFieldType('toptional_field_type') and OptionalFieldTypeContent('toptional_field_type_content')

---
+++ src/main/java/org/evolvis/veraweb/onlinereg/entities/OptionalFieldType.java
@@ -0,0 +1,41 @@
+package org.evolvis.veraweb.onlinereg.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.xml.bind.annotation.XmlRootElement;
+
+import lombok.Data;
+
+/**
+ * Class which represents the table toptional_field_type
+ * 
+ * @author jnunez
+ */
+@Data
+@XmlRootElement
+@Entity
+@Table(name = "toptional_field_type")
+public class OptionalFieldType {
+
+	@Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int pk;
+	
+	private String description;
+	
+	public int getPk() {
+		return pk;
+	}
+	
+	public String getDescription() {
+		return description;
+	}
+	
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	
+}
---
+++ src/main/java/org/evolvis/veraweb/onlinereg/entities/OptionalFieldTypeContent.java
@@ -0,0 +1,50 @@
+package org.evolvis.veraweb.onlinereg.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.xml.bind.annotation.XmlRootElement;
+
+import lombok.Data;
+
+/**
+ * Class which represents the table toptional_field_type_content
+ * 
+ * @author jnunez
+ */
+@Data
+@XmlRootElement
+@Entity
+@Table(name = "toptional_field_type_content")
+public class OptionalFieldTypeContent {
+
+	@Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int pk;
+	
+	private Integer fk_optional_field;
+	
+	private String content;
+	
+	public int getPk() {
+		return pk;
+	}
+	
+	public String getContent() {
+		return content;
+	}
+	
+	public void setContent(String content) {
+		this.content = content;
+	}
+	
+	public Integer getFk_optional_field() {
+		return fk_optional_field;
+	}
+	
+	public void setFk_optional_field(Integer fk_optional_field) {
+		this.fk_optional_field = fk_optional_field;
+	}
+}
