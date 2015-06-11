79097225803588c545563502c502c6ec552be4f7
Jon Nunez Alvarez <j.nunez-alvarez@tarent.de>
2015-06-11 10:52:57 +0200

    Correcting type of login_required to int4

--- files/1.5.1.22.sql
+++ files/1.5.1.22.sql
@@ -1,5 +1,5 @@
 -- New flag to allow events without login
-ALTER TABLE veraweb.tevent ADD COLUMN login_required BOOLEAN;
+ALTER TABLE veraweb.tevent ADD COLUMN login_required int4 DEFAULT 0;
 -- New UUID to identify created users to registration
 ALTER TABLE veraweb.tguest ADD COLUMN login_required_uuid CHARACTER VARYING(100);
 
