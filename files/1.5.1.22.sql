2098bea966a626fee3ea038e9e9a2176146242a7
Jon Nunez Alvarez <j.nunez-alvarez@tarent.de>
2015-06-11 09:50:35 +0200

    UUID to allow registration on events without login proccess

--- files/1.5.1.22.sql
+++ files/1.5.1.22.sql
@@ -1,5 +1,7 @@
 -- New flag to allow events without login
 ALTER TABLE veraweb.tevent ADD COLUMN login_required BOOLEAN;
+-- New UUID to identify created users to registration
+ALTER TABLE veraweb.tguest ADD COLUMN login_required_uuid CHARACTER VARYING(100);
 
 -- Update schema version
 UPDATE veraweb.tconfig SET cvalue = '2015-06-11' WHERE cname = 'SCHEMA_VERSION';
