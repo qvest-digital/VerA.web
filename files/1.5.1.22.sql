d8c9023fc677b9d3fcc931d4b15cbad560242f87
Jon Nunez Alvarez <j.nunez-alvarez@tarent.de>
2015-06-11 09:33:33 +0200

    Comments and schema date

--- files/1.5.1.22.sql
+++ files/1.5.1.22.sql
@@ -1,1 +1,5 @@
+-- New flag to allow events without login
 ALTER TABLE veraweb.tevent ADD COLUMN login_required BOOLEAN;
+
+-- Update schema version
+UPDATE veraweb.tconfig SET cvalue = '2015-06-11' WHERE cname = 'SCHEMA_VERSION';
