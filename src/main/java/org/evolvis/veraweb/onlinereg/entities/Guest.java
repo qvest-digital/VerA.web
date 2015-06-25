2847d1b63ebace88a50b5d72b893d2bf1864d9dc
Jon Nunez Alvarez <j.nunez-alvarez@tarent.de>
2015-06-25 14:23:27 +0200

    Bugfixing [#19588] Adding new query to allow registration check to accept

--- src/main/java/org/evolvis/veraweb/onlinereg/entities/Guest.java
+++ src/main/java/org/evolvis/veraweb/onlinereg/entities/Guest.java
@@ -64,9 +64,11 @@ import javax.xml.bind.annotation.XmlRootElement;
                     "WHERE delegation=:uuid AND tperson.iscompany='t'", resultClass=Guest.class),
 	@NamedNativeQuery(name = "Guest.findEventIdByDelegationUUIDandPersonId",
             query ="SELECT g.* FROM tguest g WHERE delegation=:uuid and fk_person=:personId ", resultClass=Guest.class),
-	@NamedNativeQuery(name = "Guest.checkUserRegistration",
-            query = "SELECT COUNT(g.*) FROM tguest g WHERE g.fk_event=:eventId AND g.osiam_login LIKE :username" +
-                    " AND invitationstatus=1 "),
+    @NamedNativeQuery(name = "Guest.checkUserRegistration",
+            query = "SELECT COUNT(g.*) FROM tguest g WHERE g.fk_event=:eventId AND g.osiam_login LIKE :username "),
+    @NamedNativeQuery(name = "Guest.checkUserRegistrationToAccept",
+            query = "SELECT COUNT(g.*) FROM tguest g WHERE g.fk_event=:eventId AND g.osiam_login LIKE :username " +
+                    "AND invitationstatus=1"),
     @NamedNativeQuery(name = "Guest.checkUserRegistrationWithoutLogin",
             query = "SELECT COUNT(g.*) FROM tguest g WHERE g.fk_event=:eventId AND g.login_required_uuid " +
                     "LIKE :noLoginRequiredUUID AND invitationstatus=1 "), // TODO invitation status as parameter?
