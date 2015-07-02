e282d91bd390100bd140b361b2a718ab601c5923
Charbel Saliba <c.saliba@tarent.de>
2015-07-02 13:06:00 +0200

    JPG saved temporarily on OA

--- src/main/java/org/evolvis/veraweb/onlinereg/fileupload/FileUploadResource.java
+++ src/main/java/org/evolvis/veraweb/onlinereg/fileupload/FileUploadResource.java
@@ -1,23 +1,27 @@
 package org.evolvis.veraweb.onlinereg.fileupload;
 
 import com.sun.jersey.api.client.Client;
+
 import lombok.extern.java.Log;
-import org.apache.commons.codec.binary.Base64;
+
 import org.evolvis.veraweb.onlinereg.Config;
 import org.evolvis.veraweb.onlinereg.utils.StatusConverter;
-import org.glassfish.jersey.media.multipart.FormDataParam;
 
-import javax.ws.rs.Consumes;
+import javax.imageio.ImageIO;
+
+import sun.misc.BASE64Encoder;
+import sun.misc.BASE64Decoder;
+
 import javax.ws.rs.FormParam;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayInputStream;
 import java.io.File;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
 
 /**
  * Created by Jon Nuñez, tarent solutions GmbH on 29.06.15.
@@ -47,59 +51,25 @@ public class FileUploadResource {
 
      @POST
      @Path("/save")
-     public String saveTempImage(@FormParam("file") String stringFile) throws IOException {
+     public String saveTempImage(@FormParam("file") String imageString) throws IOException {
+    	 
 
-     OutputStream outputStream = null;
-            byte[] data = Base64.decodeBase64(stringFile);
+    	        BufferedImage image = null;
+    	        byte[] imageByte;
+    	        try {
+    	            BASE64Decoder decoder = new BASE64Decoder();
+    	            imageByte = decoder.decodeBuffer(imageString.substring(23));
+    	            ByteArrayInputStream bis = new ByteArrayInputStream(imageByte);
+    	            image = ImageIO.read(bis);
+    	            bis.close();
+    	        } catch (Exception e) {
+    	            e.printStackTrace();
+    	        } finally {
+    	        	System.out.println("Chech image here");
+    	        }
+    	     File outputfile = new File ("/tmp/comeonbaby.jpg");
+    	     ImageIO.write(image, "jpg", outputfile);
 
-            try (OutputStream stream = new FileOutputStream("/tmp/flowyimage.png")) {
-                stream.write(data);
-            } finally {
-                if (outputStream != null) {
-                    try {
-                        //                     outputStream.flush();
-                        outputStream.close();
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-
-                }
-            }
-
-
-            // write the inputStream to a FileOutputStream
-//            outputStream =
-//                    new FileOutputStream(new File("/tmp/octopus.jpg"));
-//
-//            int read = 0;
-//            byte[] bytes = new byte[1024];
-//
-//            while ((read = file.read(bytes)) != -1) {
-//                outputStream.write(bytes, 0, read);
-//            }
-//
-//            System.out.println("Done!");
-//            return StatusConverter.convertStatus("OK");
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        } finally {
-//            if (file != null) {
-//                try {
-//                    file.close();
-//                } catch (IOException e) {
-//                    e.printStackTrace();
-//                }
-//            }
-//            if (outputStream != null) {
-//                try {
-////                     outputStream.flush();
-//                    outputStream.close();
-//                } catch (IOException e) {
-//                    e.printStackTrace();
-//                }
-//
-//            }
-//        }
             return StatusConverter.convertStatus("FILE_UPLOAD_ERROR");
 
 
