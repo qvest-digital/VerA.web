af212f3c7395bfed86114ef3b0a9491b84726768
Stefan Weiz <s.weiz@tarent.de>
2015-07-10 16:33:54 +0200

    Renames maxwaitlsit to maxreserve

--- files/1.5.1.35.sql
+++ files/1.5.1.35.sql
@@ -1,5 +1,5 @@
 -- New flag to allow events without login
-ALTER TABLE veraweb.tevent ADD COLUMN maxwaitlist int4 DEFAULT 0;
+ALTER TABLE veraweb.tevent ADD COLUMN maxreserve int4 DEFAULT 0;
 
 -- Update schema version
-UPDATE veraweb.tconfig SET cvalue = '2015-06-future' WHERE cname = 'SCHEMA_VERSION';
+UPDATE veraweb.tconfig SET cvalue = '2015-07-xx' WHERE cname = 'SCHEMA_VERSION';
