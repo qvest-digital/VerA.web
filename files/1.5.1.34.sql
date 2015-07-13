230fbabdb8f5008275cb66d2950d5b3e0f17fe23
mirabilos <t.glaser@tarent.de>
2015-07-13 10:16:01 +0200

    finalise 2015-07-10 schema version

--- files/1.5.1.34.sql
+++ files/1.5.1.34.sql
@@ -1,5 +1,11 @@
 -- New flag to allow events without login
 ALTER TABLE veraweb.tevent ADD COLUMN maxreserve int4 DEFAULT 0;
 
+-- New column to identify Guest-Photo
+ALTER TABLE veraweb.tguest ADD COLUMN image_uuid character varying(100);
+
+-- New column to allow max number of reserves
+ALTER TABLE veraweb.tevent ADD COLUMN maxreserve int4 DEFAULT 0;
+
 -- Update schema version
-UPDATE veraweb.tconfig SET cvalue = '2015-07-xx' WHERE cname = 'SCHEMA_VERSION';
+UPDATE veraweb.tconfig SET cvalue = '2015-07-10' WHERE cname = 'SCHEMA_VERSION';
--- files/1.6.25.0.sql
+++
@@ -1,5 +0,0 @@
--- New column to allow max number of reserves
-ALTER TABLE veraweb.tevent ADD COLUMN maxreserve int4 DEFAULT 0;
-
--- Update schema version
-UPDATE veraweb.tconfig SET cvalue = '2015-07-10' WHERE cname = 'SCHEMA_VERSION';
