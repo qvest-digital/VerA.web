84fb938d4357933b4d07659157356a5e9647b8c5
Jon Nunez Alvarez <j.nunez-alvarez@tarent.de>
2015-07-13 13:13:12 +0200

    Story 808 Fixing checks to guest and reserve lists

--- src/main/java/org/evolvis/veraweb/onlinereg/entities/Event.java
+++ src/main/java/org/evolvis/veraweb/onlinereg/entities/Event.java
@@ -47,7 +47,7 @@ import java.util.Date;
         @NamedQuery(name = "Event.getEventByHash", query = "SELECT e FROM Event e where e.hash = :hash")
 })
 @NamedNativeQueries({
-    @NamedNativeQuery(name="Event.list",
+    @NamedNativeQuery(name = "Event.list",
             query = "SELECT DISTINCT tevent.* FROM tevent " +
                     "LEFT JOIN tguest g ON tevent.pk=g.fk_event " +
                     "WHERE (CURRENT_TIMESTAMP < tevent.datebegin OR CURRENT_TIMESTAMP < tevent.dateend) " +
@@ -56,15 +56,25 @@ import java.util.Date;
                     "OR (tevent.maxguest > (SELECT count(pk) FROM tguest WHERE fk_event=tevent.pk AND reserve=0))) " +
                     " OR ((tevent.maxreserve = 0 OR tevent.maxreserve = -1 OR tevent.maxguest IS NULL) " +
                     "OR (tevent.maxreserve > (SELECT count(pk) FROM tguest WHERE fk_event=tevent.pk AND reserve=1))))",
-            resultClass=Event.class),
-    @NamedNativeQuery(name="Event.guestByUUID",
-                      query="SELECT count(e.*) FROM tevent e WHERE mediarepresentatives=:uuid "),
-    @NamedNativeQuery(name="Event.getEventByUUID",
-                      query="SELECT e.pk FROM tevent e where e.mediarepresentatives=:uuid "),
-    @NamedNativeQuery(name="Event.isOpen",
-                      query="SELECT count(e.*) FROM tevent e " +
+            resultClass = Event.class),
+    @NamedNativeQuery(name = "Event.guestByUUID",
+                      query = "SELECT count(e.*) FROM tevent e WHERE mediarepresentatives=:uuid "),
+    @NamedNativeQuery(name = "Event.getEventByUUID",
+                      query = "SELECT e.pk FROM tevent e where e.mediarepresentatives=:uuid "),
+    @NamedNativeQuery(name = "Event.isOpen",
+                      query = "SELECT count(e.*) FROM tevent e " +
                             "WHERE (CURRENT_TIMESTAMP < e.datebegin OR CURRENT_TIMESTAMP < e.dateend) " +
-                            "AND e.eventtype LIKE 'Offene Veranstaltung' AND pk=:eventId")
+                            "AND e.eventtype LIKE 'Offene Veranstaltung' AND pk=:eventId"),
+    @NamedNativeQuery(name = "Event.checkMaxGuestLimit",
+                      query = "SELECT DISTINCT count(*) " +
+                              "FROM veraweb.tevent " +
+                              "WHERE (maxguest <= (SELECT count(*) FROM tguest WHERE fk_event=:eventId " +
+                              "AND reserve = 0)) AND pk=:eventId"),
+    @NamedNativeQuery(name = "Event.checkMaxReserveLimit",
+                      query = "SELECT DISTINCT count(*) " +
+                              "FROM veraweb.tevent " +
+                              "WHERE (maxreserve <= (SELECT count(*) FROM tguest WHERE fk_event=:eventId " +
+                              "AND reserve = 1)) AND pk=:eventId")
 })
 
 public class Event {
