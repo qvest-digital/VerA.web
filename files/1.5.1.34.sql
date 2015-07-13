08adb83b1f42689fd9f3cc23573ff1892259936e
mirabilos <t.glaser@tarent.de>
2015-07-13 10:17:03 +0200

    … erm…

--- files/1.5.1.34.sql
+++ files/1.5.1.34.sql
@@ -4,8 +4,5 @@ ALTER TABLE veraweb.tevent ADD COLUMN maxreserve int4 DEFAULT 0;
 -- New column to identify Guest-Photo
 ALTER TABLE veraweb.tguest ADD COLUMN image_uuid character varying(100);
 
--- New column to allow max number of reserves
-ALTER TABLE veraweb.tevent ADD COLUMN maxreserve int4 DEFAULT 0;
-
 -- Update schema version
 UPDATE veraweb.tconfig SET cvalue = '2015-07-10' WHERE cname = 'SCHEMA_VERSION';
