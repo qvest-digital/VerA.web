7623ecc0a3a699b9c641d0819630be7ea51bd5a8
mirabilos <t.glaser@tarent.de>
2015-10-19 13:54:30 +0200

    let’s just use dates, not versions, for new DB changes; the upgrade script makes it easier now

--- files/upgrade.sql
+++ files/upgrade.sql
@@ -467,23 +467,25 @@ BEGIN
 		INSERT INTO veraweb.tupdate(date, value) VALUES (vdate, vmsg);
 	END IF;
 
-	-- 1.6.31.6
-    	vnewvsn := '2015-10-07';
-    	IF vcurvsn < vnewvsn THEN
-    		vmsg := 'begin.update(1.6.31.6)';
-    		INSERT INTO veraweb.tupdate(date, value) VALUES (vdate, vmsg);
+	-- anything after the old SQL files
 
-    		-- New column to identify Guest-Partner-Photo
-    		ALTER TABLE tguest add column image_uuid_p character varying(100);
+	vnewvsn := '2015-10-07';
+	IF vcurvsn < vnewvsn THEN
+		vmsg := 'begin.update(' || vnewvsn || ')';
+		INSERT INTO veraweb.tupdate(date, value) VALUES (vdate, vmsg);
 
-    		-- post-upgrade 1.6.31.6
-    		vmsg := 'end.update(1.6.31.6)';
-    		UPDATE veraweb.tconfig SET cvalue = vnewvsn WHERE cname = 'SCHEMA_VERSION';
-    		vcurvsn := vnewvsn;
-    		INSERT INTO veraweb.tupdate(date, value) VALUES (vdate, vmsg);
-    	END IF;
+		-- New column to identify Guest-Partner-Photo
+		ALTER TABLE tguest add column image_uuid_p character varying(100);
+
+		-- post-upgrade
+		vmsg := 'end.update(' || vnewvsn || ')';
+		UPDATE veraweb.tconfig SET cvalue = vnewvsn WHERE cname = 'SCHEMA_VERSION';
+		vcurvsn := vnewvsn;
+		INSERT INTO veraweb.tupdate(date, value) VALUES (vdate, vmsg);
+	END IF;
 
 	-- end
+
 	IF vcurvsn <> vversion THEN
 		RAISE WARNING 'Database version after upgrade (%) does not match target (%)',
 		    vcurvsn, vversion
