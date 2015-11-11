042f4c11fbab6c6f4412918cec4ac793e667bead
mirabilos <t.glaser@tarent.de>
2015-11-11 14:18:46 +0100

    Revert "Registry Ports geändert"
    This reverts commit e0f745ee92c210116ab631610c9eacbb8433742c.

--- dockerinstall.sh
+++ dockerinstall.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 set -ex
 cd "$(dirname "$0")"
-docker build -t veraweb-tools.lan.tarent.de/veraweb-rest-api .
-docker push veraweb-tools.lan.tarent.de/veraweb-rest-api
+docker build -t veraweb-tools.lan.tarent.de:5000/veraweb-rest-api .
+docker push veraweb-tools.lan.tarent.de:5000/veraweb-rest-api
