9614aa99b8f3d086c5358a8b3194f1987760fec6
mirabilos <t.glaser@tarent.de>
2015-11-11 14:19:01 +0100

    Revert "Registry Ports geändert"
    This reverts commit cdb7af54b8f5f4244acb1bfec1321842aeea9c87.

--- src/main/webroot-src/js/controllers/kontaktdatenController.js
+++ src/main/webroot-src/js/controllers/kontaktdatenController.js
@@ -1,6 +1,6 @@
 onlineRegApp.controller('KontaktdatenController', function($rootScope, $translate, $location, $scope) {
 if ($rootScope.user_logged_in == null) {
-        $scope.setNextPage('kontaktdaten');
+        $scope.setNextPage('/kontaktdaten');
         $location.path('/login');
     }
 });
