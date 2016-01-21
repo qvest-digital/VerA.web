85b60e8eb0884ab69921344e038844e3d31c139e
Atanas Alexandrov <a.alexandrov@tarent.de>
2016-01-21 14:45:54 +0100

    [#VERA-3] W.i.P. Benutzer aktivieren

--- src/main/java/org/evolvis/veraweb/onlinereg/rest/OsiamUserActivationResource.java
+++ src/main/java/org/evolvis/veraweb/onlinereg/rest/OsiamUserActivationResource.java
@@ -1,11 +1,16 @@
 package org.evolvis.veraweb.onlinereg.rest;
 
 import org.evolvis.veraweb.onlinereg.entities.OsiamUserActivation;
+import org.hibernate.Query;
 import org.hibernate.Session;
 
 import javax.ws.rs.FormParam;
+import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 import java.util.Calendar;
 import java.util.Date;
 
@@ -13,13 +18,14 @@ import java.util.Date;
  * Created by tzimme on 19.01.16.
  */
 @Path("/osiam/user")
+@Produces(MediaType.APPLICATION_JSON)
 public class OsiamUserActivationResource extends AbstractResource {
 
     // FIXME Set the value via properties file
     private static final Integer LINK_VALIDITY_PERIOD_IN_DAYS = 3;
 
     @POST
-    @Path("/activation")
+    @Path("/create")
     public OsiamUserActivation addOsiamUserActivationEntry(@FormParam("username") String username, @FormParam("activation_token") String activationToken) {
         final Session session = openSession();
         try {
@@ -32,6 +38,36 @@ public class OsiamUserActivationResource extends AbstractResource {
         } finally {
             session.close();
         }
+    }
+
+    @POST
+    @Path("/activate")
+    public void removeOsiamUserActivationEntry(@FormParam("osiam_user_activation") String activationToken) {
+        final Session session = openSession();
+        try {
+            final OsiamUserActivation osiamUserActivation = getOsiamUserActivation(activationToken);
+            session.delete(osiamUserActivation);
+            session.flush();
+        } finally {
+            session.close();
+        }
+    }
+
+    @GET
+    @Path("/get/activation/{activation_token}")
+    public OsiamUserActivation getOsiamUserActivation(@PathParam("activation_token") String activationToken) {
+        final Session session = openSession();
+        try {
+            final Query query = session.getNamedQuery("OsiamUserActivation.getOsiamUserActivationEntryByToken");
+            query.setString("activation_token", activationToken);
+            final OsiamUserActivation osiamUserActivation = (OsiamUserActivation) query.uniqueResult();
+            if (osiamUserActivation != null) {
+                return osiamUserActivation;
+            }
+        } finally {
+            session.close();
+        }
+        return null;
     }
 
     private Date getExpirationDate(){
