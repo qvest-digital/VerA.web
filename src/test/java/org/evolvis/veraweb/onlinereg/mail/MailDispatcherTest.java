fef62330a5e6e165776e4b053d4eeddf24f7add3
Max Weierstall <m.weierstall@tarent.de>
2016-02-05 10:46:16 +0100

    added contentType to constructor

--- src/test/java/org/evolvis/veraweb/onlinereg/mail/MailDispatcherTest.java
+++ src/test/java/org/evolvis/veraweb/onlinereg/mail/MailDispatcherTest.java
@@ -10,7 +10,7 @@ public class MailDispatcherTest {
 
     @Test@Ignore
     public void testSend() throws Exception {
-        final EmailConfiguration emailConfiguration = new EmailConfiguration("localhost", 2525, null, null, null, "noreply@tarent.de", "Subject mit Umlat ü", "Plain text content mit Umlaut ü");
+        final EmailConfiguration emailConfiguration = new EmailConfiguration("localhost", 2525, null, null, null, "noreply@tarent.de", "Subject mit Umlat ü", "Plain text content mit Umlaut ü", "HTML");
         final MailDispatcher mailDispatcher = new MailDispatcher(emailConfiguration);
         mailDispatcher.send("from", "to@fa.ke", "Subject mit Umlaut ä", "Plain text content mit Umlaut ä", "http://link.de", emailConfiguration.getContentType());
     }
