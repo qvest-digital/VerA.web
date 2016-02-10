f575803a6d31fae7b828702f3d0bde75476dffbe
Stefan Weiz <s.weiz@tarent.de>
2016-02-10 14:40:46 +0100

    VERA-4 Implements datepicker, standard javascript site actions, get user exists status in account data (W.I.P.)

--- src/main/webroot-src/js/controllers/kontaktdatenController.js
+++ src/main/webroot-src/js/controllers/kontaktdatenController.js
@@ -1,6 +1,21 @@
-onlineRegApp.controller('KontaktdatenController', function($rootScope, $translate, $location, $scope) {
+onlineRegApp.controller('KontaktdatenController', function($http, $rootScope, $translate, $location, $scope) {
 if ($rootScope.user_logged_in == null) {
         $scope.setNextPage('kontaktdaten');
         $location.path('/login');
     }
+
+    $scope.success = null;
+    $scope.error = null;
+    $rootScope.cleanMessages();
+    $scope.setNextPage('kontaktdaten');
+
+    $scope.existsUser = $http.get('/api/user/userdata/existing/' + $rootScope.user_logged_in);
+
+    jQuery(function() {
+        jQuery( "#birthday").datepicker({
+            maxDate: "+0D",
+            changeMonth: true,
+            changeYear: true
+        });
+    });
 });
