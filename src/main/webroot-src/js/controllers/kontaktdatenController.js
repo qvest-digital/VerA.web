ad3df595801aa5ecd17ad0689867c05ccb650b80
Stefan Weiz <s.weiz@tarent.de>
2016-02-15 08:53:07 +0100

    VERA-4 Implements handling, if user is registered to at least one event and datepicker. Prepare saving of user core data (W.I.P.)

--- src/main/webroot-src/js/controllers/kontaktdatenController.js
+++ src/main/webroot-src/js/controllers/kontaktdatenController.js
@@ -6,13 +6,66 @@ if ($rootScope.user_logged_in == null) {
 
     $scope.success = null;
     $scope.error = null;
+    $scope.hasUserEvents = null;
     $rootScope.cleanMessages();
     $scope.setNextPage('kontaktdaten');
 
-    $scope.existsUser = $http.get('/api/user/userdata/existing/' + $rootScope.user_logged_in);
+    $http({
+        method: 'GET',
+        url: '/api/user/userdata/existing/event/' + $rootScope.user_logged_in
+    }).success(function (result) {
+        if(result.status == 'OK') {
+            //fields should be enabled
+            $scope.userHasNoEvents = false
+        } else {
+            //fields should be disabled
+            $scope.userHasNoEvents = true
+        }
+    });
 
-    jQuery(function() {
-        jQuery( "#birthday").datepicker({
+    $scope.updateUserCoreData = function() {
+        $http({
+            method: 'POST',
+            url: 'api/user/userdata/update/' + encodeURIComponent($rootScope.user_logged_in) + '',
+            headers: {"Content-Type" : undefined},
+            data: $.param({
+                person_salutation: $scope.person.salutation,
+                person_title: $scope.person.title,
+                person_firstName: $scope.person.firstName,
+                person_lastName: $scope.person.lastName,
+                person_birthday: $scope.person.birthday,
+                person_nationality: $scope.person.nationality,
+                person_languages: $scope.person.languages,
+                person_gender: $scope.person.gender,
+                current_language: $translate.use()
+            })
+        }).success(function (result) {
+            switch(result.status) {
+                case 'OK':
+                    $scope.status = 1;
+
+                    setStatus = 1;
+
+                    $scope.update = function (parm1, parm2) {
+                        $scope.status = parm1 + ": " + parm2;
+                    };
+
+                    break;
+                default:
+                    $scope.status = 'e';
+            }
+
+            if($scope.status == 1) {
+                $location.path('/login');
+            }
+
+        }).error(function (data, status, headers, config) {
+            $scope.status = 'e';
+        });
+    };
+
+    $(function() {
+        jQuery( ".birthday").datepicker({
             maxDate: "+0D",
             changeMonth: true,
             changeYear: true
