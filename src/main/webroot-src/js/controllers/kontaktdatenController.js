251f4ac2039410f058056621b85c1092bf476251
Stefan Weiz <s.weiz@tarent.de>
2016-02-24 10:46:39 +0100

    VERA-4 Completes saving of core data logic in JavaScript; Implements getting and handling of salutations, gender and date

--- src/main/webroot-src/js/controllers/kontaktdatenController.js
+++ src/main/webroot-src/js/controllers/kontaktdatenController.js
@@ -14,32 +14,28 @@ onlineRegApp.controller('KontaktdatenController', function($http, $rootScope, $t
             url: '/api/user/userdata/existing/event/' + $rootScope.user_logged_in
         }).success(function (result) {
             if (result.status == 'OK') {
-                //fields should be enabled
+                //user has events and fields should be enabled
                 $scope.userHasNoEvents = false
             } else {
-                //fields should be disabled
+                //user has no events and fields should be disabled
                 $scope.userHasNoEvents = true
             }
         });
 
-        $http.get('api/user/userdata/' + encodeURIComponent($rootScope.user_logged_in)).then(function(person) {
-            $scope.person = person.data;
-
-            if($scope.person.birthday_a_e1 && $scope.person.birthday_a_e1 > 0) {
-                //TODO TO DATE!!!!
-                $scope.person.birthday_a_e1;
-            }
-        });
-
         if (!$scope.userHasNoEvents) {
-            //$scope.birthday.$mdDateLocaleProvider.parseDate("de");
-            // moment.locale("de");
+            //Array have to be initialised, to push elements
+            //So you can set the first value and then an array
+            $scope.salutations = [];
 
-            $scope.salutationOptions = [
-                {id: 0, name: "GENERIC_NOT_SELECT"},
-                {id: 1, name: "GENERIC_SALUTATION_MALE"},
-                {id: 2, name: "GENERIC_SALUTATION_FEMALE"}
-            ];
+            $translate('GENERIC_NOT_SELECT').then(function (text) {
+                $scope.salutations.push({'id' : 0, 'name' : text});
+            });
+
+            $http.get('api/kontaktdaten/getallsalutations').then(function(salutationList) {
+                angular.forEach(salutationList.data, function(value) {
+                    $scope.salutations.push({'id' : value.pk, 'name' : value.salutation});
+                });
+            });
 
             $scope.genderOptions = [
                 {id: 0, name: "GENERIC_NOT_SELECT"},
@@ -47,30 +43,54 @@ onlineRegApp.controller('KontaktdatenController', function($http, $rootScope, $t
                 {id: 2, name: "GENERIC_GENDER_FEMALE"}
             ];
 
-            $scope.salutation = $scope.salutationOptions[0];
-            $scope.gender = $scope.genderOptions[0];
+            $http.get('api/user/userdata/' + encodeURIComponent($rootScope.user_logged_in)).then(function(person) {
+                $scope.gender = $scope.genderOptions[0];
+                $scope.salutation = $scope.salutations[0];
 
-            $scope.birthday = new Date();
+                $scope.person = person.data;
 
-            $scope.maxBirthdayDate = new Date($scope.birthday.getFullYear(),
-                $scope.birthday.getMonth(), $scope.birthday.getDate());
+                if ($scope.person.sex_a_e1 == 'm') {
+                    $scope.gender = $scope.genderOptions[1];
+                } else if ($scope.person.sex_a_e1 == 'f') {
+                    $scope.gender = $scope.genderOptions[2];
+                }
 
-            $scope.updateUserCoreData = function () {
+                //Selects the option-value of person's salutation in the template
+                angular.forEach($scope.salutations, function(value) {
+                    if(value.name == $scope.person.salutation_a_e1) {
+                        $scope.salutation = $scope.salutations[value.id];
+                    }
+                });
+
+                if($scope.person.birthday_a_e1 && $scope.person.birthday_a_e1 > 0) {
+                    //Cast epoch to Date
+                    $scope.person.birthday_a_e1 = new Date($scope.person.birthday_a_e1);
+                }
+            });
+
+            //Sets the max selectable date to "today"
+            //Moments.js doesn't work here
+            $scope.maxBirthdayDate = new Date();
+            $scope.maxBirthdayDate = new Date($scope.maxBirthdayDate.getFullYear(), $scope.maxBirthdayDate.getMonth(),
+                $scope.maxBirthdayDate.getDate());
+
+            $scope.update_user_core_data = function () {
                 $http({
                     method: 'POST',
                     url: 'api/user/userdata/update/' + encodeURIComponent($rootScope.user_logged_in) + '',
                     headers: {"Content-Type": undefined},
                     data: $.param({
-                        person_salutation: $scope.salutation,
-                        person_title: $scope.title,
-                        person_firstName: $scope.firstName,
-                        person_lastName: $scope.lastName,
-                        person_birthday: $scope.birthday,
-                        person_nationality: $scope.nationality,
-                        person_languages: $scope.languages,
-                        person_gender: $scope.gender
+                        person_fk_salutation: $scope.salutation.id,
+                        person_salutation: $scope.salutation.name,
+                        person_title: $scope.person.title_a_e1,
+                        person_firstName: $scope.person.firstname_a_e1,
+                        person_lastName: $scope.person.lastname_a_e1,
+                        person_birthday: $scope.person.birthday_a_e1,
+                        person_nationality: $scope.person.nationality_a_e1,
+                        person_languages: $scope.person.languages_a_e1,
+                        person_gender: $scope.gender.id
                     })
-                    //This stuff left (delegationController for example)
+                    //success/error stuff left (delegationController for example)
                 }).success(function (result) {
                     switch (result.status) {
                         case 'OK':
