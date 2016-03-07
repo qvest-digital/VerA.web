5c382ddb41318a7223ff644cbdb33d21441897ec
Atanas Alexandrov <a.alexandrov@tarent.de>
2016-03-07 16:46:10 +0100

    Code-Review mit tglase

--- files/cli-import.sh
+++ files/cli-import.sh
@@ -2,75 +2,92 @@
 # Import person script for VerA.web
 
 usage() {
-	echo -e "========================================================================================================================================"
-	echo    "|                                                 Importscript für VerA.web                                                             |"
-	echo    "|                                                                                                                                       |"
-	echo    "|                                                 WICHTIG:                                                                              |"
-	echo    "|                                                  - Im Dateinamen der Importdatei darf kein Leerzeichen vorkommen                      |"
-	echo    "|                                                  - Das Zielinstanz muss am Ende / enthalten, z.B. https://host/veraweb/               |"
-    echo    "========================================================================================================================================"
-    echo -e "\nBenutzung: $0 [-u <BENUTZERNAME>] [-p <PASSWORT>] [-f <IMPORT_DATEI>] [-n <IMPORT_NAME>] [-m <MANDANT_ID>] [-i <VERAWEB_INSTANZ>]\n"
-	echo -e "Beispiel: $0 -u admin -p geheim -f datei.csv -n testname -m 2 -i https://host/veraweb/\n" 1>&2; exit 1; 
+	echo "================================================================================================"
+	echo "|         Importscript für VerA.web                                                             |"
+	echo "|                                                                                               |"
+	echo "|         WICHTIG:                                                                              |"
+	echo "|          - Im Dateinamen der Importdatei darf kein Leerzeichen vorkommen                      |"
+    echo "================================================================================================"
+    echo
+    echo "Benutzung: $0 [-u <BENUTZERNAME>] [-p <PASSWORT>] [-f <IMPORT_DATEI>] [-n <IMPORT_NAME>] [-m <MANDANT_ID>] [-i <VERAWEB_INSTANZ>]"
+	echo
+	echo "Beispiel: $0 -u admin -p geheim -f datei.csv -n testname -m 2 -i https://host/veraweb/"
+    echo
+    exit 1
 }
 
 start_import() {
-	TEMP_OUTPUT=/tmp/veraweb-import-cli.log
-	rm -f $TEMP_OUTPUT
-    curl -s -F username=$1 -F password=$2 --form "importfile=@$3" --form importSource=$4 --form format=formatCSV --form targetOrgUnit=$5 $6do/ImportPersonsFile > $TEMP_OUTPUT  
-	TOTAL_ENTRIES="$(cat $TEMP_OUTPUT | grep 'Zu importierende Datensätze insgesamt' | grep -Eo '[0-9]{1,10}<' | grep -Eo '[0-9]{1,10}')"
-	IMPORT_ID="$(cat $TEMP_OUTPUT | grep 'Import-ID' | grep -Eo '[0-9]{1,10}')"
-	DUPLICATE_ENTRIES="$(cat $TEMP_OUTPUT | grep 'Davon Dubletten' | grep -Eo '[0-9]{1,10}<' | grep -Eo '[0-9]{1,10}')"
+	if ! TEMP_OUTPUT=$(mktemp); then
+		echo >&2 "Konnte temporäre Datei nicht anlegen"
+		exit 1
+	fi
+    curl -s -F username=$1 -F password=$2 --form "importfile=@$3" --form importSource=$4 --form format=formatCSV --form targetOrgUnit=$5 $6do/ImportPersonsFile > "$TEMP_OUTPUT"  
+	TOTAL_ENTRIES="$(<"$TEMP_OUTPUT" grep 'Zu importierende Datensätze insgesamt' | grep -Eo '[0-9]{1,10}<' | grep -Eo '[0-9]{1,10}')"
+	IMPORT_ID="$(<"$TEMP_OUTPUT" grep 'Import-ID' | grep -Eo '[0-9]{1,10}')"
+	DUPLICATE_ENTRIES="$(<"$TEMP_OUTPUT" grep 'Davon Dubletten' | grep -Eo '[0-9]{1,10}<' | grep -Eo '[0-9]{1,10}')"
 	echo "Insgesamt $TOTAL_ENTRIES Datensatz/Datensätze vorhanden"
 	echo "Starte Importvorgang Nummer $IMPORT_ID..."
 	echo "Suche nach Duplicate gestartet..."
 	echo "Es wurde $DUPLICATE_ENTRIES Duplikate gefunden..."
-	if [ "$DUPLICATE_ENTRIES" -ne 0 ]; then
+	if [[ $DUPLICATE_ENTRIES -ne 0 ]]; then
 	  echo "Duplikate wurden gefunden, diese werden nun übernommen..."  
-	  ids=($(curl -s -F username=$1 -F password=$2 --form "importfile=@$3" --form importSource=asds --form format=formatCSV --form importId=$IMPORT_ID --form limit=9999999 $6do/ImportPersonsEditDuplicates | grep '\-id" v' | grep -Eo '>[0-9]{1,10}<' | grep -Eo '[0-9]{1,10}'))
+	  ids=($(curl -s -F username=$1 -F password=$2 --form "importfile=@$3" \
+		--form importSource=asds --form format=formatCSV \
+		--form "importId=$IMPORT_ID" --form limit=9999999 \
+		$6do/ImportPersonsEditDuplicates | \
+		grep '\-id" v' | \
+		grep -Eo '>[0-9]{1,10}<' | \
+		grep -Eo '[0-9]{1,10}'))
 		
 	  for id in "${ids[@]}"
 	  do
 		echo Importiere die Person mit ID ${id}
-		POSTString="--form selectNone=true --form save=Als+Dublette+%C3%BCbernehmen --form toggleAllSelect=on --form list=$id --form $id-id=$id --form $id-select=true"
-		curl -s -F username=$1 -F password=$2 --form importId=$IMPORT_ID $POSTString $6do/ImportPersonsEditDuplicates > /dev/null
-		curl -s -F username=$1 -F password=$2 --form importId=$IMPORT_ID $6do/ImportPersonsFinalise > /dev/null
+		curl -s -F "username=$1" -F "password=$2" --form "importId=$IMPORT_ID" \
+			--form selectNone=true --form save=Als+Dublette+%C3%BCbernehmen \
+			--form toggleAllSelect=on --form "list=$id" --form "$id-id=$id" \
+			--form "$id-select=true" "$6do/ImportPersonsEditDuplicates" > /dev/null
+		curl -s -F username=$1 -F password=$2 --form "importId=$IMPORT_ID" $6do/ImportPersonsFinalise > /dev/null
 	  done
 	else
 	  echo "Keine Duplikate vorhanden, importiere die Datensätze..."
-	  curl -s -F username=$1 -F password=$2 --form importId=$IMPORT_ID $6do/ImportPersonsFinalise > /dev/null
+	  curl -s -F username=$1 -F password=$2 --form "importId=$IMPORT_ID" $6do/ImportPersonsFinalise > /dev/null
 	fi
 	echo "Fertig"
+	rm -f "$TEMP_OUTPUT"
 }
 
 while getopts "u:p:f:n:m:i:" opt; do
-  case "${opt}" in
-        u)
-            u=${OPTARG}
-            ;;
-        p)
-            p=${OPTARG}
-            ;;
-        f)
-            f=${OPTARG}
-            ;;
-        n)
-            n=${OPTARG}
-            ;;
-        m)
-            m=${OPTARG}
-            ;;
-        i)
-            i=${OPTARG}
-            ;;
-        *)
-            usage
-            ;;
+  case $opt in
+  (u)
+  	u=$OPTARG
+  	;;
+  (p)
+  	p=$OPTARG
+  	;;
+  (f)
+  	f=$OPTARG
+  	;;
+  (n)
+  	n=$OPTARG
+  	;;
+  (m)
+  	m=$OPTARG
+  	;;
+  (i)
+  	i=$OPTARG
+    case $i in
+	(*/) ;;
+	(*) i=$i/ ;;
+	esac
+  	;;
+  (*)
+  	usage
+  	;;
   esac
 done
 shift $((OPTIND-1))
 
-if [ -z "${u}" ] || [ -z "${p}" ] || [ -z "${f}" ] || [ -z "${n}" ] || [ -z "${m}" ] || [ -z "${i}" ]; then
+if [[ -z $u || -z $p || -z $f || -z $n || -z $m || -z $i ]]; then
     usage
-else
-	start_import ${u} ${p} ${f} ${n} ${m} ${i}
 fi
+start_import "$u" "$p" "$f" "$n" "$m" "$i"
