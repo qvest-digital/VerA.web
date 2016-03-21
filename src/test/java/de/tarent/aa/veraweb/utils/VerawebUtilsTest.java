3f0aed97a9f433123eeac306eab65708920f46a4
Atanas Alexandrov <a.alexandrov@tarent.de>
2016-03-21 11:15:14 +0100

    [VERA-157] Test für das säubern der Keywords eingefügt

---
+++ src/test/java/de/tarent/aa/veraweb/utils/VerawebUtilsTest.java
@@ -0,0 +1,20 @@
+package de.tarent.aa.veraweb.utils;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * @author Atanas Alexandrov, tarent solutions GmbH
+ */
+public class VerawebUtilsTest {
+
+    @Test
+    public void testClearCommaSeparatedString() throws Exception {
+        // WHEN
+        final String result = VerawebUtils.clearCommaSeparatedString("a,   b,   ,   d");
+
+        // THEN
+        assertEquals("a,b,d", result);
+    }
+}
