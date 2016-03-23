cd280c77909eb093a9217d8382f6e709be1dd517
Atanas Alexandrov <a.alexandrov@tarent.de>
2016-03-23 14:04:50 +0100

    [VERA-203] Gästeliste nach Schlüsselwörter filtern - Code review

--- src/test/java/de/tarent/aa/veraweb/beans/GuestSearchTest.java
+++ src/test/java/de/tarent/aa/veraweb/beans/GuestSearchTest.java
@@ -13,11 +13,16 @@ public class GuestSearchTest {
 
     @Test
     public void test() {
+        // GIVEN
         search = new GuestSearch();
         search.event=42;
         search.keywords="gröfatz, knørßt   , baz , ";
         WhereList where = new WhereList();
+
+        // WHEN
         search.addGuestListFilter(where);
+
+        // THEN
         assertEquals("(tguest.fk_event=42 AND tguest.keywords LIKE '%gröfatz%' AND tguest.keywords LIKE '%knørßt%' AND tguest.keywords LIKE '%baz%')",where.clauseToString());
     }
 
