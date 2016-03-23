18277692d2383b3b562edcde4a7d681da13764b5
Lukas Degener <l.degener@tarent.de>
2016-03-23 13:49:29 +0100

    [VERA 203] trigger guest filter for keywords
    Search result is updated when the value of the keywords changes.
    This is done by an asynchronous request, i.e. the page does not
    reload.

---
+++ src/main/webapp/scripts/guest-filter-auto-commit.js
@@ -0,0 +1,27 @@
+$(function() {
+  var form = $("#guestListFilter");
+  var action = form.attr("action");
+  var doSubmit = function() {
+    $.post(action, form.serialize()) //Serialize looks good name=textInNameInput&&telefon=textInPhoneInput---etc
+      .done(function(data) {
+        var html = $.parseHTML(data);
+        $("#formlist").replaceWith($(html).find("#formlist"));
+        $("#guestSummary").replaceWith($(html).find("#guestSummary"));
+      });
+    return false;
+  };
+  //based on an example from the Bacon.js site
+
+  var text = $("#search-keywords")
+    .asEventStream('input')
+    .debounce(300)
+    .map(function(event) {
+      return event.target.value;
+    })
+    .skipDuplicates()
+    .onValue(function() {
+      //form.get(0).submit();
+      doSubmit();
+    });
+
+});
