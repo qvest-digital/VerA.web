aa4f5115c31e58480a0b681b2a9945af64076893
Jens Oberender <j.oberender@tarent.de>
2016-03-24 13:35:06 +0100

    [VERA-155] Added PersonMailinglist entity.

---
+++ src/main/java/org/evolvis/veraweb/onlinereg/entities/PersonMailinglist.java
@@ -0,0 +1,32 @@
+package org.evolvis.veraweb.onlinereg.entities;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+import javax.xml.bind.annotation.XmlRootElement;
+
+import lombok.Data;
+
+@Data
+@XmlRootElement
+@Entity
+@Table(name = "tperson_mailinglist")
+@NamedQueries(value = { @NamedQuery(name = "PersonMailinglist.findByMailinglist", query = "SELECT p FROM PersonMailinglist p where mailinglistId = :listId"), })
+public class PersonMailinglist {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int pk;
+    @ManyToOne
+    @JoinColumn(name = "fk_person", nullable = false)
+    private Person person;
+    @Column(name = "fk_mailinglist")
+    private int mailinglistId;
+    private String address;
+}
