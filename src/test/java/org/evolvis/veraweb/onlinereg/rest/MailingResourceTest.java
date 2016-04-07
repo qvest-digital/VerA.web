900c735cd81fd634d53e281cb6521f740629acdc
Atanas Alexandrov <a.alexandrov@tarent.de>
2016-04-07 07:53:29 +0200

    Test erweitert

--- src/test/java/org/evolvis/veraweb/onlinereg/rest/MailingResourceTest.java
+++ src/test/java/org/evolvis/veraweb/onlinereg/rest/MailingResourceTest.java
@@ -12,19 +12,25 @@ import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
+import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.apache.commons.io.FileUtils;
 import org.evolvis.veraweb.onlinereg.entities.Person;
 import org.evolvis.veraweb.onlinereg.entities.PersonMailinglist;
 import org.evolvis.veraweb.onlinereg.mail.EmailConfiguration;
 import org.evolvis.veraweb.onlinereg.mail.MailDispatchMonitor;
 import org.evolvis.veraweb.onlinereg.mail.MailDispatcher;
+import org.glassfish.jersey.media.multipart.BodyPartEntity;
 import org.glassfish.jersey.media.multipart.FormDataBodyPart;
+import org.glassfish.jersey.media.multipart.FormDataContentDisposition;
 import org.glassfish.jersey.media.multipart.FormDataMultiPart;
 import org.hibernate.Query;
 import org.hibernate.Session;
@@ -94,31 +100,49 @@ public class MailingResourceTest {
     }
 
     @Test
-    public void testUploadFile() throws MessagingException {
+    public void testUploadFile() throws MessagingException, IOException {
         // GIVEN
         prepareSession();
-        FormDataBodyPart formDataBodyPart1 = mock(FormDataBodyPart.class);
-        FormDataBodyPart formDataBodyPart2 = mock(FormDataBodyPart.class);
-        FormDataBodyPart formDataBodyPart3 = mock(FormDataBodyPart.class);
+        final MailDispatcher mailDispatcher = mock(MailDispatcher.class);
+        objectToTest.setMailDispatcher(mailDispatcher);
+        objectToTest.setEmailConfiguration(mock(EmailConfiguration.class));
+        final FormDataBodyPart formDataBodyPart1 = mock(FormDataBodyPart.class);
+        final FormDataBodyPart formDataBodyPart2 = mock(FormDataBodyPart.class);
+        final FormDataBodyPart formDataBodyPart3 = mock(FormDataBodyPart.class);
+        final FormDataBodyPart formDataBodyPart4 = mock(FormDataBodyPart.class);
+        final FormDataBodyPart formDataBodyPart5 = mock(FormDataBodyPart.class);
+        final Query query = mock(Query.class);
+        final MailDispatchMonitor mailDispatchMonitor = mock(MailDispatchMonitor.class);
+        final BodyPartEntity entity1 = mock(BodyPartEntity.class);
+        final BodyPartEntity entity2 = mock(BodyPartEntity.class);
+        final List<FormDataBodyPart> listFormDataBodyParts = new ArrayList<>();
+        listFormDataBodyParts.add(formDataBodyPart4);
+        listFormDataBodyParts.add(formDataBodyPart5);
+        final PersonMailinglist mailinglist = new PersonMailinglist();
+        mailinglist.setAddress("test@test");
+        mailinglist.setMailinglistId(1);
+        mailinglist.setPerson(mock(Person.class));
+        mailinglist.setPk(1);
+        final List ids = new ArrayList();
+        ids.add(mailinglist);
+        FormDataContentDisposition fdcd1 = mock(FormDataContentDisposition.class);
+        FormDataContentDisposition fdcd2 = mock(FormDataContentDisposition.class);
         when(formData.getField("mail-subject")).thenReturn(formDataBodyPart1);
         when(formData.getField("mail-text")).thenReturn(formDataBodyPart2);
         when(formData.getField("mailinglist-id")).thenReturn(formDataBodyPart3);
         when(formDataBodyPart1.getEntityAs(String.class)).thenReturn("subject");
         when(formDataBodyPart2.getEntityAs(String.class)).thenReturn("text");
         when(formDataBodyPart3.getEntityAs(String.class)).thenReturn("1");
-        Query query = mock(Query.class);
+        when(formData.getFields("files")).thenReturn(listFormDataBodyParts);
+        when(formDataBodyPart4.getFormDataContentDisposition()).thenReturn(fdcd1);
+        when(formDataBodyPart5.getFormDataContentDisposition()).thenReturn(fdcd2);
+        when(fdcd1.getFileName()).thenReturn("abcd");
+        when(fdcd2.getFileName()).thenReturn("efgh");
+        when(formDataBodyPart4.getEntity()).thenReturn(entity1);
+        when(formDataBodyPart5.getEntity()).thenReturn(entity2);
+        when(entity1.getInputStream()).thenReturn(new ByteArrayInputStream("file1".getBytes(StandardCharsets.UTF_8)));
+        when(entity2.getInputStream()).thenReturn(new ByteArrayInputStream("file2".getBytes(StandardCharsets.UTF_8)));
         when(session.getNamedQuery("PersonMailinglist.findByMailinglist")).thenReturn(query);
-        final PersonMailinglist e = new PersonMailinglist();
-        e.setAddress("test@test");
-        e.setMailinglistId(1);
-        e.setPerson(mock(Person.class));
-        e.setPk(1);
-        List ids = new ArrayList();
-        ids.add(e);
-        final MailDispatcher mailDispatcher = mock(MailDispatcher.class);
-        objectToTest.setMailDispatcher(mailDispatcher);
-        objectToTest.setEmailConfiguration(mock(EmailConfiguration.class));
-        MailDispatchMonitor mailDispatchMonitor = mock(MailDispatchMonitor.class);
         when(mailDispatcher.sendEmailWithAttachments(any(String.class), any(String.class), any(String.class), any(String.class), any(Map.class) )).thenReturn(mailDispatchMonitor);
         when(query.list()).thenReturn(ids);
 
