44074ac16ba18043241a78db2cd4bde7f0b5a8bf
Max Weierstall <m.weierstall@tarent.de>
2016-04-20 16:36:04 +0200

    initial unit test

--- src/main/java/org/evolvis/veraweb/export/App.java
+++ src/main/java/org/evolvis/veraweb/export/App.java
@@ -4,10 +4,15 @@ package org.evolvis.veraweb.export;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App{
+
+    String text;
+
+    public App(String text) {
+        this.text = text;
+    }
+
+    public String print(){
+        return text.trim();
     }
 }
