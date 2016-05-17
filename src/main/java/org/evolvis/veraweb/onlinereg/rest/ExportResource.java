c3959106febc9a7373a5e64f9d36921fc5446ff5
Max Weierstall <m.weierstall@tarent.de>
2016-05-17 18:32:42 +0200

    W.I.P. [VERA-255] Resource for the CSV Exporter gibt Datei zurück

--- src/main/java/org/evolvis/veraweb/onlinereg/rest/ExportResource.java
+++ src/main/java/org/evolvis/veraweb/onlinereg/rest/ExportResource.java
@@ -6,28 +6,42 @@ import javax.naming.Context;
 import javax.naming.InitialContext;
 import javax.naming.NamingException;
 import javax.sql.DataSource;
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
-import javax.ws.rs.core.MediaType;
+import javax.ws.rs.*;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.StreamingOutput;
+
+import java.io.*;
+
 
 /**
  * Created by mweier on 23.03.16.
  */
 @Path("/export")
-@Produces(MediaType.TEXT_PLAIN)
+@Produces("text/csv")
 public class ExportResource extends AbstractResource{
 
     @GET
     @Path("/guestList/{eventId}")
-    public DataSource getGuestList() throws NamingException {
+    public Response getGuestList(@PathParam("eventId") final int eventId) throws NamingException, UnsupportedEncodingException {
 
-        InitialContext initContext = new InitialContext();
-        Context envContext  = (Context)initContext.lookup("java:comp/env");
-        DataSource dataSource = (DataSource)envContext.lookup("jdbc/vwonlinereg");
+        final InitialContext initContext = new InitialContext();
+        final Context envContext  = (Context)initContext.lookup("java:comp/env");
+        final DataSource dataSource = (DataSource)envContext.lookup("jdbc/vwonlinereg");
 
-//        CsvExporter csvExporter = new CsvExporter(writer, dataSource);
+        StreamingOutput stream = new StreamingOutput() {
+            @Override
+            public void write(OutputStream os) throws IOException, WebApplicationException {
+                Writer writer = new BufferedWriter(new OutputStreamWriter(os));
 
-        return dataSource;
+                CsvExporter csvExporter = new CsvExporter(writer, dataSource, eventId);
+                csvExporter.export();
+
+                writer.flush();
+            }
+        };
+
+        return Response.ok(stream).build();
     }
+
+
 }
