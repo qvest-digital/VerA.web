523131cabdfc5592279bf331ea670ae349fd8a7f
Max Weierstall <m.weierstall@tarent.de>
2016-05-20 17:08:18 +0200

    [VERA-255] Export Test ohne setExtractor

--- src/test/groovy/org/evolvis/veraweb/export/CsvExporterTest.groovy
+++ src/test/groovy/org/evolvis/veraweb/export/CsvExporterTest.groovy
@@ -3,40 +3,34 @@ package org.evolvis.veraweb.export
 import de.tarent.extract.ExtractIo
 import de.tarent.extract.Extractor
 import org.apache.commons.io.IOUtils
-import spock.lang.Ignore
 import spock.lang.Specification
 
 import javax.sql.DataSource
+import java.lang.reflect.Field
 
 /**
  * Created by mweier on 26.04.16.
  */
 class CsvExporterTest extends Specification {
 
-    def csvExporter
-    def extractor
+    CsvExporter csvExporter
+    Writer writer = Mock(Writer)
+    DataSource dataSource = Mock(DataSource)
+    Extractor extractor = Mock(Extractor)
 
     public void setup() {
-        DataSource dataSource = Mock(DataSource)
-        Writer writer = Mock(Writer)
-        extractor = Mock(Extractor)
-
         csvExporter = new CsvExporter(writer, dataSource, 42)
-//        csvExporter.setExtractor(extractor)
 
+        Field field = CsvExporter.class.getDeclaredField("extractor")
+        field.setAccessible(true);
+        field.set(csvExporter, extractor);
     }
 
-    @Ignore
     void testExport() {
         when:
             csvExporter.export()
 
         then:
-            1 * extractor.run(_) >> {arguments->
-                ExtractIo io = (ExtractIo) arguments[0];
-                String actual = IOUtils.toString(io.reader());
-                String expected = IOUtils.toString(getClass().getClassLoader().getResourceAsStream("config.yaml"));
-                assert actual == expected;
-            }
+            1 * extractor.run(_,_)
     }
 }
