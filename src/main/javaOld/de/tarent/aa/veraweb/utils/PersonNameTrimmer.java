2d9899d530943376f19b386f154240bca5403a6e
Max Weierstall <m.weierstall@tarent.de>
2016-05-23 14:48:02 +0200

    alle trim methoden in der utils klasse auf private gesetzt

--- src/main/javaOld/de/tarent/aa/veraweb/utils/PersonNameTrimmer.java
+++ src/main/javaOld/de/tarent/aa/veraweb/utils/PersonNameTrimmer.java
@@ -9,44 +9,44 @@ public class PersonNameTrimmer {
         trimSpacesFromNamesForPartner(person);
     }
 
-    public static void trimSpacesFromNamesForMainPerson(Person person) {
+    private static void trimSpacesFromNamesForMainPerson(Person person) {
         trimNamesParnerFirstEncoding(person);
         trimNamesPartnerSecondEncoding(person);
         trimNamesPartnerThirdEncoding(person);
     }
 
-    public static void trimSpacesFromNamesForPartner(Person person) {
+    private static void trimSpacesFromNamesForPartner(Person person) {
         trimNamesMainPersonFirstEncoding(person);
         trimNamesMainPersonSecondEncoding(person);
         trimNamesMainPersonThirdEncoding(person);
     }
 
-    public static void trimNamesMainPersonFirstEncoding(Person person) {
+    private static void trimNamesMainPersonFirstEncoding(Person person) {
         person.firstname_b_e1 = person.firstname_b_e1.trim();
         person.lastname_b_e1 = person.lastname_b_e1.trim();
     }
 
-    public static void trimNamesMainPersonSecondEncoding(Person person) {
+    private static void trimNamesMainPersonSecondEncoding(Person person) {
         person.firstname_b_e2 = person.firstname_b_e2.trim();
         person.lastname_b_e2 = person.lastname_b_e2.trim();
     }
 
-    public static void trimNamesMainPersonThirdEncoding(Person person) {
+    private static void trimNamesMainPersonThirdEncoding(Person person) {
         person.firstname_b_e3 = person.firstname_b_e3.trim();
         person.lastname_b_e3 = person.lastname_b_e3.trim();
     }
 
-    public static void trimNamesParnerFirstEncoding(Person person) {
+    private static void trimNamesParnerFirstEncoding(Person person) {
         person.firstname_a_e1 = person.firstname_a_e1.trim();
         person.lastname_a_e1 = person.lastname_a_e1.trim();
     }
 
-    public static void trimNamesPartnerSecondEncoding(Person person) {
+    private static void trimNamesPartnerSecondEncoding(Person person) {
         person.firstname_a_e2 = person.firstname_a_e2.trim();
         person.lastname_a_e2 = person.lastname_a_e2.trim();
     }
 
-    public static void trimNamesPartnerThirdEncoding(Person person) {
+    private static void trimNamesPartnerThirdEncoding(Person person) {
         person.firstname_a_e3 = person.firstname_a_e3.trim();
         person.lastname_a_e3 = person.lastname_a_e3.trim();
     }
