ded15f6cf8d0c919b9519d1e173cbcbcc5fe17eb
Max Weierstall <m.weierstall@tarent.de>
2016-05-27 16:01:53 +0200

    W.I.P. [VERA-249] 'Passwort zurücksetzen' Backend Funktion angepasst

--- src/main/webroot-src/js/controllers/forgotPasswordController.js
+++ src/main/webroot-src/js/controllers/forgotPasswordController.js
@@ -1,5 +1,31 @@
-module.exports = function($http, $scope, $routeParams, $location, $rootScope, show, $timeout, vwoa, param) {
+module.exports = function($http, $scope, $location, show, $timeout, param, $translate) {
   $scope.forgotPassword = function() {
-    alert("jo")
+        $http({
+            method: 'POST',
+            // TODO: REST-API call ggf. anpassen
+            url: 'api/user/reset/' + encodeURIComponent($scope.osiam.userName) + '',
+                headers: {
+                    "Content-Type" : undefined
+                },
+                data: param({
+                current_language: $translate.use()
+            })
+        }).success(function (result) {
+            switch(result.status) {
+            case 'OK':
+                show.success("USER_PASSWORD_RESET_MAIL");
+                break;
+            case 'USER_NOT_EXISTS':
+                // TODO: Entscheiden ob es eine Fehlermeldung bezüglich nicht existenter User gibt,
+                // TODO: oder ob aus Sicherheitsgründen darauf verzichtet wird?
+                show.error("USER_PASSWORD_RESET_MAIL");
+                break;
+            default:
+                show.error("GENERIC_ERROR");
+            }
+
+        }).error(function (data, status, headers, config) {
+                show.error("GENERIC_ERROR");
+        });
   };
 };
