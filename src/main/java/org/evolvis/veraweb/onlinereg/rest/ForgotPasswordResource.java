8b4827027cec84f8fae5e99cadf2f5cde66ba14d
Atanas Alexandrov <a.alexandrov@tarent.de>
2016-06-02 10:06:17 +0200

    [VERA-249] Passwort vergessen Der Requesthändler nimmt die Anfrage an, sucht nach Benutzer und wenn die Suche erfolgreich war, wird die E-Mail verschickt. (ist aber noch W.i.P, da momentan noch Dummy-Daten drine sind)

--- src/main/java/org/evolvis/veraweb/onlinereg/rest/ForgotPasswordResource.java
+++ src/main/java/org/evolvis/veraweb/onlinereg/rest/ForgotPasswordResource.java
@@ -1,8 +1,13 @@
 package org.evolvis.veraweb.onlinereg.rest;
 
 import org.evolvis.veraweb.onlinereg.entities.Person;
+import org.evolvis.veraweb.onlinereg.mail.EmailConfiguration;
+import org.evolvis.veraweb.onlinereg.mail.MailDispatcher;
+import org.hibernate.Query;
 import org.hibernate.Session;
 
+import javax.mail.MessagingException;
+import javax.mail.NoSuchProviderException;
 import javax.ws.rs.FormParam;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
@@ -16,10 +21,32 @@ import javax.ws.rs.core.MediaType;
 @Produces(MediaType.APPLICATION_JSON)
 public class ForgotPasswordResource extends AbstractResource {
 
+    private MailDispatcher mailDispatcher;
+    private EmailConfiguration emailConfiguration;
+
     @POST
     @Path("/request/reset-password-link")
-    public String requestResetPasswordLink(@FormParam("username") String username) {
-        return username;
+    public void requestResetPasswordLink(@FormParam("username") String username) throws MessagingException {
+        final Session session = openSession();
+        try {
+            final Query query = session.getNamedQuery("Person.findByUsername");
+            Person person = (Person) query.uniqueResult();
+            if (person != null) {
+                sendResetPasswordLinkEmail();
+            }
+        } finally {
+            session.close();
+        }
+    }
+
+    private void sendResetPasswordLinkEmail() throws MessagingException {
+        if (mailDispatcher == null) {
+            if (emailConfiguration == null) {
+                emailConfiguration = new EmailConfiguration("de_DE");
+            }
+            mailDispatcher = new MailDispatcher(emailConfiguration);
+        }
+        mailDispatcher.sendVerificationEmail("me", "me", "resetPassword", "resset password ${link}", "http://localhost/reset/password/", "html");
     }
 
 }
--- src/test/groovy/org/evolvis/veraweb/onlinereg/rest/ForgotPasswordResourceTest.groovy
+++ src/test/groovy/org/evolvis/veraweb/onlinereg/rest/ForgotPasswordResourceTest.groovy
@@ -1,24 +1,69 @@
 package org.evolvis.veraweb.onlinereg.rest
 
+import org.evolvis.veraweb.onlinereg.entities.Person
+import org.evolvis.veraweb.onlinereg.mail.EmailConfiguration
+import org.evolvis.veraweb.onlinereg.mail.MailDispatcher
+import org.hibernate.Query
+import org.hibernate.Session
+import org.hibernate.SessionFactory
 import spock.lang.Specification
+
+import javax.mail.Transport
+import javax.servlet.ServletContext
 
 /**
  * @author Atanas Alexandrov, tarent solutions GmbH
  */
 class ForgotPasswordResourceTest extends Specification {
 
+    ServletContext context = Mock(ServletContext)
+    SessionFactory sessionFactory = Mock(SessionFactory)
+    Session session = Mock(Session)
+    def dispatcher
     def forgotPasswordResource
 
-    public void setup() {
-        forgotPasswordResource = new ForgotPasswordResource();
+    def transport = Mock(Transport)
+
+    void setup() {
+        def emailConfiguration = new EmailConfiguration("host", 465, "ssl", "username", "password", "from@tarent.de", "subject", "content", "plaintext")
+        dispatcher = new MailDispatcher(emailConfiguration)
+        dispatcher.setTransport(transport)
+        context.getAttribute("SessionFactory") >> sessionFactory
+        sessionFactory.openSession() >> session
+        forgotPasswordResource = new ForgotPasswordResource(mailDispatcher: dispatcher, context: context)
     }
 
-    public void "request reset password link"() {
+    public void "request reset password link successfull"() {
+        given:
+            Query query = Mock(Query)
+            Person person = Mock(Person)
+            session.getNamedQuery("Person.findByUsername") >> query
+            query.uniqueResult() >> person
+
         when:
-            def result = forgotPasswordResource.requestResetPasswordLink("tarentuser")
+            forgotPasswordResource.requestResetPasswordLink("tarentuser")
 
         then:
-            assert result == "tarentuser"
+            1 * transport.connect('host', 'username', 'password')
+            1 * transport.close()
+            session != null
+            1 * session.close()
+    }
+
+    public void "request reset password link failed"() {
+        given:
+            Query query = Mock(Query)
+            session.getNamedQuery("Person.findByUsername") >> query
+            query.uniqueResult() >> null
+
+        when:
+            forgotPasswordResource.requestResetPasswordLink("tarentuser")
+
+        then:
+            0 * transport.connect('host', 'username', 'password')
+            0 * transport.close()
+            session != null
+            1 * session.close()
     }
 
 }
