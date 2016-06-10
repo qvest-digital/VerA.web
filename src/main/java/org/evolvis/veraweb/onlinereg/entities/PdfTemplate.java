669c0759caac4252bb5f9d7a5428b122053cf62d
Max Weierstall <m.weierstall@tarent.de>
2016-06-10 14:02:11 +0200

    [VERA-13] Vorlage löschen - NamedQuery hinzugefügt

--- src/main/java/org/evolvis/veraweb/onlinereg/entities/PdfTemplate.java
+++ src/main/java/org/evolvis/veraweb/onlinereg/entities/PdfTemplate.java
@@ -7,6 +7,8 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
 import javax.persistence.Table;
 import javax.xml.bind.annotation.XmlRootElement;
 import java.sql.Timestamp;
@@ -18,7 +20,14 @@ import java.sql.Timestamp;
 @XmlRootElement
 @Entity
 @Table(name = "pdftemplate")
+@NamedQueries({
+    @NamedQuery(name = PdfTemplate.DELETE_PDF_TEMPLATE,
+        query = "delete from PdfTemplate p where fk=:" + PdfTemplate.PARAM_PDF_ID)
+})
 public class PdfTemplate {
+
+    public static final String DELETE_PDF_TEMPLATE = "PdfTemplate.deletePdfTemplateById";
+    public static final String PARAM_PDF_ID = "id";
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
