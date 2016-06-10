16b47d50e0651a999ed687192fddc9374b2f8322
Max Weierstall <m.weierstall@tarent.de>
2016-06-10 15:40:48 +0200

    [VERA-13] Vorlage bearbeiten - Methode mit Sinn versehen

--- src/main/java/org/evolvis/veraweb/onlinereg/rest/PdfTemplateResource.java
+++ src/main/java/org/evolvis/veraweb/onlinereg/rest/PdfTemplateResource.java
@@ -24,7 +24,8 @@ public class PdfTemplateResource extends AbstractResource {
         Session session = openSession();
         try {
             if (id != null) {
-                updatePdfTemplate();
+                updatePdfTemplate(session, id, name);
+                session.flush();
             } else {
                 final PdfTemplate pdfTemplate = initPdfTemplate(name);
                 session.save(pdfTemplate);
@@ -46,8 +47,11 @@ public class PdfTemplateResource extends AbstractResource {
         return pdfTemplate;
     }
 
-    private void updatePdfTemplate() {
-        System.out.println("TODO");
+    private void updatePdfTemplate(Session session, Integer id, String name) {
+        final Query query = session.getNamedQuery("PdfTemplate.updatePdfTemplateById");
+        query.setInteger("id", id);
+        query.setString("name", name);
+        query.executeUpdate();
     }
 
     @POST
