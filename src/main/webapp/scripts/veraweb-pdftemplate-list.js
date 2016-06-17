891f0eac066b8ca4b11230c67c5af26ad155ae66
Atanas Alexandrov <a.alexandrov@tarent.de>
2016-06-17 10:37:13 +0200

    [VERA-13] Hidden-Fields vor der Tabelle einfügen

--- src/main/webapp/scripts/veraweb-pdftemplate-list.js
+++ src/main/webapp/scripts/veraweb-pdftemplate-list.js
@@ -9,6 +9,7 @@ $(document).ready(function() {
             $(".errormsg").remove();
             $(".successmsg").remove();
             for (i = 0; i < data.length; i++) {
+                $("#pdftemplate-list-overview").before("<input type='hidden' name='list' value='"+ data[i].pk + "'/>");
                 $("#pdftemplate-list-overview").find("tbody").find("tr:last").before("<tr id='pdftemplate-" + data[i].pk + "'><td><input type='checkbox' id='" + data[i].pk + "' name='" + data[i].pk +"-select'></td><td>" + data[i].pk + "</td><td>" + data[i].name + "</td></tr>");
             }
         },
