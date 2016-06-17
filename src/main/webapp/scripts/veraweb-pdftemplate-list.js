3f91086911b4f42a97de22be61ec7ef6facdbc12
Atanas Alexandrov <a.alexandrov@tarent.de>
2016-06-17 10:28:29 +0200

    [VERA-13] Liste der Vorlagen über die REST-API laden

---
+++ src/main/webapp/scripts/veraweb-pdftemplate-list.js
@@ -0,0 +1,22 @@
+$(document).ready(function() {
+    $.ajax({
+        url: $("#pdfetemplate-list").data("pdflist"),
+        data: {
+            mandantid:$("#pdfetemplate-orgunit").data("orgunit")
+        },
+        type: 'GET',
+        success: function(data){
+            $(".errormsg").remove();
+            $(".successmsg").remove();
+            for (i = 0; i < data.length; i++) {
+                $("#pdftemplate-list-overview").find("tbody").find("tr:last").before("<tr id='pdftemplate-" + data[i].pk + "'><td><input type='checkbox' id='" + data[i].pk + "' name='" + data[i].pk +"-select'></td><td>" + data[i].pk + "</td><td>" + data[i].name + "</td></tr>");
+            }
+        },
+        error: function(data) {
+            $(".errormsg").remove();
+            $(".successmsg").remove();
+            var errorMsg = $("#pdfetemplate-list-errormsg").data("errormsg");
+            showWarning(errorMsg);
+        }
+    });
+});
