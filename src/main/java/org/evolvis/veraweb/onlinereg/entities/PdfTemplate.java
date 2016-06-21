1e026fbd8269455c96550260947cd31b4de35fdd
Max Weierstall <m.weierstall@tarent.de>
2016-06-21 16:47:55 +0200

    [VERA-13] Update PDF Template Content Query

--- src/main/java/org/evolvis/veraweb/onlinereg/entities/PdfTemplate.java
+++ src/main/java/org/evolvis/veraweb/onlinereg/entities/PdfTemplate.java
@@ -28,6 +28,8 @@ import java.sql.Timestamp;
         query = "select p from PdfTemplate p where p.fk_orgunit=:" + PdfTemplate.PARAM_PDF_ORGUNIT),
     @NamedQuery(name = PdfTemplate.UPDATE_PDF_TEMPLATE,
             query = "update PdfTemplate p set name=:" + PdfTemplate.PARAM_PDF_NAME + " where p.pk=:" + PdfTemplate.PARAM_PDF_ID),
+    @NamedQuery(name = PdfTemplate.UPDATE_PDF_TEMPLATE_CONTENT,
+            query = "update PdfTemplate p set name=:" + PdfTemplate.PARAM_PDF_NAME + ", content=:" + PdfTemplate.PARAM_PDF_CONTENT + " where p.pk=:" + PdfTemplate.PARAM_PDF_ID),
     @NamedQuery(name = PdfTemplate.DELETE_PDF_TEMPLATE,
             query = "delete from PdfTemplate p where p.pk=:" + PdfTemplate.PARAM_PDF_ID)
 })
@@ -36,10 +38,12 @@ public class PdfTemplate {
     public static final String GET_PDF_TEMPLATE = "PdfTemplate.getPdfTemplateById";
     public static final String GET_PDF_TEMPLATE_LIST_BY_ORGUNIT = "PdfTemplate.getPdfTemplateListByOrgunit";
     public static final String UPDATE_PDF_TEMPLATE = "PdfTemplate.updatePdfTemplateById";
+    public static final String UPDATE_PDF_TEMPLATE_CONTENT = "PdfTemplate.updatePdfTemplateByIdWithContent";
     public static final String DELETE_PDF_TEMPLATE = "PdfTemplate.deletePdfTemplateById";
     public static final String PARAM_PDF_ID = "id";
     public static final String PARAM_PDF_ORGUNIT = "fk_orgunit";
     public static final String PARAM_PDF_NAME = "name";
+    public static final String PARAM_PDF_CONTENT = "content";
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
