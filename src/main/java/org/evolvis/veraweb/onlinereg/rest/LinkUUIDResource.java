e4b001828adb3122dc484fe1b56ec61538d047a0
Atanas Alexandrov <a.alexandrov@tarent.de>
2016-07-04 15:20:46 +0200

    TechDebt: Unbenutze Imports entfernt

--- src/main/java/org/evolvis/veraweb/onlinereg/rest/LinkUUIDResource.java
+++ src/main/java/org/evolvis/veraweb/onlinereg/rest/LinkUUIDResource.java
@@ -29,11 +29,8 @@ import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
-import javax.ws.rs.core.Link;
 import javax.ws.rs.core.MediaType;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.UUID;
 
 /**
  * @author Atanas Alexandrov, tarent solutions GmbH
