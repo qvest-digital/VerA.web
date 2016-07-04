436d8926e3bc29f52ab3bc4325636f2570356eb9
Atanas Alexandrov <a.alexandrov@tarent.de>
2016-07-04 14:44:27 +0200

    TechDebt: Unbenutzter Code entfernt

--- src/main/java/org/evolvis/veraweb/onlinereg/rest/FormDataResource.java
+++ src/main/java/org/evolvis/veraweb/onlinereg/rest/FormDataResource.java
@@ -12,7 +12,7 @@ import java.nio.file.Paths;
  * Created by eugen on 16.06.16.
  */
 public class FormDataResource extends AbstractResource {
-    private static final Logger LOGGER = Logger.getLogger(FormDataResource.class);
+    
     protected String tmpPath = System.getProperty("java.io.tmpdir");
 
     File saveTempFile(final FormDataBodyPart part) throws IOException {
