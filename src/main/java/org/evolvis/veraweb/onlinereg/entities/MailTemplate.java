df5f6460fcfabe6986937ac993ee56973aa16653
Atanas Alexandrov <a.alexandrov@tarent.de>
2016-07-06 15:58:15 +0200

    VERA-367: Entity eingefügt

---
+++ src/main/java/org/evolvis/veraweb/onlinereg/entities/MailTemplate.java
@@ -0,0 +1,36 @@
+package org.evolvis.veraweb.onlinereg.entities;
+
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ * @author Atanas Alexandrov, tarent solutions GmbH
+ */
+@Data
+@XmlRootElement
+@Entity
+@Table(name = "tmaildraft")
+@NamedQueries({
+        @NamedQuery(name = MailTemplate.GET_MAILTEMPLATE_BY_ID,
+                query = "SELECT mt FROM MailTemplate mt WHERE pk=:templateId")
+
+})
+public class MailTemplate {
+
+    public static final String GET_MAILTEMPLATE_BY_ID = "MailTemplate.getPdfTemplateById";
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer pk;
+    private String name;
+    private String subject;
+    private String content;
+}
