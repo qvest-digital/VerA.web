6e2c90bad44a11d85c1a5255081fcc927937e52e
Atanas Alexandrov <a.alexandrov@tarent.de>
2016-07-07 10:15:18 +0200

    VERA-367: Umstellung auf QueryParam statt PathParam

--- src/main/java/org/evolvis/veraweb/onlinereg/rest/MailTemplateResource.java
+++ src/main/java/org/evolvis/veraweb/onlinereg/rest/MailTemplateResource.java
@@ -8,6 +8,7 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
@@ -19,8 +20,8 @@ import javax.ws.rs.core.Response;
 public class MailTemplateResource extends AbstractResource {
 
     @GET
-    @Path("/{templateId}")
-    public Response getMailTemplate(@PathParam("templateId") Integer templateId) {
+    @Path("/")
+    public Response getMailTemplate(@QueryParam("templateId") Integer templateId) {
         final Session session = openSession();
         try {
             final Query query = session.getNamedQuery("MailTemplate.getPdfTemplateById");
