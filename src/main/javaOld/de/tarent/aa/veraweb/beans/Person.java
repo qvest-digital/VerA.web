90da22ee7a76f000ac02dbba486be53c25cb4102
Atanas Alexandrov <a.alexandrov@tarent.de>
2016-12-06 10:44:24 +0100

    [VERA-453] Personenstammblatt ergänzen um BMVG ID W.i.P. Feld persistieren

--- src/main/javaOld/de/tarent/aa/veraweb/beans/Person.java
+++ src/main/javaOld/de/tarent/aa/veraweb/beans/Person.java
@@ -50,7 +50,7 @@ public class Person extends AbstractHistoryBean implements PersonConstants, OrgU
     /** ID */
     public Integer id;
     @Pattern(regexp="\\d{2}-\\d{3}-\\d{3}")
-    @Size(max=10)
+    @Size(max=100)
     public String internalId;
     /** ID der Mandanten-Einheit */
     public Integer orgunit;
@@ -794,5 +794,9 @@ public class Person extends AbstractHistoryBean implements PersonConstants, OrgU
 
     public void setId(Integer id) {
         this.id = id;
+    }
+
+    public void setInternalId(String internalId) {
+        this.internalId = internalId;
     }
 }
