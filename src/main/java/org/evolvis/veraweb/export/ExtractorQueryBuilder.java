61833000ad78bf143582af8439e6026fc0310ce3
Atanas Alexandrov <a.alexandrov@tarent.de>
2016-12-14 11:25:21 +0100

    [VERA-455] Gästedetails -> Zusätzliche Personendaten: Export anpassen

--- src/main/java/org/evolvis/veraweb/export/ExtractorQueryBuilder.java
+++ src/main/java/org/evolvis/veraweb/export/ExtractorQueryBuilder.java
@@ -5,10 +5,12 @@ import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import de.tarent.extract.ColumnMapping;
 import de.tarent.extract.ExtractorQuery;
 
 public class ExtractorQueryBuilder {
 
+    public static final String OPTIONAL_FIELD_LABEL_PREFIX = "OPTIONAL_FIELD_LABEL_";
     private final ExtractorQuery template;
     private Map<String, String> substitutions = new HashMap<String, String>();
 
@@ -23,10 +25,23 @@ public class ExtractorQueryBuilder {
 
     public ExtractorQuery build() {
         final ExtractorQuery q = new ExtractorQuery();
-        q.setMappings(template.getMappings());
+        q.setMappings(applyMappingsSubstitutions(template.getMappings()));
         q.setProgressInterval(template.getProgressInterval());
         q.setSql(applySubstitutions(template.getSql()));
         return q;
+    }
+
+    private Map<String, ColumnMapping> applyMappingsSubstitutions(Map<String, ColumnMapping> mappings) {
+        for (Map.Entry<String, ColumnMapping> entry : mappings.entrySet()) {
+            if (entry.getKey().startsWith(OPTIONAL_FIELD_LABEL_PREFIX)) {
+                final String mapTo = entry.getValue().getMapTo();
+                final String group = mapTo.substring(2,mapTo.length()-1);
+                final ColumnMapping replacement = new ColumnMapping(mapTo.replaceAll("\\$\\{([^\\}]+)\\}", lookupSubstitution(group)));
+                mappings.put(entry.getKey(), replacement);
+                mappings.remove(entry);
+            }
+        }
+        return mappings;
     }
 
     private String applySubstitutions(String sql) {
