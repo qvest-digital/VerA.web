e69a8ca00b174fedc22c8a70c1b931fd12940b17
Max Weierstall <m.weierstall@tarent.de>
2016-12-27 18:24:49 +0100

    [VERA-270] Impressum der OA (einsprachig)

---
+++ src/main/java/org/evolvis/veraweb/onlinereg/rest/ImprintResource.java
@@ -0,0 +1,42 @@
+package org.evolvis.veraweb.onlinereg.rest;
+
+import org.evolvis.veraweb.onlinereg.utils.VworPropertiesReader;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import java.util.HashMap;
+
+/**
+ * Created by mweier on 27.12.16.
+ */
+@Path("/imprint")
+@Produces(MediaType.APPLICATION_JSON)
+public class ImprintResource extends AbstractResource{
+
+    private VworPropertiesReader vworPropertiesReader;
+
+    @GET
+    @Path("/")
+    public HashMap<String, String> getImprintList() {
+
+        final VworPropertiesReader propertiesReader = getVworPropertiesReader();
+        final HashMap<String, String> map = new HashMap<>();
+
+        for(String key : propertiesReader.getProperties().stringPropertyNames()){
+            if(key.startsWith("imprint.")){
+                map.put(key.substring(8), propertiesReader.getProperty(key));
+            }
+        }
+
+        return map;
+    }
+
+    private VworPropertiesReader getVworPropertiesReader() {
+        if (vworPropertiesReader == null) {
+            vworPropertiesReader = new VworPropertiesReader();
+        }
+        return vworPropertiesReader;
+    }
+}
