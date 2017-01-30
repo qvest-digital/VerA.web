761c1b135142279fdef0fa2b1f59937a6b376855
Max Weierstall <m.weierstall@tarent.de>
2017-01-30 15:54:50 +0100

    [VERA-270] Review

---
+++ src/main/java/org/evolvis/veraweb/onlinereg/entities/Imprint.java
@@ -0,0 +1,20 @@
+package org.evolvis.veraweb.onlinereg.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+/**
+ * Created by mweier on 30.01.17.
+ */
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Imprint {
+
+    private String heading;
+    private String content;
+}
