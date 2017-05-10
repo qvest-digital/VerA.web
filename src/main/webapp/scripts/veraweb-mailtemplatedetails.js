d3f81b219ae02c729134eb18452f39352664fb4c
Atanas Alexandrov <a.alexandrov@tarent.de>
2017-05-10 10:37:32 +0200

    [VERA-518] Refactoring Der JS Code in separate Datei auslagern (inkl. Code für das Einfügen von Platzhaltern)

---
+++ src/main/webapp/scripts/veraweb-mailtemplatedetails.js
@@ -0,0 +1,26 @@
+(function(){
+
+	function addPlatzhalter(s) {
+		var sel = document.getElementById('platzhalter');
+		var text = sel.options[sel.selectedIndex].value;
+		if (text.length > 0) {
+			CKEDITOR.instances.maildrafttext.insertText('<' + text + '>');
+		}
+	}
+
+    function setEmailContent() {
+        var data = CKEDITOR.instances.maildrafttext.getData();
+        $("#maildrafttext").val(data);
+    }
+
+    function executeSave() {
+        setEmailContent();
+        $('#formlist').submit();
+    }
+
+    $(document).ready(function() {
+        CKEDITOR.replace('maildrafttext');
+        $("input[name='send']").click(executeSave);
+        $("input[name='add']").click(addPlatzhalter);
+    });
+})();
