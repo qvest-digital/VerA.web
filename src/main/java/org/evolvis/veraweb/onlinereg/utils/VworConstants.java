0a2b4fb4d3b41c6dfe665f378646acb4f7b075d1
mirabilos <t.glaser@tarent.de>
2017-09-20 17:35:00 +0200

    mvn com.mycila:license-maven-plugin:3.0:format

--- src/main/java/org/evolvis/veraweb/onlinereg/utils/VworConstants.java
+++ src/main/java/org/evolvis/veraweb/onlinereg/utils/VworConstants.java
@@ -1,24 +1,24 @@
-/**
+package org.evolvis.veraweb.onlinereg.utils;
+
+/*-
  * veraweb, platform independent webservice-based event management
  * (Veranstaltungsmanagment VerA.web), is
  * Copyright © 2004–2008 tarent GmbH
  * Copyright © 2013–2016 tarent solutions GmbH
- * <p/>
+ *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2 of the License, or
  * (at your option) any later version.
- * <p/>
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * <p/>
+ *
  * You should have received a copy of the GNU General Public License along
  * with this program; if not, see: http://www.gnu.org/licenses/
  */
-package org.evolvis.veraweb.onlinereg.utils;
-
 /**
  * @author Jon Nuñez, tarent solutions GmbH on 02.07.15.
  */
