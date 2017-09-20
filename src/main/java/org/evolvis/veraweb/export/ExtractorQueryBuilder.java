2f99533e69c31199cb57f9cb2fcf093aa5e70181
mirabilos <t.glaser@tarent.de>
2017-09-20 17:34:59 +0200

    mvn com.mycila:license-maven-plugin:3.0:format

--- src/main/java/org/evolvis/veraweb/export/ExtractorQueryBuilder.java
+++ src/main/java/org/evolvis/veraweb/export/ExtractorQueryBuilder.java
@@ -1,5 +1,29 @@
 package org.evolvis.veraweb.export;
 
+/*-
+ * veraweb, platform independent webservice-based event management
+ * (Veranstaltungsmanagment VerA.web), export module is
+ * Copyright © 2016, 2017
+ * 	Атанас Александров <a.alexandrov@tarent.de>
+ * Copyright © 2016
+ * 	Lukas Degener <l.degener@tarent.de>
+ * 	Max Weierstall <m.weierstall@tarent.de>
+ * Copyright © 2017
+ * 	mirabilos <t.glaser@tarent.de>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, see: http://www.gnu.org/licenses/
+ */
 import java.util.HashMap;
 import java.util.Map;
 import java.util.regex.Matcher;
