7939de7db14b0222396e9dbbcb68468479d4028d
mirabilos <t.glaser@tarent.de>
2017-09-21 20:08:54 +0200

    add upstream repo’s git infos

--- src/main/webapp/js/ext/ckeditor/dev/builder/build.sh
+++ src/main/webapp/js/ext/ckeditor/dev/builder/build.sh
@@ -7,6 +7,7 @@
 # Modifications by ⮡ tarent for building from the upstream CKEditor
 # release for VerA.web:
 # • download using https, not http
+# • embed hardcoded git information for upstream repo’s tag
 
 set -e
 
@@ -62,12 +63,12 @@ echo "Starting CKBuilder..."
 JAVA_ARGS=${ARGS// -t / } # Remove -t from args.
 
 VERSION="4.6.0 DEV"
-REVISION=$(git rev-parse --verify --short HEAD)
+REVISION=20af9170a #$(git rev-parse --verify --short HEAD)
 SEMVER_REGEX="^([0-9]+)\.([0-9]+)\.([0-9]+)(\-[0-9A-Za-z-]+)?(\+[0-9A-Za-z-]+)?$"
 
 # Get version number from tag (if available and follows semantic versioning principles).
 # Use 2>/dev/null to block "fatal: no tag exactly matches", true is needed because of "set -e".
-TAG=$(git symbolic-ref -q --short HEAD || git describe --tags --exact-match 2>/dev/null) || true
+TAG=4.6.2 #$(git symbolic-ref -q --short HEAD || git describe --tags --exact-match 2>/dev/null) || true
 # "Git Bash" does not support regular expressions.
 if echo $TAG | grep -E "$SEMVER_REGEX" > /dev/null
 then
