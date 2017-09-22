e1d6a58de0ebefcbef0399003297e39d003bdcfd
mirabilos <t.glaser@tarent.de>
2017-09-22 16:39:17 +0200

    merge Serbian, somewhat

--- core/src/main/js/ext/jscalendar/lang/calendar-sr-utf8.js
+++ core/src/main/js/ext/jscalendar/lang/calendar-sr-utf8.js
@@ -2,7 +2,9 @@
 
 // Author: Mihai Bazon, <mishoo@infoiasi.ro>
 // Translation: Nenad Nikolic <shone@europe.com>
-// Feel free to use / redistribute under the GNU LGPL.
+// Author Srle Doberman <srleiqnet@hotmail.com>, September 2005.
+// Feel free to use this script under the terms of the GNU Lesser General
+// Public License, as long as you do not remove or alter this notice.
 
 // full day names
 Calendar._DN = new Array
@@ -34,7 +36,7 @@ Calendar._MN = new Array
  "April",
  "Maj",
  "Jun",
- "Jul",
+ "July",
  "Avgust",
  "Septembar",
  "Oktobar",
@@ -66,23 +68,41 @@ Calendar._TT["ABOUT"] =
 "Najnovija verzija kontrole nalazi se http://dynarch.com/mishoo/calendar.epl\n" +
 "Distribuirano po GNU LGPL licencom.  Za detalje pogledaj http://gnu.org/licenses/lgpl.html." +
 "\n\n" +
+/// either this {
 "Izbor datuma:\n" +
 "- Koristi dugmiće \xab, \xbb za izbor godine\n" +
 "- Koristi dugmiće " + String.fromCharCode(0x2039) + ", " + String.fromCharCode(0x203a) + " za izbor meseca\n" +
 "- Za brži izbor, držati pritisnut taster miša iznad bilo kog od pomenutih dugmića";
+/// } or this {
+//"Selekcija datuma:\n" +
+//"- Koristite \xab, \xbb dugmiće za selekciju godine\n" +
+//"- Koristite " + String.fromCharCode(0x2039) + ", " +
+//String.fromCharCode(0x203a) + " dugmiće za selekciju meseca\n" +
+//"- Dugi pritisak na dugmiće iznad za bržu selekciju.";
+/// }
 Calendar._TT["ABOUT_TIME"] = "\n\n" +
+/// either this {
 "Izbor vremena:\n" +
 "- Kliktaj na sate ili minute povećava njihove vrednosti\n" +
 "- Shift-klik smanjuje njihove vrednosti\n" +
 "- klikni i vuci za brži izbor.";
+/// } or this {
+//"Selekcija vremena:\n" +
+//"- Pritisnite na bilo koji deo vremena za njegovo povećanje\n" +
+//"- ili (Shift-)Pritisnite za umanjivanje vremena\n" +
+//"- ili pritisnite i povucite za bržu selekciju.";
+/// }
 
 Calendar._TT["PREV_YEAR"] = "Prethodna godina (dugi pritisak za meni)";
 Calendar._TT["PREV_MONTH"] = "Prethodni mesec (dugi pritisak za meni)";
 Calendar._TT["GO_TODAY"] = "Idi na današnji dan";
+//Calendar._TT["GO_TODAY"] = "Idite do današnjeg datuma";
 Calendar._TT["NEXT_MONTH"] = "Sledeći mesec (dugi pritisak za meni)";
 Calendar._TT["NEXT_YEAR"] = "Sledeća godina (dugi pritisak za meni)";
 Calendar._TT["SEL_DATE"] = "Izaberi datum";
+//Calendar._TT["SEL_DATE"] = "Izaberite datum";
 Calendar._TT["DRAG_TO_MOVE"] = "Pritisni i vuci za promenu pozicije";
+//Calendar._TT["DRAG_TO_MOVE"] = "Pritisnite i povucite za promenu pozicije";
 Calendar._TT["PART_TODAY"] = " (danas)";
 
 // Choose first day of week.
@@ -94,12 +114,14 @@ Calendar._TT["SUN_FIRST"] = "Prikaži nedelju kao prvi dan nedelje";
 Calendar._TT["WEEKEND"] = "0,6";
 
 Calendar._TT["CLOSE"] = "Zatvori";
+//Calendar._TT["CLOSE"] = "Zatvorite";
 Calendar._TT["TODAY"] = "Danas";
 Calendar._TT["TIME_PART"] = "(Shift-)klikni i vuci za promenu vrednosti";
+//Calendar._TT["TIME_PART"] = "(Shift-)Pritisnite ili povucite za promenu vrednosti";
 
 // date formats
-Calendar._TT["DEF_DATE_FORMAT"] = "%d-%m-%Y";
-Calendar._TT["TT_DATE_FORMAT"] = "%A, %B %e";
+Calendar._TT["DEF_DATE_FORMAT"] = "%d.%m.%Y";
+Calendar._TT["TT_DATE_FORMAT"] = "%A %d.%m.%Y";
 
-Calendar._TT["WK"] = "wk";
-Calendar._TT["TIME"] = "Time:";
+Calendar._TT["WK"] = "Nd";
+Calendar._TT["TIME"] = "Vreme:";
