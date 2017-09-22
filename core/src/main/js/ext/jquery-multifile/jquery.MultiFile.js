42f141a0f5c29e3f1e4c3525a1d604e3fd770f9a
mirabilos <t.glaser@tarent.de>
2017-09-22 15:56:10 +0200

    UTF-8!

--- core/src/main/js/ext/jquery-multifile/jquery.MultiFile.js
+++ core/src/main/js/ext/jquery-multifile/jquery.MultiFile.js
@@ -395,7 +395,7 @@ if (window.jQuery)(function ($) {
 							if($.type(MultiFile.error)=='function')
 								MultiFile.error(ERROR.join('\n\n'));
 
-							// 2007-06-24: BUG FIX - Thanks to Adrian Wróbel <adrian [dot] wrobel [at] gmail.com>
+							// 2007-06-24: BUG FIX - Thanks to Adrian WrÃ¶bel <adrian [dot] wrobel [at] gmail.com>
 							// Ditch the trouble maker and add a fresh new element
 							MultiFile.n--;
 							MultiFile.addSlave(newEle[0], slave_count);
