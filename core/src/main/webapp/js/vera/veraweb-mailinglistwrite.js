79118427687680783519c3f6d63423bf6706deeb
mirabilos <t.glaser@tarent.de>
2017-09-26 19:12:40 +0200

    skip symlinking for our own js

--- core/src/main/webapp/js/vera/veraweb-mailinglistwrite.js
+++ core/src/main/webapp/js/vera/veraweb-mailinglistwrite.js
@@ -1,1 +1,147 @@
-../../../js/vera/veraweb-mailinglistwrite.js
+/*
+ * Veranstaltungsmanagement VerA.web (platform-independent
+ * webservice-based event management) is Copyright
+ *  © 2014, 2015, 2016, 2017 Атанас Александров <a.alexandrov@tarent.de>
+ *  © 2013 Иванка Александрова <i.alexandrova@tarent.de>
+ *  © 2013 Patrick Apel <p.apel@tarent.de>
+ *  © 2016 Eugen Auschew <e.auschew@tarent.de>
+ *  © 2013 Andrei Boulgakov <a.boulgakov@tarent.de>
+ *  © 2013 Valentin But <v.but@tarent.de>
+ *  © 2016 Lukas Degener <l.degener@tarent.de>
+ *  © 2017 Axel Dirla <a.dirla@tarent.de>
+ *  © 2015 Julian Drawe <j.drawe@tarent.de>
+ *  © 2014 Dominik George <d.george@tarent.de>
+ *  © 2013 Sascha Girrulat <s.girrulat@tarent.de>
+ *  © 2008 David Goemans <d.goemans@tarent.de>
+ *  © 2015 Viktor Hamm <v.hamm@tarent.de>
+ *  © 2013 Katja Hapke <k.hapke@tarent.de>
+ *  © 2013 Hendrik Helwich <h.helwich@tarent.de>
+ *  © 2007 jan <jan@evolvis.org>
+ *  © 2005, 2006, 2007, 2008 Christoph Jerolimov <jerolimov@gmx.de>
+ *  © 2008, 2009, 2010 Carsten Klein <c.klein@tarent.de>
+ *  © 2014 Martin Ley <m.ley@tarent.de>
+ *  © 2014, 2015 Max Marche <m.marche@tarent.de>
+ *  © 2013, 2014, 2015, 2016, 2017 mirabilos <t.glaser@tarent.de>
+ *  © 2016 Cristian Molina <c.molina@tarent.de>
+ *  © 2017 Michael Nienhaus <m.nienhaus@tarent.de>
+ *  © 2013 Claudia Nuessle <c.nuessle@tarent.de>
+ *  © 2014, 2015 Jon Nunez Alvarez <j.nunez-alvarez@tarent.de>
+ *  © 2016 Jens Oberender <j.oberender@tarent.de>
+ *  © 2016, 2017 Miluška Pech <m.pech@tarent.de>
+ *  © 2009 Martin Pelzer <m.pelzer@tarent.de>
+ *  © 2013 Marc Radel <m.radel@tarent.de>
+ *  © 2013 Sebastian Reimers <s.reimers@tarent.de>
+ *  © 2015 Charbel Saliba <c.saliba@tarent.de>
+ *  © 2008, 2009, 2010 Thomas Schmitz <t.schmitz@tarent.de>
+ *  © 2013 Volker Schmitz <v.schmitz@tarent.de>
+ *  © 2014 Sven Schumann <s.schumann@tarent.de>
+ *  © 2014 Sevilay Temiz <s.temiz@tarent.de>
+ *  © 2013 Kevin Viola Schmitz <k.schmitz@tarent.de>
+ *  © 2015 Stefan Walenda <s.walenda@tarent.de>
+ *  © 2015, 2016, 2017 Max Weierstall <m.weierstall@tarent.de>
+ *  © 2013 Rebecca Weinz <r.weinz@tarent.de>
+ *  © 2015, 2016 Stefan Weiz <s.weiz@tarent.de>
+ *  © 2015, 2016 Tim Zimmer <t.zimmer@tarent.de>
+ * and older code, Copyright © 2004–2008 ⮡ tarent GmbH and contributors.
+ * Licensor is tarent solutions GmbH, http://www.tarent.de/
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, see: http://www.gnu.org/licenses/
+ */
+(function () {
+	$(document).ready(function () {
+		CKEDITOR.replace('mailtext'); // Enable CKEditor
+		$("input[name='send']").click(executeAllActions);
+		$("input[name='add']").click(addPlatzhalter);
+	});
+
+	function executeAllActions() {
+		rewriteAction();
+		setEmailContent();
+		executeSubmit();
+	}
+
+	function executeSubmit() {
+		$('#formlist').submit();
+	}
+
+	function setEmailContent() {
+		var data = CKEDITOR.instances.mailtext.getData();
+		$("#mailtext").val(data);
+	}
+
+	function rewriteAction() {
+		$("#formlist").attr("action", $("#formaction").data("formaction"));
+	}
+
+	function addPlatzhalter(s) {
+		var sel = document.getElementById('platzhalter');
+		var text = sel.options[sel.selectedIndex].value;
+		if (text.length > 0) {
+			CKEDITOR.instances.mailtext.insertText('<' + text + '>');
+		}
+	}
+
+	function disableSubmitButton() {
+		$('[type="button"]').attr('disabled', 'disabled').addClass('disabled');
+	}
+
+	function enableSubmitButton() {
+		$('[type="button"]').removeAttr('disabled').removeClass('disabled');
+	}
+
+	function displaySpinner() {
+		$('#spinner').show();
+	}
+
+	function hideSpinner() {
+		$('#spinner').hide();
+	}
+	$(document).ready(function () {
+		$('#formlist').ajaxForm({
+			beforeSubmit: function (arr, $form, options) {
+				$("#formlist").attr("action", "${paths.staticWeb}mailing");
+				displaySpinner();
+				disableSubmitButton();
+			},
+			error: function (response) {
+				$(".errormsg").remove();
+				if (response.status === 400) {
+					var arr = response.responseText.split("\n");
+					var text = '$errorMsgAddress' + "<br><br>";
+					for (i = 0; i < arr.length; i++) {
+						var syntax = arr[i].indexOf("ADDRESS_SYNTAX_NOT_CORRECT:");
+						if (syntax != -1) {
+							var mail = arr[i].indexOf(":");
+							text += arr[i].substring(mail + 1) + "<br>";
+						}
+					}
+					showWarning(text);
+				} else if (response.status === 502) {
+					showWarning('$errorMsgVerteiler');
+				} else {
+					showWarning('$errorMsg');
+				}
+				hideSpinner();
+				enableSubmitButton();
+			},
+			success: function () {
+				$(".errormsg").remove();
+				hideSpinner();
+				enableSubmitButton();
+				$('#main_content').hide();
+				$('#success').show();
+			}
+		});
+	});
+})();
