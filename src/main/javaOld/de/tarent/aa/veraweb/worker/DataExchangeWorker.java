c5ae7aa19290e94a2e42fb34329791ab77d0b311
mirabilos <t.glaser@tarent.de>
2017-10-20 17:44:47 +0200

    fix indent

--- src/main/javaOld/de/tarent/aa/veraweb/worker/DataExchangeWorker.java
+++ src/main/javaOld/de/tarent/aa/veraweb/worker/DataExchangeWorker.java
@@ -228,9 +228,8 @@ public class DataExchangeWorker {
 
                     // Beschränkung auf Kategorie, wenn Benutzer eine ausgewählt hat
                     Integer categoryId = null;
-                    if (EXPORT_FILTER_CATEGORY
-                            .equals(filter)) // category == 0 bedeutet: in irgendeiner Kategorie, = -1 bedeutet: in keiner Kategorie
-                    {
+                    if (EXPORT_FILTER_CATEGORY.equals(filter)) {
+                        // category == 0 bedeutet: in irgendeiner Kategorie, = -1 bedeutet: in keiner Kategorie
                         categoryId = category;
                     }
 
@@ -241,16 +240,14 @@ public class DataExchangeWorker {
 
                     // Dann exportieren
                     exporter.startExport();
-                    if (EXPORT_FILTER_EVENT
-                            .equals(filter)) // event == 0 bedeutet: in irgendeiner Veranstaltung, = -1 bedeutet: in keiner Veranstaltung
-                    {
+                    if (EXPORT_FILTER_EVENT.equals(filter)) {
+                        // event == 0 bedeutet: in irgendeiner Veranstaltung, = -1 bedeutet: in keiner Veranstaltung
                         exportEvent(database, event, exporter, orgUnit);
-                    } else if (EXPORT_FILTER_CATEGORY
-                            .equals(filter)) // category == 0 bedeutet: in irgendeiner Kategorie, = -1 bedeutet: in keiner Kategorie
-                    {
+                    } else if (EXPORT_FILTER_CATEGORY.equals(filter)) {
+                        // category == 0 bedeutet: in irgendeiner Kategorie, = -1 bedeutet: in keiner Kategorie
                         exportCategory(database, category, exporter, orgUnit);
-                    } else // guter Default?
-                    {
+                    } else {
+                        // guter Default?
                         exportAll(database, exporter, orgUnit);
                     }
                     exporter.endExport();
