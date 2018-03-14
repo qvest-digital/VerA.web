429b34247151c900a88dd50c83f8afdf7864075b
mirabilos <t.glaser@tarent.de>
2018-03-14 19:16:27 +0100

    simplify

--- release/Docker/httpd/start.sh
+++ release/Docker/httpd/start.sh
@@ -1,9 +1,7 @@
-#!/bin/bash
-if [ ! -d "$APACHE_RUN_DIR" ]; then
-	mkdir "$APACHE_RUN_DIR"
-	chown $APACHE_RUN_USER:$APACHE_RUN_GROUP "$APACHE_RUN_DIR"
+#!/bin/sh
+if ! test -d "$APACHE_RUN_DIR"; then
+	mkdir -p "$APACHE_RUN_DIR"
+	chown "$APACHE_RUN_USER:$APACHE_RUN_GROUP" "$APACHE_RUN_DIR"
 fi
-if [ -f "$APACHE_PID_FILE" ]; then
-	rm "$APACHE_PID_FILE"
-fi
+rm -f "$APACHE_PID_FILE"
 exec /usr/sbin/apache2 -DFOREGROUND -k start
