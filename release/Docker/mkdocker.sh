5c5703ffe09454c453f9cbcf0754ff102bf3c9ae
mirabilos <t.glaser@tarent.de>
2018-03-14 20:29:54 +0100

    try to do all sorts of install, release, etc.

--- release/Docker/mkdocker.sh
+++ release/Docker/mkdocker.sh
@@ -1,11 +1,16 @@
 #!/usr/bin/env mksh
 set -ex
 cd "$(dirname "$0")"
+tag=$1
 
 doone() (
 	cd "$1"
 	docker build -t veraweb-tools.lan.tarent.de:5000/"$2" .
 	docker push veraweb-tools.lan.tarent.de:5000/"$2"
+	[[ -n $tag ]] || return 0
+	docker tag veraweb-tools.lan.tarent.de:5000/"$2" \
+	    veraweb-tools.lan.tarent.de:5000/"$2:$tag"
+	docker push veraweb-tools.lan.tarent.de:5000/"$2:$tag"
 )
 
 doone core veraweb-core
@@ -14,3 +19,4 @@ doone vwor veraweb-rest-api
 # extra
 doone httpd veraweb-httpd
 doone ldap veraweb-ldap
+exit 0
