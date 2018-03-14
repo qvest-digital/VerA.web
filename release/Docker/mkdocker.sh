cdd44ecaedead6e29db79263900911186afe7a91
mirabilos <t.glaser@tarent.de>
2018-03-14 20:39:31 +0100

    image export

--- release/Docker/mkdocker.sh
+++ release/Docker/mkdocker.sh
@@ -2,6 +2,7 @@
 set -ex
 cd "$(dirname "$0")"
 tag=$1
+set -A ours
 
 doone() (
 	cd "$1"
@@ -11,6 +12,7 @@ doone() (
 	docker tag veraweb-tools.lan.tarent.de:5000/"$2" \
 	    veraweb-tools.lan.tarent.de:5000/"$2:$tag"
 	docker push veraweb-tools.lan.tarent.de:5000/"$2:$tag"
+	set -A ours -- "${ours[@]}" veraweb-tools.lan.tarent.de:5000/"$2:$tag"
 )
 
 doone core veraweb-core
@@ -19,4 +21,22 @@ doone vwor veraweb-rest-api
 # extra
 doone httpd veraweb-httpd
 doone ldap veraweb-ldap
-exit 0
+
+# release export
+[[ -n $tag ]] || exit 0
+
+set -A externals -- \
+    burberius/osiam-simple:2.5.1 \
+    postgres:9.3
+
+for x in "${externals[@]}"; do
+	docker pull "$x"
+done
+
+mkdir -p "/var/www/$tag"
+
+for x in "${ours[@]}" "${externals[@]}"; do
+	docker save -o "/var/www/$tag/${x##*/}.img" "$x"
+	gzip -n9 "/var/www/$tag/${x##*/}.img" &
+done
+wait
