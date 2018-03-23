e573079863f92ed9fc86c373f4da8b39309c1579
mirabilos <t.glaser@tarent.de>
2018-03-23 20:41:16 +0100

    mvn com.mycila:license-maven-plugin:3.0:format

--- src/main/java/de/tarent/octopus/request/servlet/HttpHelper.java
+++ src/main/java/de/tarent/octopus/request/servlet/HttpHelper.java
@@ -2,7 +2,8 @@ package de.tarent.octopus.request.servlet;
 
 /*-
  * tarent-octopus, Webservice Data Integrator and Application Server
- * Copyright © 2002–2017 tarent solutions GmbH and its contributors
+ * Copyright © 2002–2018 tarent solutions GmbH and its contributors
+ * Copyright © 2015, 2017, 2018 mirabilos (t.glaser@tarent.de)
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
