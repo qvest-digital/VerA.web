ffee612413492684bf937cc6702f18ce7466d746
mirabilos <t.glaser@tarent.de>
2018-03-24 00:08:52 +0100

    reformat

--- src/main/java/de/tarent/octopus/content/TcAll.java
+++ src/main/java/de/tarent/octopus/content/TcAll.java
@@ -23,39 +23,29 @@ package de.tarent.octopus.content;
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  * THE SOFTWARE.
  */
-import java.io.File;
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.LinkedHashMap;
-import java.util.List;
-import java.util.Map;
 
+import de.tarent.octopus.client.OctopusTask;
 import de.tarent.octopus.config.TcCommonConfig;
 import de.tarent.octopus.config.TcConfig;
 import de.tarent.octopus.config.TcModuleConfig;
 import de.tarent.octopus.config.TcPersonalConfig;
 import de.tarent.octopus.request.TcRequest;
-import de.tarent.octopus.server.OctopusContext;
 import de.tarent.octopus.server.Closeable;
-import java.util.ArrayList;
-import de.tarent.octopus.client.OctopusTask;
+import de.tarent.octopus.server.OctopusContext;
+
+import java.io.File;
+import java.util.*;
 
 /**
  * Kontainer zur Speicherung von TcContent, TcRequest und TcConfig.
  */
-public class TcAll
-    implements OctopusContext {
-
+public class TcAll implements OctopusContext {
 	private TcContent content;
 	private TcConfig config;
 	private TcRequest request;
 
 	/**
-	 * Erzeugt ein TcAll Object.
-	 *
-	 * @param tcRequest
-	 * @param tcContent
-	 * @param tcConfig
+	 * Erzeugt ein TcAll-Objekt
 	 */
 	public TcAll(TcRequest tcRequest, TcContent tcContent, TcConfig tcConfig) {
 		this.request = tcRequest;
@@ -64,9 +54,8 @@ public class TcAll
 	}
 
 	/**
-	 * Erzeugt ein TcAll Object mit dem übergebenem Request und einem neuen (leeren) Content.
+	 * Erzeugt ein TcAll-Objekt mit dem übergebenem Request und einem neuen (leeren) Content.
 	 *
-	 * @param requestmap
 	 * @param requesttype siehe de.tarent.octopus.request.HttpHelper.REQUEST_TYPE_*
 	 */
 	public TcAll(Map requestmap, int requesttype) {
@@ -77,85 +66,73 @@ public class TcAll
 		this.config = new TcConfig_Clone();
 	}
 
-    public Object getContextField(String fieldName) {
-        // Aus dem Content
-        if (fieldName.startsWith(CONTENT_FIELD_PREFIX)) {
-            return contentAsObject(fieldName.substring(CONTENT_FIELD_PREFIX.length()));
-        }
+	public Object getContextField(String fieldName) {
+		if (fieldName.startsWith(CONTENT_FIELD_PREFIX)) {
+			// Aus dem Content
+			return contentAsObject(fieldName.substring(CONTENT_FIELD_PREFIX.length()));
+		} else if (fieldName.startsWith(REQUEST_FIELD_PREFIX)) {
+			// Aus dem Request
+			return requestAsObject(fieldName.substring(REQUEST_FIELD_PREFIX.length()));
+		} else if (fieldName.startsWith(SESSION_FIELD_PREFIX)) {
+			// Aus der Session
+			return sessionAsObject(fieldName.substring(SESSION_FIELD_PREFIX.length()));
+		} else if (fieldName.startsWith(CONFIG_FIELD_PREFIX)) {
+			// Aus der Config
+			return moduleConfig().getParamAsObject(fieldName.substring(CONFIG_FIELD_PREFIX.length()));
+		}
 
-        // Aus dem Request
-        else if (fieldName.startsWith(REQUEST_FIELD_PREFIX)) {
-            return requestAsObject(fieldName.substring(REQUEST_FIELD_PREFIX.length()));
-        }
+		// DEFAULT, ohne Prefix:
+		// Wenn vorhanden aus dem Content, sonst aus dem Request oder der Session.
+		if (null != contentAsObject(fieldName))
+			return contentAsObject(fieldName);
+		else if (null != requestAsObject(fieldName))
+			return requestAsObject(fieldName);
+		else
+			return sessionAsObject(fieldName);
+	}
 
-        // Aus der Session
-        else if (fieldName.startsWith(SESSION_FIELD_PREFIX)) {
-            return sessionAsObject(fieldName.substring(SESSION_FIELD_PREFIX.length()));
-        }
+	public void setContextField(String fieldName, Object value) {
+		if (fieldName.startsWith(CONTENT_FIELD_PREFIX)) {
+			// In den Content
+			setContent(fieldName.substring(CONTENT_FIELD_PREFIX.length()), value);
+		} else if (fieldName.startsWith(REQUEST_FIELD_PREFIX)) {
+			// In den Request - IST NICHT ERLAUBT
+			throw new RuntimeException(
+			    "Anfragefehler: Setzen von Parametern im Request ist nicht erlaubt.");
+		} else if (fieldName.startsWith(SESSION_FIELD_PREFIX)) {
+			// In die Session
+			setSession(fieldName.substring(SESSION_FIELD_PREFIX.length()), value);
+		} else if (fieldName.startsWith(CONFIG_FIELD_PREFIX)) {
+			// In die Config - IST NICHT ERLAUBT
+			throw new RuntimeException(
+			    "Anfragefehler: Setzen von Parametern der Config ist nicht erlaubt.");
+		} else {
+			// DEFAULT, ohne Prefix:
+			// In den Content
+			setContent(fieldName, value);
+		}
+	}
 
-        // Aus der Config
-        else if (fieldName.startsWith(CONFIG_FIELD_PREFIX)) {
-            return moduleConfig().getParamAsObject(fieldName.substring(CONFIG_FIELD_PREFIX.length()));
-        }
+	public OctopusTask getTask(String taskName) {
+		return request.getTask(taskName);
+	}
 
-        // DEFAULT, ohne Prefix:
-        // Wenn vorhanden aus dem Content, sonst aus dem Request oder der Session.
-        if (null != contentAsObject(fieldName))
-            return contentAsObject(fieldName);
-        else if (null != requestAsObject(fieldName))
-            return requestAsObject(fieldName);
-        else
-            return sessionAsObject(fieldName);
-    }
+	public void addCleanupCode(Closeable closeable) {
+		addCleanupCodeInternal(closeable);
+	}
 
-    public void setContextField(String fieldName, Object value) {
-        // In den Content
-        if (fieldName.startsWith(CONTENT_FIELD_PREFIX)) {
-            setContent(fieldName.substring(CONTENT_FIELD_PREFIX.length()), value);
-        }
+	public void addCleanupCode(Runnable runnable) {
+		addCleanupCodeInternal(runnable);
+	}
 
-        // In den Request - IST NICHT ERLAUBT
-        else if (fieldName.startsWith(REQUEST_FIELD_PREFIX)) {
-            throw new RuntimeException("Anfragefehler: Setzen von Parametern im Request ist nicht erlaubt.");
-        }
-
-        // In die Session
-        else if (fieldName.startsWith(SESSION_FIELD_PREFIX)) {
-            setSession(fieldName.substring(SESSION_FIELD_PREFIX.length()), value);
-        }
-
-        // In die Config - IST NICHT ERLAUBT
-        else if (fieldName.startsWith(CONFIG_FIELD_PREFIX)) {
-            throw new RuntimeException("Anfragefehler: Setzen von Parametern der Config ist nicht erlaubt.");
-        }
-
-        // DEFAULT, ohne Prefix:
-        // In den Content
-        else {
-            setContent(fieldName, value);
-        }
-    }
-
-    public OctopusTask getTask(String taskName) {
-        return request.getTask(taskName);
-    }
-
-    public void addCleanupCode(Closeable closeable) {
-        addCleanupCodeInternal(closeable);
-    }
-
-    public void addCleanupCode(Runnable runnable) {
-        addCleanupCodeInternal(runnable);
-    }
-
-    protected void addCleanupCodeInternal(Object cleanable) {
-        List cleanableList = (List)contentAsObject(CLEANUP_OBJECT_LIST);
-        if (cleanableList == null) {
-            cleanableList = new ArrayList();
-            setContent(CLEANUP_OBJECT_LIST, cleanableList);
-        }
-        cleanableList.add(cleanable);
-    }
+	protected void addCleanupCodeInternal(Object cleanable) {
+		List cleanableList = (List)contentAsObject(CLEANUP_OBJECT_LIST);
+		if (cleanableList == null) {
+			cleanableList = new ArrayList();
+			setContent(CLEANUP_OBJECT_LIST, cleanableList);
+		}
+		cleanableList.add(cleanable);
+	}
 
 	/**
 	 * @return TcContent-Object
@@ -319,7 +296,7 @@ public class TcAll
 	private class TcConfig_Clone extends TcConfig {
 		Map session = new HashMap();
 
-		public TcConfig_Clone() {
+		TcConfig_Clone() {
 			super(null, null, null);
 		}
 
