7cf4338300c6c570d03cc514f06e01ac392ab2d1
mirabilos <t.glaser@tarent.de>
2018-03-23 23:44:22 +0100

    reformat

--- src/main/java/de/tarent/octopus/request/TcTaskList.java
+++ src/main/java/de/tarent/octopus/request/TcTaskList.java
@@ -23,16 +23,16 @@ package de.tarent.octopus.request;
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  * THE SOFTWARE.
  */
+
 import de.tarent.octopus.config.TcModuleConfig;
 import de.tarent.octopus.content.TcPortDefinition;
 import de.tarent.octopus.util.Xml;
+import org.w3c.dom.Element;
 
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
-
-import org.w3c.dom.Element;
 
 /**
  * Klasse zur Repräsentation einer Sammlung von Tasks
@@ -40,72 +40,73 @@ import org.w3c.dom.Element;
  * @author <a href="mailto:mancke@mancke-software.de">Sebastian Mancke</a>, <b>tarent GmbH</b>
  */
 public class TcTaskList {
-    protected String nameSpace;
-    protected Map tasks = new HashMap();
-    protected String description;
+	protected String nameSpace;
+	protected Map tasks = new HashMap();
+	protected String description;
 
-    public TcTaskList() {
-    }
+	public TcTaskList() {
+	}
 
-    public TcTask getTask(String name) {
-        return (TcTask) tasks.get(name);
-    }
+	public TcTask getTask(String name) {
+		return (TcTask)tasks.get(name);
+	}
 
-    /**
-     * Auslesen der TaskInformationen
-     * Kopie auf TcCommon Config
-     * Soll später nur noch hier existieren.
-     */
-    public void parseTasks(Element tasksNode, TcModuleConfig moduleConfig) {
-        // Evtl. auch nicht vorhanden
-        nameSpace = tasksNode.getAttribute("name");
-        if (nameSpace == null || nameSpace.length() == 0)
-            nameSpace = moduleConfig.getName();
+	/**
+	 * Auslesen der TaskInformationen
+	 * Kopie auf TcCommon Config
+	 * Soll später nur noch hier existieren.
+	 */
+	public void parseTasks(Element tasksNode, TcModuleConfig moduleConfig) {
+		// Evtl. auch nicht vorhanden
+		nameSpace = tasksNode.getAttribute("name");
+		if (nameSpace == null || nameSpace.length() == 0)
+			nameSpace = moduleConfig.getName();
 
-        Element currTaskElement = Xml.getFirstChildElement(tasksNode);
-        while (currTaskElement != null) {
-            TcTask currTask = new TcTask(this, currTaskElement, moduleConfig);
-            tasks.put(currTask.getName(), currTask);
+		Element currTaskElement = Xml.getFirstChildElement(tasksNode);
+		while (currTaskElement != null) {
+			TcTask currTask = new TcTask(this, currTaskElement, moduleConfig);
+			tasks.put(currTask.getName(), currTask);
 
-            currTaskElement = Xml.getNextSiblingElement(currTaskElement);
-        }
-    }
+			currTaskElement = Xml.getNextSiblingElement(currTaskElement);
+		}
+	}
 
-    public TcPortDefinition getPortDefinition() {
-        TcPortDefinition pd = new TcPortDefinition(nameSpace, description);
-        for (Iterator e = tasks.values().iterator(); e.hasNext();) {
-            TcTask currTask = (TcTask) e.next();
-            pd.addOperation(currTask.getOperationDefinition());
-        }
-        return pd;
-    }
+	public TcPortDefinition getPortDefinition() {
+		TcPortDefinition pd = new TcPortDefinition(nameSpace, description);
+		for (Iterator e = tasks.values().iterator(); e.hasNext(); ) {
+			TcTask currTask = (TcTask)e.next();
+			pd.addOperation(currTask.getOperationDefinition());
+		}
+		return pd;
+	}
 
-    /**
-     * Liefert eine Map.
-     * Diese enthält für jedes Task, das fehlerhaft ist einen Eintrag
-     * mit dem Tasknamen als String Key und Fehlerliste als String Vector.
-     * Oder null, wenn kein Task fehlerhalt ist.
-     *
-     * @return Map mit Tasknamen als String Keys und Vectoren von Strings als Values, oder null
-     */
-    public Map getErrors() {
-        Map out = new HashMap();
-        for (Iterator e = tasks.values().iterator(); e.hasNext();) {
-            TcTask currTask = (TcTask) e.next();
-            List errList = currTask.getTaskErrors();
-            if (errList.size() > 0)
-                out.put(currTask.getName(), errList);
-        }
-        if (out.size() > 0)
-            return out;
-        return null;
-    }
+	/**
+	 * Liefert eine Map.
+	 * Diese enthält für jedes Task, das fehlerhaft ist einen Eintrag
+	 * mit dem Tasknamen als String Key und Fehlerliste als String Vector.
+	 * Oder null, wenn kein Task fehlerhalt ist.
+	 *
+	 * @return Map mit Tasknamen als String Keys und Vectoren von Strings als Values, oder null
+	 */
+	public Map getErrors() {
+		Map out = new HashMap();
+		for (Iterator e = tasks.values().iterator(); e.hasNext(); ) {
+			TcTask currTask = (TcTask)e.next();
+			List errList = currTask.getTaskErrors();
+			if (errList.size() > 0)
+				out.put(currTask.getName(), errList);
+		}
+		if (out.size() > 0)
+			return out;
+		return null;
+	}
 
-    /**
-     * Liefert die Tasks
-     * @return Map
-     */
-    public Iterator getTasksKeys() {
-        return tasks.keySet().iterator();
-    }
+	/**
+	 * Liefert die Tasks
+	 *
+	 * @return Map
+	 */
+	public Iterator getTasksKeys() {
+		return tasks.keySet().iterator();
+	}
 }
