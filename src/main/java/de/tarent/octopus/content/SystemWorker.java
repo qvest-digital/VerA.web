09a47a6ccb5cc598f4e1982e3a0e3e4f06d5d3df
mirabilos <t.glaser@tarent.de>
2018-03-23 22:01:22 +0100

    reformat

--- src/main/java/de/tarent/octopus/content/SystemWorker.java
+++ src/main/java/de/tarent/octopus/content/SystemWorker.java
@@ -23,6 +23,7 @@ package de.tarent.octopus.content;
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  * THE SOFTWARE.
  */
+
 import de.tarent.octopus.config.TcConfig;
 import de.tarent.octopus.config.TcModuleConfig;
 import de.tarent.octopus.request.TcRequest;
@@ -35,81 +36,90 @@ import de.tarent.octopus.util.CVS;
  * @author mikel
  */
 public class SystemWorker implements TcContentWorker {
-    //
-    // Schnittstelle TcContentWorker
-    //
-    /**
-     * Diese Methode wird nach Erzeugung des Workers aufgerufen, so dass dieser
-     * sich im Kontext seines Moduls konfigurieren kann.
-     *
-     * @param config Modulkonfiguration.
-     * @see de.tarent.octopus.content.TcContentWorker#init(de.tarent.octopus.config.TcModuleConfig)
-     */
-    public void init(TcModuleConfig config) {
-    }
+	//
+	// Schnittstelle TcContentWorker
+	//
 
-    /**
-     * Abarbeiten einer Action mit diesem ContentWorker
-     * Die Ergebnisse werden in dem tcContent-Kontainer abgelegt.
-     * Ein ContentWorker kann für mehrere Actions zuständig sein.
-     *
-     * @param tcConfig Konfiguration
-     * @param actionName Name der Aktion, die von diesem Worker ausgeführt werden soll.
-     * @param tcRequest Die Anfragedaten
-     * @param tcContent Der Content-Kontainer, in dem die Daten abgelegt werden können.
-     * @return String mit einem Statuscode z.B. ok oder error
-     * @see de.tarent.octopus.content.TcContentWorker#doAction(de.tarent.octopus.config.TcConfig, java.lang.String, de.tarent.octopus.request.TcRequest, de.tarent.octopus.content.TcContent)
-     */
-    public String doAction(TcConfig tcConfig, String actionName, TcRequest tcRequest, TcContent tcContent)
-            throws TcContentProzessException {
-        // ACTION_RELOAD_MODULE
-        if (ACTION_RELOAD_MODULE.equals(actionName)) {
-            String moduleName = tcConfig.getModuleConfig().getName();
-            tcConfig.getCommonConfig().deregisterModule(moduleName);
+	/**
+	 * Diese Methode wird nach Erzeugung des Workers aufgerufen, so dass dieser
+	 * sich im Kontext seines Moduls konfigurieren kann.
+	 *
+	 * @param config Modulkonfiguration.
+	 *
+	 * @see de.tarent.octopus.content.TcContentWorker#init(de.tarent.octopus.config.TcModuleConfig)
+	 */
+	public void init(TcModuleConfig config) {
+	}
 
-            TcModuleConfig moduleConfig = tcConfig.getCommonConfig().getModuleConfig(moduleName);
-            return moduleConfig != null ? RESULT_ok : RESULT_error;
-        } else {
-            throw new TcContentProzessException(
-                    "Nicht unterstützte Aktion im SystemWorker: " + actionName);
-        }
-    }
+	/**
+	 * Abarbeiten einer Action mit diesem ContentWorker
+	 * Die Ergebnisse werden in dem tcContent-Kontainer abgelegt.
+	 * Ein ContentWorker kann für mehrere Actions zuständig sein.
+	 *
+	 * @param tcConfig   Konfiguration
+	 * @param actionName Name der Aktion, die von diesem Worker ausgeführt werden soll.
+	 * @param tcRequest  Die Anfragedaten
+	 * @param tcContent  Der Content-Kontainer, in dem die Daten abgelegt werden können.
+	 *
+	 * @return String mit einem Statuscode z.B. ok oder error
+	 *
+	 * @see de.tarent.octopus.content.TcContentWorker#doAction(de.tarent.octopus.config.TcConfig, java.lang.String,
+	 *     de.tarent.octopus.request.TcRequest, de.tarent.octopus.content.TcContent)
+	 */
+	public String doAction(TcConfig tcConfig, String actionName, TcRequest tcRequest, TcContent tcContent)
+	throws TcContentProzessException {
+		// ACTION_RELOAD_MODULE
+		if (ACTION_RELOAD_MODULE.equals(actionName)) {
+			String moduleName = tcConfig.getModuleConfig().getName();
+			tcConfig.getCommonConfig().deregisterModule(moduleName);
 
-    /**
-     * Liefert eine Beschreibgung der Actions und deren Eingabeparameter,
-     * die von diesem Worker bereit gestellt werden.
-     *
-     * @return Eine Abstrakte Beschreibung der Methoden und Parameter
-     * @see de.tarent.octopus.content.TcContentWorker#getWorkerDefinition()
-     */
-    public TcPortDefinition getWorkerDefinition() {
-        TcPortDefinition port =
-            new TcPortDefinition(
-                SystemWorker.class.getName(),
-                "Worker für Systemfunktionen");
+			TcModuleConfig moduleConfig = tcConfig.getCommonConfig().getModuleConfig(moduleName);
+			return moduleConfig != null ? RESULT_ok : RESULT_error;
+		} else {
+			throw new TcContentProzessException(
+			    "Nicht unterstützte Aktion im SystemWorker: " + actionName);
+		}
+	}
 
-        TcOperationDefinition operation = port.addOperation(ACTION_RELOAD_MODULE, "Neuladen des aufrufenden Moduls");
-        operation.setInputMessage();
-        operation.setOutputMessage();
+	/**
+	 * Liefert eine Beschreibgung der Actions und deren Eingabeparameter,
+	 * die von diesem Worker bereit gestellt werden.
+	 *
+	 * @return Eine Abstrakte Beschreibung der Methoden und Parameter
+	 *
+	 * @see de.tarent.octopus.content.TcContentWorker#getWorkerDefinition()
+	 */
+	public TcPortDefinition getWorkerDefinition() {
+		TcPortDefinition port =
+		    new TcPortDefinition(
+			SystemWorker.class.getName(),
+			"Worker für Systemfunktionen");
+
+		TcOperationDefinition operation = port.addOperation(ACTION_RELOAD_MODULE,
+		    "Neuladen des aufrufenden Moduls");
+		operation.setInputMessage();
+		operation.setOutputMessage();
 //            .addPart("url", TcMessageDefinition.TYPE_SCALAR, "Url des Systems mit Sessioninformationen.")
 //            .addPart("jsessionid", TcMessageDefinition.TYPE_SCALAR, "Die Session Id.")
 //            .addPart("paths", TcMessageDefinition.TYPE_STRUCT, "Pfade z.B. zu den Templateverzeichnissen.");
 
-        return port;
-    }
+		return port;
+	}
 
-    /**
-     * Diese Methode liefert einen Versionseintrag.
-     *
-     * @return Version des Workers.
-     * @see de.tarent.octopus.content.TcContentWorker#getVersion()
-     */
-    public String getVersion() {
-        return CVS.getContent("$Revision: 1.1.1.1 $") + " (" + CVS.getContent("$Date: 2005/11/21 13:33:37 $") + ')';
-    }
+	/**
+	 * Diese Methode liefert einen Versionseintrag.
+	 *
+	 * @return Version des Workers.
+	 *
+	 * @see de.tarent.octopus.content.TcContentWorker#getVersion()
+	 */
+	public String getVersion() {
+		return CVS.getContent("$Revision: 1.1.1.1 $") + " (" + CVS.getContent("$Date: 2005/11/21 13:33:37 $") +
+		    ')';
+	}
 
-    //
-    // Konstanten
-    //
-    final static String ACTION_RELOAD_MODULE = "reloadModule";
+	//
+	// Konstanten
+	//
+	final static String ACTION_RELOAD_MODULE = "reloadModule";
 }
--- src/main/java/de/tarent/octopus/content/TcPutParams.java
+++ src/main/java/de/tarent/octopus/content/TcPutParams.java
@@ -23,8 +23,6 @@ package de.tarent.octopus.content;
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  * THE SOFTWARE.
  */
-import java.util.HashMap;
-import java.util.Map;
 
 import de.tarent.octopus.config.TcCommonConfig;
 import de.tarent.octopus.config.TcConfig;
@@ -32,91 +30,100 @@ import de.tarent.octopus.config.TcModuleConfig;
 import de.tarent.octopus.request.TcRequest;
 import de.tarent.octopus.util.CVS;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Worker, der die Felder des Requests in den Content schieben kann
  *
  * @author <a href="mailto:mancke@mancke-software.de">Sebastian Mancke</a>, <b>tarent GmbH</b>
  */
 public class TcPutParams implements TcContentWorker {
-    public static String CONTENT_PREFIX = "request";
+	public static String CONTENT_PREFIX = "request";
 
-    /**
-     * Diese Methode wird nach Erzeugung des Workers aufgerufen, so dass dieser
-     * sich im Kontext seines Moduls konfigurieren kann.
-     *
-     * @param config Modulkonfiguration.
-     * @see de.tarent.octopus.content.TcContentWorker#init(de.tarent.octopus.config.TcModuleConfig)
-     */
-    public void init(TcModuleConfig config) {
-    }
+	/**
+	 * Diese Methode wird nach Erzeugung des Workers aufgerufen, so dass dieser
+	 * sich im Kontext seines Moduls konfigurieren kann.
+	 *
+	 * @param config Modulkonfiguration.
+	 *
+	 * @see de.tarent.octopus.content.TcContentWorker#init(de.tarent.octopus.config.TcModuleConfig)
+	 */
+	public void init(TcModuleConfig config) {
+	}
 
-    public String doAction(TcConfig tcConfig, String actionName, TcRequest tcRequest, TcContent tcContent)
-        throws TcContentProzessException {
+	public String doAction(TcConfig tcConfig, String actionName, TcRequest tcRequest, TcContent tcContent)
+	throws TcContentProzessException {
 
-        String returnStatus = RESULT_error;
+		String returnStatus = RESULT_error;
 
-        if ("putMinimal".equals(actionName)) {
-            returnStatus = putMinimal(tcConfig, tcRequest, tcContent);
-        } else if ("putAll".equals(actionName)) {
-            returnStatus = putAll(tcConfig, tcRequest, tcContent);
-        } else {
-            throw new TcContentProzessException(
-                "Nicht unterstützte action im Worker 'TcPutParams': " + actionName);
-        }
-        return returnStatus;
-    }
+		if ("putMinimal".equals(actionName)) {
+			returnStatus = putMinimal(tcConfig, tcRequest, tcContent);
+		} else if ("putAll".equals(actionName)) {
+			returnStatus = putAll(tcConfig, tcRequest, tcContent);
+		} else {
+			throw new TcContentProzessException(
+			    "Nicht unterstützte action im Worker 'TcPutParams': " + actionName);
+		}
+		return returnStatus;
+	}
 
-    public String putMinimal(TcConfig tcConfig, TcRequest tcRequest, TcContent tcContent) {
-        TcCommonConfig commonConfig = tcConfig.getCommonConfig();
+	public String putMinimal(TcConfig tcConfig, TcRequest tcRequest, TcContent tcContent) {
+		TcCommonConfig commonConfig = tcConfig.getCommonConfig();
 
-        tcContent.setField("url", tcRequest.get("encodedUrl"));
-        tcContent.setField("jsessionid", tcRequest.get("jsessionid"));
+		tcContent.setField("url", tcRequest.get("encodedUrl"));
+		tcContent.setField("jsessionid", tcRequest.get("jsessionid"));
 
-        Map paths = new HashMap();
-        paths.put("templatesRelative", "");
-        paths.put("staticWeb", tcConfig.getRelativeWebRootPath());
-        if (commonConfig.getDefaultModuleName() != null)
-            paths.put("defaultStaticWeb", commonConfig.getRelativeWebRootPath(commonConfig.getDefaultModuleName()));
-        tcContent.setField("paths", paths);
+		Map paths = new HashMap();
+		paths.put("templatesRelative", "");
+		paths.put("staticWeb", tcConfig.getRelativeWebRootPath());
+		if (commonConfig.getDefaultModuleName() != null)
+			paths.put("defaultStaticWeb",
+			    commonConfig.getRelativeWebRootPath(commonConfig.getDefaultModuleName()));
+		tcContent.setField("paths", paths);
 
-        return RESULT_ok;
-    }
+		return RESULT_ok;
+	}
 
-    public String putAll(TcConfig tcConfig, TcRequest tcRequest, TcContent tcContent) {
-        tcContent.setField(CONTENT_PREFIX, tcRequest.getRequestParameters());
-        return RESULT_ok;
-    }
+	public String putAll(TcConfig tcConfig, TcRequest tcRequest, TcContent tcContent) {
+		tcContent.setField(CONTENT_PREFIX, tcRequest.getRequestParameters());
+		return RESULT_ok;
+	}
 
-    public TcPortDefinition getWorkerDefinition() {
-        TcPortDefinition port =
-            new TcPortDefinition(
-                "de.tarent.octopus.content.TcPutRequestParams",
-                "Worker, der die Felder des TcRequests in den Content schieben kann.");
+	public TcPortDefinition getWorkerDefinition() {
+		TcPortDefinition port =
+		    new TcPortDefinition(
+			"de.tarent.octopus.content.TcPutRequestParams",
+			"Worker, der die Felder des TcRequests in den Content schieben kann.");
 
-        TcOperationDefinition putMinimal = port.addOperation("putMinimal", "Setzen weniger notwendiger Felder.");
+		TcOperationDefinition putMinimal = port.addOperation("putMinimal",
+		    "Setzen weniger notwendiger Felder.");
 
-        putMinimal.setInputMessage();
-        putMinimal
-            .setOutputMessage()
-            .addPart("url", TcMessageDefinition.TYPE_SCALAR, "Url des Systems mit Sessioninformationen.")
-            .addPart("jsessionid", TcMessageDefinition.TYPE_SCALAR, "Die Session Id.")
-            .addPart("paths", TcMessageDefinition.TYPE_STRUCT, "Pfade z.B. zu den Templateverzeichnissen.");
+		putMinimal.setInputMessage();
+		putMinimal
+		    .setOutputMessage()
+		    .addPart("url", TcMessageDefinition.TYPE_SCALAR, "Url des Systems mit Sessioninformationen.")
+		    .addPart("jsessionid", TcMessageDefinition.TYPE_SCALAR, "Die Session Id.")
+		    .addPart("paths", TcMessageDefinition.TYPE_STRUCT, "Pfade z.B. zu den Templateverzeichnissen.");
 
-        TcOperationDefinition putAll = port.addOperation("putAll", "Setzen aller Felder des TcRequest Objektes.");
+		TcOperationDefinition putAll = port.addOperation("putAll",
+		    "Setzen aller Felder des TcRequest Objektes.");
 
-        putAll.setInputMessage();
-        putAll.setOutputMessage().addPart(CONTENT_PREFIX, TcMessageDefinition.TYPE_STRUCT, "Parameter des Request.");
+		putAll.setInputMessage();
+		putAll.setOutputMessage().addPart(CONTENT_PREFIX, TcMessageDefinition.TYPE_STRUCT,
+		    "Parameter des Request.");
 
-        return port;
-    }
+		return port;
+	}
 
-    /**
-     * Diese Methode liefert einen Versionseintrag.
-     *
-     * @return Version des Workers.
-     * @see de.tarent.octopus.content.TcContentWorker#getVersion()
-     */
-    public String getVersion() {
-        return CVS.getContent("$Revision: 1.3 $") + " (" + CVS.getContent("$Date: 2006/02/23 15:07:57 $") + ')';
-    }
+	/**
+	 * Diese Methode liefert einen Versionseintrag.
+	 *
+	 * @return Version des Workers.
+	 *
+	 * @see de.tarent.octopus.content.TcContentWorker#getVersion()
+	 */
+	public String getVersion() {
+		return CVS.getContent("$Revision: 1.3 $") + " (" + CVS.getContent("$Date: 2006/02/23 15:07:57 $") + ')';
+	}
 }
