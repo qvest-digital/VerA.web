9542a2df96004d42fc55313e58f1d4248eb542ed
mirabilos <t.glaser@tarent.de>
2018-03-23 21:39:06 +0100

    some cleanup

--- src/main/java/de/tarent/octopus/logging/LogFactory.java
+++ src/main/java/de/tarent/octopus/logging/LogFactory.java
@@ -41,35 +41,35 @@ import java.util.logging.*;
 
 /**
  * A simple factory which should be used to get a log instance in tarent projects.
- * This implementations use an on small configuration in a ressource properties file
+ * This implementations use an on small configuration in a resource properties file
  * /tarent-logging.properties to decide which underlaying logging system should be used.
  *
  * This way makes it possible to use apache.commmons.logging in a shared environment
  * (i.e. J2EE) and without having to use the same underlying logging system.
  *
- * For configuration, there has to be a file /tarent-logging.properties in the classpath
- * with the single property <code>logging.api</code>.
- * Possible values for this Property are: jdk (Java util logging backend),
- * log4j (Log4j backend), commons (apache commons default behavior),
- * simple (apache commons simple logger)
+ * For configuration, there has to be a file /tarent-logging.properties in the
+ * classpath with the single property <code>logging.api</code>.
+ * Possible values for this Property are:
+ * jdk (Java util logging backend), log4j (Log4j backend),
+ * commons (apache commons default behavior), simple (apache commons simple logger)
  */
 public class LogFactory {
 
-	public static final String TARENT_LOGGING_PROPERTIES = "/tarent-logging.properties";
+	private static final String TARENT_LOGGING_PROPERTIES = "/tarent-logging.properties";
 
-	public static final String LOGGING_API = "logging.api";
+	private static final String LOGGING_API = "logging.api";
 
-	public static final String LOGGING_API_JDK14 = "jdk";
-	public static final String LOGGING_API_LOG4J = "log4j";
-	public static final String LOGGING_API_COMMONS = "commons";
-	public static final String LOGGING_API_SIMPLE = "simple";
+	private static final String LOGGING_API_JDK14 = "jdk";
+	private static final String LOGGING_API_LOG4J = "log4j";
+	private static final String LOGGING_API_COMMONS = "commons";
+	private static final String LOGGING_API_SIMPLE = "simple";
 
-	protected static final int JDK14_LOGGER = 1;
-	protected static final int LOG4J_LOGGER = 2;
-	protected static final int COMMONS_LOGGER = 3;
-	protected static final int SIMPLE_LOGGER = 4;
+	private static final int JDK14_LOGGER = 1;
+	private static final int LOG4J_LOGGER = 2;
+	private static final int COMMONS_LOGGER = 3;
+	private static final int SIMPLE_LOGGER = 4;
 
-	static int logger = JDK14_LOGGER;
+	private static int CHOSEN_LOGGER = JDK14_LOGGER;
 
 	private static Logger baseLogger = null;
 	private static FileHandler fileLogHandler = null;
@@ -92,7 +92,7 @@ public class LogFactory {
 			return org.apache.commons.logging.LogFactory.getLog(clazz);
 	}
 
-	public static void loadProperties() {
+	private static void loadProperties() {
 		InputStream in = LogFactory.class.getResourceAsStream(TARENT_LOGGING_PROPERTIES);
 		if (in != null) {
 			Properties properties = new Properties();
@@ -100,22 +100,22 @@ public class LogFactory {
 				properties.load(in);
 				Object value = properties.get(LOGGING_API);
 				if (LOGGING_API_JDK14.equals(value))
-					logger = JDK14_LOGGER;
+					CHOSEN_LOGGER = JDK14_LOGGER;
 				else if (LOGGING_API_LOG4J.equals(value))
-					logger = LOG4J_LOGGER;
+					CHOSEN_LOGGER = LOG4J_LOGGER;
 				else if (LOGGING_API_COMMONS.equals(value))
-					logger = COMMONS_LOGGER;
+					CHOSEN_LOGGER = COMMONS_LOGGER;
 				else if (LOGGING_API_SIMPLE.equals(value))
-					logger = SIMPLE_LOGGER;
+					CHOSEN_LOGGER = SIMPLE_LOGGER;
 			} catch (IOException e) {
-				log("FATAL: Error while reading logging configuration from ressource: " +
+				log("FATAL: Error while reading logging configuration from resource: " +
 				    TARENT_LOGGING_PROPERTIES, e);
 			}
 		}
 	}
 
 	/**
-	 * Init. the logging.
+	 * initialise the logging.
 	 */
 	public static void initOctopusLogging(TcEnv env) throws IOException {
 		if (useJdkLogger())
@@ -124,7 +124,7 @@ public class LogFactory {
 			initLog4jOctopusLogging(env);
 	}
 
-	public static void initLog4jOctopusLogging(TcEnv env) {
+	private static void initLog4jOctopusLogging(TcEnv env) {
 		String rootPath = env.getValueAsString(TcEnv.KEY_PATHS_ROOT);
 		File f = new File(rootPath, "log4j_properties.xml");
 		if (!f.exists())
@@ -133,7 +133,7 @@ public class LogFactory {
 			DOMConfigurator.configure(f.getAbsolutePath());
 	}
 
-	public static void initJdkOctopusLogging(TcEnv env) throws IOException {
+	private static void initJdkOctopusLogging(TcEnv env) throws IOException {
 		if (baseLogger != null)
 			return;
 
@@ -145,9 +145,9 @@ public class LogFactory {
 
 		String pattern = env.getValueAsString(TcEnv.KEY_LOGGING_PATTERN);
 
-		// Get absolut pattern file.
+		// get absolute pattern file
 		if (pattern == null || pattern.trim().length() == 0) {
-			// if no pattern defined, fallback to default filename.
+			// if no pattern defined, fall back to default filename
 			String rootPath = env.getValueAsString(TcEnv.KEY_PATHS_ROOT);
 			String logPath = env.getValueAsString(TcEnv.KEY_PATHS_LOGFILE);
 			if (logPath == null || logPath.trim().length() == 0) {
@@ -160,7 +160,7 @@ public class LogFactory {
 					pattern = new File(rootPath, logPath).getAbsolutePath();
 			}
 		} else {
-			// if pattern defined use this with root and log path (optional).
+			// if pattern defined, use this with root and log path (optional)
 			String rootPath = env.getValueAsString(TcEnv.KEY_PATHS_ROOT);
 			String logPath = env.getValueAsString(TcEnv.KEY_PATHS_LOGFILE);
 			if (logPath == null || logPath.trim().length() == 0) {
@@ -175,8 +175,8 @@ public class LogFactory {
 		}
 		log(Resources.getInstance().get("REQUESTPROXY_LOG_START_LOGGING_TO", pattern), null);
 
-		// Get maximum size of logging file.
-		int loggingLimit = 4 * 1024 * 1024; // default 4 MB
+		// get maximum size of logging file
+		int loggingLimit = 4 * 1024 * 1024; // default 4 MiB
 		try {
 			String param = env.getValueAsString(TcEnv.KEY_LOGGING_LIMIT);
 			if (param != null && param.trim().equals("disabled")) {
@@ -185,10 +185,10 @@ public class LogFactory {
 				loggingLimit = Integer.parseInt(param.trim());
 			}
 		} catch (NumberFormatException e) {
-			log(Resources.getInstance().get("REQUESTPROXY_LOG_PARSEERROR_LIMIT", "4 MB"), e);
+			log(Resources.getInstance().get("REQUESTPROXY_LOG_PARSEERROR_LIMIT", "4 MiB"), e);
 		}
 
-		// Get maximum logfile count.
+		// get maximum logfile count
 		int loggingCount = 10; // default 10 files
 		try {
 			String param = env.getValueAsString(TcEnv.KEY_LOGGING_COUNT);
@@ -201,18 +201,18 @@ public class LogFactory {
 			log(Resources.getInstance().get("REQUESTPROXY_LOG_PARSEERROR_LIMIT", "10"), e);
 		}
 
-		// Init. FileHandle
+		// initialise FileHandle
 		fileLogHandler = new FileHandler(pattern, loggingLimit, loggingCount, true);
 		fileLogHandler.setEncoding("UTF-8");
 		fileLogHandler.setFormatter(new SimpleFormatter());
 		baseLogger.addHandler(fileLogHandler);
 
-		// Append optional socket logger.
+		// append optional socket logger
 		int loggingPort = -1;
 		try {
 			String param = env.get("logging.port");
 			if (param != null && param.trim().length() != 0) {
-				loggingPort = Integer.decode(param).intValue();
+				loggingPort = Integer.decode(param);
 			}
 		} catch (NumberFormatException e) {
 			log("Fehler beim Parsen des Log-Ports; benutze Default-Wert", e);
@@ -241,11 +241,12 @@ public class LogFactory {
 					String enc = handler.getEncoding();
 					if (enc == null) {
 						enc = Charset.defaultCharset().name();
-						if (enc == null) {
+						/*if (enc == null) {
 							baseLogger.severe("Logger parent(" + xlogger_parent +
 							    ") element(" + xlogger_element + ") name(" + handler +
-							    ") encoding is NULL");
-						} else if (enc.equals("US-ASCII")) {
+							    ") encoding is nonexistent");
+						} else*/
+						if (enc.equals("US-ASCII")) {
 							baseLogger.warning("Logger parent(" + xlogger_parent +
 							    ") element(" + xlogger_element +
 							    ") name(" + handler +
@@ -280,7 +281,7 @@ public class LogFactory {
 
 		baseLogger.info(Resources.getInstance().get("REQUESTPROXY_LOG_START_LOGGING"));
 
-		// Set logging.level
+		// set logging.level
 		String loggingLevel = env.getValueAsString(TcEnv.KEY_LOGGING_LEVEL);
 		try {
 			Level level = Level.parse(loggingLevel);
@@ -299,33 +300,34 @@ public class LogFactory {
 			deInitLog4jOctopusLogging();
 	}
 
-	public static void deInitLog4jOctopusLogging() {
+	private static void deInitLog4jOctopusLogging() {
 		org.apache.log4j.LogManager.shutdown();
 	}
 
-	public static void deInitJdkOctopusLogging() {
-		if (baseLogger != null)
+	private static void deInitJdkOctopusLogging() {
+		if (baseLogger != null) {
 			baseLogger.removeHandler(fileLogHandler);
-		if (portLogHandler != null)
-			baseLogger.removeHandler(portLogHandler);
+			if (portLogHandler != null)
+				baseLogger.removeHandler(portLogHandler);
+		}
 		if (fileLogHandler != null)
 			fileLogHandler.close();
 	}
 
-	static boolean useJdkLogger() {
-		return (JDK14_LOGGER == logger);
+	private static boolean useJdkLogger() {
+		return (JDK14_LOGGER == CHOSEN_LOGGER);
 	}
 
-	static boolean useLog4jLogger() {
-		return (LOG4J_LOGGER == logger);
+	private static boolean useLog4jLogger() {
+		return (LOG4J_LOGGER == CHOSEN_LOGGER);
 	}
 
-	static boolean useCommonsLogger() {
-		return (COMMONS_LOGGER == logger);
+	private static boolean useCommonsLogger() {
+		return (COMMONS_LOGGER == CHOSEN_LOGGER);
 	}
 
-	static boolean useSimpleLog() {
-		return (SIMPLE_LOGGER == logger);
+	private static boolean useSimpleLog() {
+		return (SIMPLE_LOGGER == CHOSEN_LOGGER);
 	}
 
 	public static void log(String message, Exception e) {
@@ -337,12 +339,12 @@ public class LogFactory {
 	/**
 	 * Replaces all Variables ${key} with the corresponding system property
 	 */
-	protected static String expandSystemProperties(String string) {
+	private static String expandSystemProperties(String string) {
 		int startPos = string.indexOf("${");
 		if (startPos == -1)
 			return string;
 
-		StringBuffer sb = new StringBuffer(string);
+		StringBuilder sb = new StringBuilder(string);
 		while (-1 != startPos) {
 			int endPos = sb.indexOf("}", startPos);
 			String propertyName = sb.substring(startPos + 2, endPos);
