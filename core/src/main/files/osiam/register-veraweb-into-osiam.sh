1e88f7a5c0193b15139decd0dd7bab297877ca2d
mirabilos <t.glaser@tarent.de>
2018-04-06 11:42:29 +0200

    work aroung another OSIAM incompatibility with itself:
    just upsert twice, one for each OSIAM 2.3/2.4 and 2.5, ignoring
    any errors, then check whether the result is actually written (phew!)

--- core/src/main/files/osiam/register-veraweb-into-osiam.sh
+++ core/src/main/files/osiam/register-veraweb-into-osiam.sh
@@ -1,9 +1,15 @@
 #!/bin/sh
 #-
-# Copyright (c) 2015, 2016
+# Copyright (c) 2015, 2016, 2018
 #	Thorsten Glaser <t.glaser@tarent.de>
 # Copyright (c) 2013
 #	mirabilos <m@mirbsd.org>
+#
+# Special thanks to Andreas Buschka!
+#
+# Additional credits to Erwin Brandstetter
+#	<https://stackoverflow.com/users/939860/erwin-brandstetter>
+# his explanation from: https://stackoverflow.com/a/18964090/2171120
 #
 # Provided that these terms and disclaimer and all copyright notices
 # are retained or reproduced in an accompanying document, permission
@@ -129,6 +135,52 @@ INSERT INTO scim_extension_field
 	    AND sef.extension_internal_id=se.internal_id
 	    AND se.urn='urn:scim:schemas:veraweb:1.5:Person'
     );
+INSERT INTO scim_extension_field
+    (internal_id, name, required, type, extension)
+    SELECT (SELECT MAX(internal_id) + 1 FROM scim_extension_field),
+	'tpersonid', false, 'INTEGER',
+	(SELECT internal_id FROM scim_extension WHERE urn='urn:scim:schemas:veraweb:1.5:Person')
+    WHERE NOT EXISTS (
+	SELECT sef.internal_id FROM scim_extension_field sef, scim_extension se
+	WHERE sef.name='tpersonid'
+	    AND sef.extension=se.internal_id
+	    AND se.urn='urn:scim:schemas:veraweb:1.5:Person'
+    );
+WITH raw_combined_table AS (
+	SELECT internal_id, name, is_required::TEXT, type,
+	    CASE WHEN osiam23 THEN extension_internal_id::TEXT ELSE NULL END AS extension_internal_id
+	FROM scim_extension_field AS is_required
+	CROSS JOIN (
+		SELECT EXISTS (
+			SELECT 1 FROM pg_catalog.pg_attribute
+			    WHERE attrelid='scim_extension_field'::regclass
+			    AND attname='extension_internal_id' AND NOT attisdropped AND attnum > 0
+		) AS osiam23
+	) extension_internal_id
+	UNION ALL
+	SELECT internal_id, name, required::TEXT AS is_required, type,
+	    CASE WHEN osiam25 THEN extension::TEXT ELSE NULL END AS extension_internal_id
+	FROM scim_extension_field AS required
+	CROSS JOIN (
+		SELECT EXISTS (
+			SELECT 1 FROM pg_catalog.pg_attribute
+			    WHERE attrelid='scim_extension_field'::regclass
+			    AND attname='extension' AND NOT attisdropped AND attnum > 0
+		) AS osiam25
+	) extension
+)
+-- SELECT rct.* FROM raw_combined_table rct WHERE rct.extension_internal_id IS NOT NULL;
+SELECT CASE WHEN EXISTS (
+	SELECT 1 FROM raw_combined_table rct
+	WHERE rct.extension_internal_id IS NOT NULL
+	    AND rct.name='tpersonid'
+	    AND rct.extension_internal_id=(
+		SELECT internal_id::TEXT FROM scim_extension
+		WHERE urn='urn:scim:schemas:veraweb:1.5:Person'
+	    )
+) THEN 'Everything OK, please ignore all "column ... does not exist" errors above!'
+ELSE ' *** ERROR *** SCIM schema database adjustment failed, please check manually!'
+END AS "database upgrade result";
 EOF
 
 print -u2 I: downloading auth token
