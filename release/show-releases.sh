5efeb6af7d939e84fb65d710088cd65359a1292b
mirabilos <t.glaser@tarent.de>
2018-05-11 17:54:37 +0200

    script to show full releases in Nexus

---
+++ release/show-releases.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env mksh
+set -e
+set -o pipefail
+
+wget -qO - 'https://repo-bn-01.lan.tarent.de/service/rest/beta/search?repository=maven-releases&maven.groupId=org.evolvis.veraweb&maven.artifactId=online-anmeldung' | \
+    jq -r '.items[].version' | \
+    sort --version-sort
