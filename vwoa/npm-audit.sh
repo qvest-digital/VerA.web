5d866ea3db7cb49b82024c0f2b427b5033ba3e67
mirabilos <t.glaser@tarent.de>
2018-05-11 20:22:51 +0200

    for highlighting

--- vwoa/npm-audit.sh
+++ vwoa/npm-audit.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 cd "$(dirname "$0")"/src/main/webroot-src || exit 1
 npm audit
-echo ⇒ errorlevel $?
+rv=$?
+if test x"$rv" = x"0"; then
+	echo "[INFO] npm audit ran with no problems"
+else
+	echo "[ERROR] npm audit returned errorlevel $?"
+fi
 exit 0
