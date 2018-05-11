874c32b757907b4a5298ef1a13bed2ac0ab0a645
mirabilos <t.glaser@tarent.de>
2018-05-11 20:46:32 +0200

    package up release/ during build

--- release/show-releases.sh
+++ release/show-releases.sh
@@ -2,6 +2,6 @@
 set -e
 set -o pipefail
 
-wget -qO - 'https://repo-bn-01.lan.tarent.de/service/rest/beta/search?repository=maven-releases&maven.groupId=org.evolvis.veraweb&maven.artifactId=online-anmeldung' | \
+wget -qO - 'https://repo-bn-01.lan.tarent.de/service/rest/beta/search?repository=maven-releases&maven.groupId=org.evolvis.veraweb&maven.artifactId=veraweb-parent' | \
     jq -r '.items[].version' | \
     sort --version-sort
