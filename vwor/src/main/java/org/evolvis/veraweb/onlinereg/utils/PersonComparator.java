a0777fae1fb1098ec811f9c3e2d7b59de33525a7
mirabilos <t.glaser@tarent.de>
2018-05-30 18:23:56 +0200

    whitespace

--- vwor/src/main/java/org/evolvis/veraweb/onlinereg/utils/PersonComparator.java
+++ vwor/src/main/java/org/evolvis/veraweb/onlinereg/utils/PersonComparator.java
@@ -5,9 +5,7 @@ import org.evolvis.veraweb.onlinereg.entities.Person;
 import java.util.Comparator;
 
 public class PersonComparator implements Comparator<Person> {
-
     public int compare(Person p1, Person p2) {
         return p1.getLastname_a_e1().compareTo(p2.getLastname_a_e1());
     }
-
 }
