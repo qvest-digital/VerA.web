61cfa4aced0eb1325ce40f554a60c49ac0f3cce4
Yorka Neumann <y.neumann@tarent.de>
2018-06-13 16:08:28 +0200

    implemented configurable viewable button to open browser tab

--- core/src/main/java/de/tarent/aa/veraweb/beans/ViewConfig.java
+++ core/src/main/java/de/tarent/aa/veraweb/beans/ViewConfig.java
@@ -75,15 +75,24 @@ import java.util.Properties;
 public class ViewConfig extends MapBean {
 
     public Boolean SHOW_INTERNAL_ID;
-    public Boolean SHOW_VERAWEB_ID = Boolean.TRUE;
+    public Boolean SHOW_VERAWEB_ID;
+    public Boolean SHOW_GLOBAL_OPENTAB;
 
+    public final static String SHOW_GLOBAL_OPENTAB_KEY = "veraweb.show.global.opentab.button";
     public final static String SHOW_INTERNAL_ID_KEY = "veraweb.show.person.internalId";
     public final static String SHOW_VERWEB_ID_KEY = "veraweb.show.person.verawebId";
 
+
     public ViewConfig(Properties properties) {
-        SHOW_INTERNAL_ID = Boolean.valueOf(properties.getProperty(SHOW_INTERNAL_ID_KEY));
-        if(properties.getProperty(SHOW_VERWEB_ID_KEY) != null) {
-            SHOW_VERAWEB_ID = Boolean.valueOf(properties.getProperty(SHOW_VERWEB_ID_KEY));
+        SHOW_INTERNAL_ID = booleanValueOf(properties.getProperty(SHOW_INTERNAL_ID_KEY), false);
+        SHOW_VERAWEB_ID = booleanValueOf(properties.getProperty(SHOW_VERWEB_ID_KEY), true);
+        SHOW_GLOBAL_OPENTAB = booleanValueOf(properties.getProperty(SHOW_GLOBAL_OPENTAB_KEY), false);
+    }
+
+    public Boolean booleanValueOf(String propertyString, Boolean defaultValue) {
+        if (propertyString != null || defaultValue == null) {
+            return Boolean.valueOf(propertyString);
         }
+        return defaultValue;
     }
 }
--- core/src/test/groovy/de/tarent/aa/veraweb/beans/ViewConfigTest.groovy
+++ core/src/test/groovy/de/tarent/aa/veraweb/beans/ViewConfigTest.groovy
@@ -91,6 +91,14 @@ class ViewConfigTest extends Specification {
         testObject.SHOW_VERAWEB_ID == Boolean.TRUE
     }
 
+    def 'if show global open tab property is not given the default will be used'() {
+        when: 'no properties are given at all'
+        ViewConfig testObject = new ViewConfig(properties)
+
+        then:
+        testObject.SHOW_GLOBAL_OPENTAB == Boolean.FALSE
+    }
+
     def 'if show veraweb id property is given this value will be used'() {
         given: 'property is given and is not equal to default'
         properties.put(ViewConfig.SHOW_VERWEB_ID_KEY, 'false')
@@ -111,5 +119,16 @@ class ViewConfigTest extends Specification {
 
         then:
         testObject.SHOW_VERAWEB_ID == Boolean.TRUE
+    }
+
+    def 'if show open tab property is given this value will be used'() {
+        given: 'property is given and is not equal to default'
+        properties.put(ViewConfig.SHOW_GLOBAL_OPENTAB_KEY, 'true')
+
+        when: 'no properties are given at all'
+        ViewConfig testObject = new ViewConfig(properties)
+
+        then:
+        testObject.SHOW_GLOBAL_OPENTAB == Boolean.TRUE
     }
 }
