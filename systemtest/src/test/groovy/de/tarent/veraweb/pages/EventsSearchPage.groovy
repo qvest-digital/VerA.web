c05e506ae9d095977f92d2e2ee205da44cc52d4c
Timo Kanera <t.kanera@tarent.de>
2018-07-09 15:51:52 +0200

    add ui test logic to navigate to event/guest list

---
+++ systemtest/src/test/groovy/de/tarent/veraweb/pages/EventsSearchPage.groovy
@@ -0,0 +1,23 @@
+package de.tarent.veraweb.pages
+
+import geb.Page
+import org.openqa.selenium.By
+
+class EventsSearchPage extends Page {
+
+    static at = {
+        pageTitle.text() == 'Veranstaltungssuche'
+    }
+
+    static content = {
+        pageTitle { $('h1') }
+        contentContainer { $('#content_container')}
+        searchFieldShortName { contentContainer.find(By.id('input.shortname'))}
+        searchButton { contentContainer.find(By.id('button.startSearch'))}
+    }
+
+    def searchEvent(String query) {
+        searchFieldShortName = query
+        searchButton.click()
+    }
+}
