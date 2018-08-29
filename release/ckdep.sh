e06f2fdd658d405d870627cc83d3295a65626d36
mirabilos <mirabilos@evolvis.org>
2018-08-29 12:33:26 +0000

    oops, one more compile

--- release/ckdep.sh
+++ release/ckdep.sh
@@ -69,7 +69,7 @@ if (( build_vwoa )); then
 		npm list --only prod --json true 2>/dev/null | jq -r \
 		    '.dependencies | to_entries[] | recurse(.value.dependencies | objects | to_entries[]) | [.key, .value.version] | map(gsub("(?<x>[^!#-&*-~ -�]+)"; "{\(.x | @base64)}")) | "npm::" + .[0] + " " + .[1] + " ok"'
 		node_modules/bower/bin/bower list | tee /dev/stderr | LC_ALL=C.UTF-8 sed --posix -n \
-		    '/^[ ─-╿]\{1,\}\([a-z0-9.-]\{1,\}\)#\([^ ]\{1,\}\)\( .*\)\{0,1\}$/s//bower::\1 \2 ok/p'
+		    '/^[ ─-╿]\{1,\}\([a-z0-9.-]\{1,\}\)#\([^ ]\{1,\}\)\( .*\)\{0,1\}$/s//bower::\1 \2 compile ok/p'
 	) 2>&1 >>ckdep-vwoa.tmp | sed 's!^![INFO] !' >&2
 	x=$(sort -u <ckdep-vwoa.tmp)
 	lastline=
