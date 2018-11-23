0a98079c85a76b7f72ac76f0f97254f212bbbda3
mirabilos <t.glaser@tarent.de>
2018-11-23 17:51:04 +0100

    mvn com.mycila:license-maven-plugin:3.0:format
    plus reformat

--- shared/src/main/java/org/evolvis/veraweb/common/RestPaths.java
+++ shared/src/main/java/org/evolvis/veraweb/common/RestPaths.java
@@ -1,10 +1,77 @@
 package org.evolvis.veraweb.common;
 
-public class RestPaths {
+/*-
+ * Veranstaltungsmanagement VerA.web (platform-independent
+ * webservice-based event management) is Copyright
+ *  © 2018 Атанас Александров (sirakov@gmail.com)
+ *  © 2014, 2015, 2016, 2017 Атанас Александров (a.alexandrov@tarent.de)
+ *  © 2013 Иванка Александрова (i.alexandrova@tarent.de)
+ *  © 2013 Patrick Apel (p.apel@tarent.de)
+ *  © 2016 Eugen Auschew (e.auschew@tarent.de)
+ *  © 2013 Andrei Boulgakov (a.boulgakov@tarent.de)
+ *  © 2013 Valentin But (v.but@tarent.de)
+ *  © 2016 Lukas Degener (l.degener@tarent.de)
+ *  © 2017 Axel Dirla (a.dirla@tarent.de)
+ *  © 2015 Julian Drawe (j.drawe@tarent.de)
+ *  © 2014 Dominik George (d.george@tarent.de)
+ *  © 2013 Sascha Girrulat (s.girrulat@tarent.de)
+ *  © 2008 David Goemans (d.goemans@tarent.de)
+ *  © 2018 Christian Gorski (c.gorski@tarent.de)
+ *  © 2015 Viktor Hamm (v.hamm@tarent.de)
+ *  © 2013 Katja Hapke (k.hapke@tarent.de)
+ *  © 2013 Hendrik Helwich (h.helwich@tarent.de)
+ *  © 2018 Thomas Hensel (t.hensel@tarent.de)
+ *  © 2018 Benedict Hoeger (b.hoeger@tarent.de)
+ *  © 2018 Titian Horvath (t.horvath@tarent.de)
+ *  © 2005, 2006, 2007, 2008 Christoph Jerolimov (jerolimov@gmx.de)
+ *  © 2018 Timo Kanera (t.kanera@tarent.de)
+ *  © 2008, 2009, 2010 Carsten Klein (c.klein@tarent.de)
+ *  © 2014 Martin Ley (m.ley@tarent.de)
+ *  © 2014, 2015 Max Marche (m.marche@tarent.de)
+ *  © 2007 Jan Meyer (jan@evolvis.org)
+ *  © 2013, 2014, 2015, 2016, 2017, 2018 mirabilos (t.glaser@tarent.de)
+ *  © 2016 Cristian Molina (c.molina@tarent.de)
+ *  © 2018 Yorka Neumann (y.neumann@tarent.de)
+ *  © 2017 Michael Nienhaus (m.nienhaus@tarent.de)
+ *  © 2013 Claudia Nuessle (c.nuessle@tarent.de)
+ *  © 2014, 2015 Jon Nuñez Alvarez (j.nunez-alvarez@tarent.de)
+ *  © 2016 Jens Oberender (j.oberender@tarent.de)
+ *  © 2016, 2017 Miluška Pech (m.pech@tarent.de)
+ *  © 2009 Martin Pelzer (m.pelzer@tarent.de)
+ *  © 2013 Marc Radel (m.radel@tarent.de)
+ *  © 2013 Sebastian Reimers (s.reimers@tarent.de)
+ *  © 2015 Charbel Saliba (c.saliba@tarent.de)
+ *  © 2008, 2009, 2010 Thomas Schmitz (t.schmitz@tarent.de)
+ *  © 2013 Volker Schmitz (v.schmitz@tarent.de)
+ *  © 2014 Sven Schumann (s.schumann@tarent.de)
+ *  © 2014 Sevilay Temiz (s.temiz@tarent.de)
+ *  © 2013 Kevin Viola Schmitz (k.schmitz@tarent.de)
+ *  © 2015 Stefan Walenda (s.walenda@tarent.de)
+ *  © 2015, 2016, 2017 Max Weierstall (m.weierstall@tarent.de)
+ *  © 2013 Rebecca Weinz (r.weinz@tarent.de)
+ *  © 2015, 2016 Stefan Weiz (s.weiz@tarent.de)
+ *  © 2015, 2016 Tim Zimmer (t.zimmer@tarent.de)
+ * and older code, Copyright © 2004–2008 ⮡ tarent GmbH and contributors.
+ * Licensor is tarent solutions GmbH, http://www.tarent.de/
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, see: http://www.gnu.org/licenses/
+ */
 
+public class RestPaths {
     /**
      * Category-Resource
-     * */
+     */
     public static final String REST_CATEGORY = "/category";
     public static final String REST_CATEGORY_GET_ID = "/{catname}/{uuid}";
     public static final String REST_CATEGORY_IDENTIFY = "/identify";
@@ -13,7 +80,7 @@ public class RestPaths {
 
     /**
      * Delegation-Resource
-     * */
+     */
     public static final String REST_DELEGATION = "/delegation";
     public static final String REST_DELEGATION_GET_OPTIONAL_FIELDS = "/fields/list/{eventId}/{guestId}";
     public static final String REST_DELEGATION_REMOVE_FIELDS = "/remove/fields";
@@ -22,13 +89,13 @@ public class RestPaths {
 
     /**
      * Email-Resource
-     * */
+     */
     public static final String REST_EMAIL = "/email";
     public static final String REST_EMAIL_CONFIRM = "/confirmation/send";
 
     /**
      * Event-Resource
-     * */
+     */
     public static final String REST_EVENT = "/event";
     public static final String REST_EVENT_GET_USER_EVENTS = "/userevents/{username}";
     public static final String REST_EVENT_CHECK_REGISTERED = "/userevents/existing/{username}";
@@ -43,44 +110,44 @@ public class RestPaths {
 
     /**
      * Export-Resource
-     * */
+     */
     public static final String REST_EXPORT = "/export";
     public static final String REST_EXPORT_GET_GUESTLIST = "/guestList/{eventId}";
 
     /**
      * FileUpload-Resource
-     * */
+     */
     public static final String REST_FILEUPLOAD = "fileupload";
     public static final String REST_FILEUPLOAD_SAVE = "/save";
     public static final String REST_FILEUPLOAD_DOWNLOAD = "/download/{imgUUID}";
 
     /**
      * ForgotLogin-Resource
-     * */
+     */
     public static final String REST_FORGOTLOGIN = "/forgotLogin";
     public static final String REST_FORGOTLOGIN_RESEND = "/resend/login";
 
     /**
      * ForgotPassword-Resource
-     * */
+     */
     public static final String REST_FORGOTPASSWORD = "/forgotPassword";
     public static final String REST_FORGOTPASSWORD_RESET_LINK = "/request/reset-password-link";
 
     /**
      * FreeVisitors-Resource
-     * */
+     */
     public static final String REST_FREEVISITORS = "/freevisitors";
     public static final String REST_FREEVISITORS_GET_GUEST = "/noLoginRequired/{noLoginRequiredUUID}";
 
     /**
      * Function-Resource
-     * */
+     */
     public static final String REST_FUNCTION = "/function";
     public static final String REST_FUNCTION_GET_FUNCTION = "/fields/list/function";
 
     /**
      * Guest-Resource
-     * */
+     */
     public static final String REST_GUEST = "/guest";
     public static final String REST_GUEST_GET_GUEST = "/{eventId}/{userId}";
     public static final String REST_GUEST_GET_GUEST_ID = "/concrete/{eventId}/{userId}";
@@ -103,23 +170,23 @@ public class RestPaths {
 
     /**
      * Health-Resource
-     * */
+     */
     public static final String REST_HEALTH_AVAILABLE = "/available";
 
     /**
      * Imprint-Resource
-     * */
+     */
     public static final String REST_IMPRINT = "/imprint";
     public static final String REST_IMPRINT_LANGUAGE = "/{language}";
 
     /**
      * Info-Resource
-     * */
+     */
     public static final String REST_INFO = "/info";
 
     /**
      * LINKUUID-Resource
-     * */
+     */
     public static final String REST_LINK = "/links";
     public static final String REST_LINK_UUID = "/{uuid}";
     public static final String REST_LINK_DELETE = "/delete";
@@ -127,7 +194,7 @@ public class RestPaths {
 
     /**
      * Mailing-Resource
-     * */
+     */
     public static final String REST_MAILING = "/mailing";
 
     /**
@@ -146,13 +213,13 @@ public class RestPaths {
 
     /**
      * OptionalField-Resource
-     * */
+     */
     public static final String REST_OPTIONALFIELDS = "/optional/fields";
     public static final String REST_OPTIONALFIELDS_GET_ALL = "/list/{eventId}";
 
     /**
      * OptionalFieldTypeContent-Resource
-     * */
+     */
     public static final String REST_OPTIONALFIELDS_TYPECONTENT = "/typecontent";
     public static final String REST_OPTIONALFIELDS_TYPECONTENT_ID = "/{optionalFieldId}";
 
@@ -168,7 +235,7 @@ public class RestPaths {
 
     /**
      * PdfTemplate-Resource
-     * */
+     */
     public static final String REST_PDFTEMPLATE = "/pdftemplate";
     public static final String REST_PDFTEMPLATE_EDIT = "/edit";
     public static final String REST_PDFTEMPLATE_DELETE = "/delete";
@@ -195,7 +262,7 @@ public class RestPaths {
     public static final String REST_PERSON_USERDATA = "/userdata/{username}";
     public static final String REST_PERSON_USERDATA_LITE = "/userdata/lite/{username}";
     public static final String REST_PERSON_UPDATE_ORGUNIT = "/update/orgunit";
-    public static final String REST_PERSON_GET_USER= "/list/{personId}";
+    public static final String REST_PERSON_GET_USER = "/list/{personId}";
 
     /**
      * SalutationAlternative-Resource
