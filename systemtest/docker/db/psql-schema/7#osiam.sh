3317ef6df66459df6485b0f9611d3edfc83a86d4
mirabilos <t.glaser@tarent.de>
2019-05-29 18:26:15 +0200

    update systemtest/ accordingly

--- systemtest/docker/db/psql-schema/7#osiam.sh
+++
@@ -1,5 +0,0 @@
-if test x"$(psql -U veraweb -tAc "SELECT 1 FROM pg_database WHERE datname='ong'")" != x"1"; then
-  echo "CREATE USER ong WITH PASSWORD 'b4s3dg0d';" | psql -U veraweb
-  echo "CREATE DATABASE ong;" | psql -U veraweb
-  echo "GRANT ALL PRIVILEGES ON DATABASE ong TO ong;" | psql -U veraweb
-fi
