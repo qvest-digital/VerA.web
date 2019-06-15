7a8e12235cbe65accaffec68c73ac00291716154
mirabilos <t.glaser@tarent.de>
2019-06-15 05:10:29 +0200

    tweak a little and let IntelliJ do its thing

--- shared/src/main/java/org/evolvis/veraweb/util/DelegationPasswordGenerator.java
+++ shared/src/main/java/org/evolvis/veraweb/util/DelegationPasswordGenerator.java
@@ -69,27 +69,23 @@ package org.evolvis.veraweb.util;
  * with this program; if not, see: http://www.gnu.org/licenses/
  */
 
+import lombok.extern.log4j.Log4j2;
+
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
+@Log4j2
 public class DelegationPasswordGenerator {
+    private static final SimpleDateFormat DATE_FORMAT = new SimpleDateFormat("yyyy-MM-dd");
 
-    public static final SimpleDateFormat DATE_FORMAT = new SimpleDateFormat("yyyy-MM-dd");
-
-    public String generatePassword(String eventName, Date eventBegin, String companyName) {
-        return generateDelegationPassword(eventName, eventBegin, companyName);
+    public String generatePassword(final String eventName, final Date eventBegin, final String companyName) {
+        logger.trace("generating password for {}", eventName);
+        return extractFirstThreeChars(eventName) +
+          extractFirstThreeChars(companyName) +
+          DATE_FORMAT.format(eventBegin);
     }
 
-    private static String generateDelegationPassword(final String shortName, final Date begin, String companyName) {
-        final StringBuilder passwordBuilder = new StringBuilder();
-        final SimpleDateFormat dateFormat = DATE_FORMAT;
-        passwordBuilder.append(extractFirstXChars(shortName, 3));
-        passwordBuilder.append(extractFirstXChars(companyName, 3));
-        passwordBuilder.append(dateFormat.format(begin));
-        return passwordBuilder.toString();
-    }
-
-    private static String extractFirstXChars(String value, int x) {
-        return value.substring(0, Math.min(value.length(), x));
+    private static String extractFirstThreeChars(String value) {
+        return value.substring(0, Math.min(value.length(), 3));
     }
 }
