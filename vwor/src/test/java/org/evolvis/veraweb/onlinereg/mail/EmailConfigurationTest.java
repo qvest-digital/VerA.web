4aab50d0ded0aa5a48bdbbea0fe6cfb9cbf35924
mirabilos <t.glaser@tarent.de>
2019-06-21 22:23:44 +0200

    adjust for more OSIAM removal

--- vwor/src/test/java/org/evolvis/veraweb/onlinereg/mail/EmailConfigurationTest.java
+++ vwor/src/test/java/org/evolvis/veraweb/onlinereg/mail/EmailConfigurationTest.java
@@ -77,8 +77,6 @@ import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
 
 import static org.mockito.Matchers.any;
-import static org.mockito.Mockito.atLeast;
-import static org.mockito.Mockito.atMost;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
@@ -92,20 +90,19 @@ public class EmailConfigurationTest {
     VworPropertiesReader propertiesReader;
 
     @Before
-    public void setUp() throws Exception {
+    public void setUp() {
         when(propertiesReader.getProperty(any(String.class))).thenReturn("42");
     }
 
     @Test
-    public void testReadProperties() throws Exception {
+    public void testReadProperties() {
         new EmailConfiguration(propertiesReader);
         // THEN
-        verify(propertiesReader, atLeast(8)).getProperty(any(String.class));
-        verify(propertiesReader, atMost(13)).getProperty(any(String.class));
+        verify(propertiesReader, times(6)).getProperty(any(String.class));
     }
 
     @Test
-    public void testReadPropertiesTheSecond() throws Exception {
+    public void testReadPropertiesTheSecond() {
         // GIVEN
         when(propertiesReader.getProperty("mail.smtp.port")).thenReturn("25");
 
@@ -113,6 +110,6 @@ public class EmailConfigurationTest {
         new EmailConfiguration(propertiesReader);
 
         // THEN
-        verify(propertiesReader, times(13)).getProperty(any(String.class));
+        verify(propertiesReader, times(6)).getProperty(any(String.class));
     }
 }
