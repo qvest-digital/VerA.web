145f73cf30c87fa176ece9d1c398b9df4f19ea66
mirabilos <t.glaser@tarent.de>
2019-06-21 21:43:44 +0200

    g/c all vwor endpoints the core does NOT use

--- shared/src/main/java/org/evolvis/veraweb/common/RestPaths.java
+++ shared/src/main/java/org/evolvis/veraweb/common/RestPaths.java
@@ -105,6 +105,12 @@ public class RestPaths {
     public static final String REST_MAILTEMPLATE = "/mailtemplate";
 
     /**
+     * OptionalField-Resource
+     */
+    public static final String REST_OPTIONALFIELDS = "/optional/fields";
+    public static final String REST_OPTIONALFIELDS_GET_ALL = "/list/{eventId}";
+
+    /**
      * PdfTemplate-Resource
      */
     public static final String REST_PDFTEMPLATE = "/pdftemplate";
@@ -114,12 +120,6 @@ public class RestPaths {
     public static final String REST_PDFTEMPLATE_EXPORT = "/export";
 
     /**
-     * SalutationResource
-     */
-    public static final String REST_SALUTATION = "/salutation";
-    public static final String REST_SALUTATION_GET_ALL = "/getallsalutations";
-
-    /**
      * SalutationAlternative-Resource
      */
     public static final String REST_SALUTATION_ALTERNATIVE = "/salutation/alternative";
@@ -127,135 +127,4 @@ public class RestPaths {
     public static final String REST_SALUTATION_ALTERNATIVE_UNUSED = "/unused/{pdftemplateId}";
     public static final String REST_SALUTATION_ALTERNATIVE_DELETE = "delete/{salutationId}";
     public static final String REST_SALUTATION_ALTERNATIVE_SAVE = "/save/{pdftemplateId}/";
-
-    /**
-     * Category-Resource
-     */
-    public static final String REST_CATEGORY = "/category";
-    public static final String REST_CATEGORY_GET_ID = "/{catname}/{uuid}";
-    public static final String REST_CATEGORY_IDENTIFY = "/identify";
-    public static final String REST_CATEGORY_GET_NAME = "/catname/{uuid}/{personId}";
-    public static final String REST_CATEGORY_GET_PERSON_DATA = "person/data";
-
-    /**
-     * Delegation-Resource
-     */
-    public static final String REST_DELEGATION = "/delegation";
-    public static final String REST_DELEGATION_GET_OPTIONAL_FIELDS = "/fields/list/{eventId}/{guestId}";
-    public static final String REST_DELEGATION_REMOVE_FIELDS = "/remove/fields";
-    public static final String REST_DELEGATION_GET_LABEL = "/field/{eventId}";
-    public static final String REST_DELEGATION_SAVE_FIELD = "/field/save";
-
-    /**
-     * Event-Resource
-     */
-    public static final String REST_EVENT = "/event";
-    public static final String REST_EVENT_GET_USER_EVENTS = "/userevents/{username}";
-    public static final String REST_EVENT_CHECK_REGISTERED = "/userevents/existing/{username}";
-    public static final String REST_EVENT_GET_PERSON_ID = "/userid/{username}";
-    public static final String REST_EVENT_GET_EVENT = "/{eventId}";
-    public static final String REST_EVENT_EXIST = "/exist/{uuid}";
-    public static final String REST_EVENT_GET_EVENT_ID = "/require/{uuid}";
-    public static final String REST_EVENT_CHECK_OPEN = "/isopen/{eventId}";
-    public static final String REST_EVENT_GUESLIST_STATUS = "/guestlist/status/{eventId}";
-    public static final String REST_EVENT_RESERVELIST_STATUS = "/reservelist/status/{eventId}";
-    public static final String REST_EVENT_GET_EVENT_BY_UUID = "/uuid/{uuid}";
-
-    /**
-     * ForgotLogin-Resource
-     */
-    public static final String REST_FORGOTLOGIN = "/forgotLogin";
-    public static final String REST_FORGOTLOGIN_RESEND = "/resend/login";
-
-    /**
-     * ForgotPassword-Resource
-     */
-    public static final String REST_FORGOTPASSWORD = "/forgotPassword";
-    public static final String REST_FORGOTPASSWORD_RESET_LINK = "/request/reset-password-link";
-
-    /**
-     * FreeVisitors-Resource
-     */
-    public static final String REST_FREEVISITORS = "/freevisitors";
-    public static final String REST_FREEVISITORS_GET_GUEST = "/noLoginRequired/{noLoginRequiredUUID}";
-
-    /**
-     * Function-Resource
-     */
-    public static final String REST_FUNCTION = "/function";
-    public static final String REST_FUNCTION_GET_FUNCTION = "/fields/list/function";
-
-    /**
-     * Guest-Resource
-     */
-    public static final String REST_GUEST = "/guest";
-    public static final String REST_GUEST_GET_GUEST = "/{eventId}/{userId}";
-    public static final String REST_GUEST_GET_GUEST_ID = "/concrete/{eventId}/{userId}";
-    public static final String REST_GUEST_GET_IMAGE_UUID = "/image/{delegationUUID}/{personId}";
-    public static final String REST_GUEST_GET_GUEST_BY_EVENT_USER_ID = "/{eventId}/{userId}";
-    public static final String REST_GUEST_UPDATE_ENTITY = "/update/entity";
-    public static final String REST_GUEST_UPDATE_GUEST = "/update/{eventId}/{userId}";
-    public static final String REST_GUEST_UPDATE_NOLOGIN = "/update/nologin/{noLoginRequiredUUID}";
-    public static final String REST_GUEST_BY_UUID = "/{uuid}";
-    public static final String REST_GUEST_FIND_BY_DELEGATION = "/delegation/{uuid}/{userId}";
-    public static final String REST_GUEST_FIND_BY_DELEGATION_UUID = "/delegation/{uuid}";
-    public static final String REST_GUEST_EXIST_UUID = "/exist/{uuid}";
-    public static final String REST_GUEST_REGISTERED = "/registered/delegation/{username}/{delegation}";
-    public static final String REST_GUEST_REGISTERED_BY_USERNAME_EVENT = "/registered/{username}/{eventId}";
-    public static final String REST_GUEST_REGISTERED_ACCEPT = "/registered/accept/{username}/{eventId}";
-    public static final String REST_GUEST_REGISTERED_NOLOGIN = "/registered/nologin/{noLoginRequiredUUID}/{eventId}";
-    public static final String REST_GUEST_ADD_TO_EVENT = "/{uuid}/register";
-    public static final String REST_GUEST_REGISTER = "/register";
-    public static final String REST_GUEST_ISRESERVE = "/isreserve/{eventId}/{username}";
-
-    /**
-     * LINKUUID-Resource
-     */
-    public static final String REST_LINK = "/links";
-    public static final String REST_LINK_UUID = "/{uuid}";
-    public static final String REST_LINK_DELETE = "/delete";
-    public static final String REST_LINK_GET_BY_ID = "/byPersonId/{personId}";
-
-    /**
-     * MediaRepresentativeActivation-Resource
-     */
-    public static final String REST_MEDIA = "/press/activation";
-    public static final String REST_MEDIA_CREATE = "/create";
-    public static final String REST_MEDIA_EXISTS = "/exists/{encodedAddress}/{eventId}";
-    public static final String REST_MEDIA_EXISTS_TOKEN = "/exists/{mediaRepresentativeActivationToken}";
-    public static final String REST_MEDIA_UPDATE = "/update";
-
-    /**
-     * OptionalField-Resource
-     */
-    public static final String REST_OPTIONALFIELDS = "/optional/fields";
-    public static final String REST_OPTIONALFIELDS_GET_ALL = "/list/{eventId}";
-
-    /**
-     * OptionalFieldTypeContent-Resource
-     */
-    public static final String REST_OPTIONALFIELDS_TYPECONTENT = "/typecontent";
-    public static final String REST_OPTIONALFIELDS_TYPECONTENT_ID = "/{optionalFieldId}";
-
-    /**
-     * PersonCategory-Resource
-     */
-    public static final String REST_PERSONCATEGORY = "/personcategory";
-    public static final String REST_PERSONCATEGORY_ADD = "/add";
-
-    /**
-     * Person-Resource
-     */
-    public static final String REST_PERSON = "/person";
-    public static final String REST_PERSON_DELEGATE = "/delegate";
-    public static final String REST_PERSON_DELEGATE_UPDATE = "/delegate/update";
-    public static final String REST_PERSON_PRESS = "/press";
-    public static final String REST_PERSON_USERCOREDATA_UPDATE = "/usercoredata/update/";
-    public static final String REST_PERSON_USERINFO = "/userinfo/{username}";
-    public static final String REST_PERSON_UUID = "/{uuid}";
-    public static final String REST_PERSON_COMPANY = "/company/{uuid}";
-    public static final String REST_PERSON_USERDATA = "/userdata/{username}";
-    public static final String REST_PERSON_USERDATA_LITE = "/userdata/lite/{username}";
-    public static final String REST_PERSON_UPDATE_ORGUNIT = "/update/orgunit";
-    public static final String REST_PERSON_GET_USER = "/list/{personId}";
 }
