91c13de235f26b3e30e0906419b1ab46015a90e4
Titian Horvath <t.horvath@tarent.de>
2019-07-09 10:45:35 +0200

    fix not sending mails with capital letters

--- vwor/src/main/java/org/evolvis/veraweb/onlinereg/rest/MailingResource.java
+++ vwor/src/main/java/org/evolvis/veraweb/onlinereg/rest/MailingResource.java
@@ -169,7 +169,7 @@ public class MailingResource extends FormDataResource {
 
         for (final PersonMailinglist recipient : recipients) {
             final String from = getFrom(recipient);
-            if (pattern.matcher(recipient.getAddress()).matches()) {
+            if (pattern.matcher(recipient.getAddress().toLowerCase()).matches()) {
                 try {
                     final MailDispatchMonitor monitor = mailDispatcher.sendEmailWithAttachments(from, recipient.getAddress(),
                       subject, substitutePlaceholders(text, recipient.getPerson()), files);
