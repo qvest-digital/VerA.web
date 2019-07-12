edddbbc13634c7233e2072a02f79bcafe9d05cd7
mirabilos <t.glaser@tarent.de>
2019-07-12 21:11:38 +0200

    Revert "add debugging for the subtask/toplevel loggging", passed
    This reverts commit 7ec7b050a6a6eb120ac3ef48e027f9afa2e64991.

--- core/src/main/java/de/tarent/aa/veraweb/worker/PersonDetailWorker.java
+++ core/src/main/java/de/tarent/aa/veraweb/worker/PersonDetailWorker.java
@@ -529,7 +529,6 @@ public class PersonDetailWorker implements PersonConstants {
         if (saveperson != null && saveperson) {
             octopusContext.setStatus("saveperson");
         }
-        throw new RuntimeException("miau");
     }
 
     public static final String INPUT_verify[] = { "person-nodupcheck" };
