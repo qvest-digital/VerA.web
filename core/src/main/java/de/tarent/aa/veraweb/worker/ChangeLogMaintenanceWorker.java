f9749537f6f620bb8b232d1275651084872abcb4
mirabilos <t.glaser@tarent.de>
2019-07-15 18:47:29 +0200

    clean up a little and reduce logging in non-error case

--- core/src/main/java/de/tarent/aa/veraweb/worker/ChangeLogMaintenanceWorker.java
+++ core/src/main/java/de/tarent/aa/veraweb/worker/ChangeLogMaintenanceWorker.java
@@ -102,11 +102,8 @@ import de.tarent.octopus.beans.veraweb.DatabaseVeraWeb;
 import de.tarent.octopus.server.OctopusContext;
 import lombok.extern.log4j.Log4j2;
 
-import java.io.IOException;
 import java.sql.Date;
-import java.sql.SQLException;
 import java.util.Calendar;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 
@@ -114,8 +111,6 @@ import java.util.Map;
  * Worker that runs constantly purges old entries from the changelog.
  *
  * @author cklein
- * @version $Revision: 1.1 $
- * @see ChangeLogMaintenanceWorker
  */
 @Log4j2
 public class ChangeLogMaintenanceWorker implements Runnable {
@@ -141,9 +136,8 @@ public class ChangeLogMaintenanceWorker implements Runnable {
     private Duration getConfiguration() {
         Duration result = Duration.fromString("P1Y");
 
-        List list = (List) cntx.contentAsObject("allConfig");
-        for (Iterator it = list.iterator(); it.hasNext(); ) {
-            Map entry = (Map) it.next();
+        List<Map> list = (List<Map>) cntx.contentAsObject("allConfig");
+        for (final Map entry : list) {
             if ("changeLogRetentionPolicy".compareTo((String) entry.get("key")) == 0) {
                 result = Duration.fromString((String) entry.get("value"));
                 break;
@@ -156,34 +150,32 @@ public class ChangeLogMaintenanceWorker implements Runnable {
     /**
      * Starts the background maintenance service.
      *
-     * @param cntx Octopus-Context
+     * @param ctx Octopus-Context
      */
-    public void load(OctopusContext cntx) {
-        this.logger.info("ChangeLogMaintenanceWorker wird im Hintergrund gestartet.");
-        this.cntx = cntx;
+    public void load(final OctopusContext ctx) {
+        logger.debug("Worker wird im Hintergrund gestartet");
+        cntx = ctx;
 
-        this.retentionPolicy = this.getConfiguration();
-        if (this.retentionPolicy.toString().equals("P0")) {
+        retentionPolicy = this.getConfiguration();
+        if (retentionPolicy.toString().equals("P0")) {
             // log invalid setting and use 1yr. default
-            this.logger
-              .warn("changeLogRetentionPolicy Konfigurationseinstellung ist fehlerhaft. Die Einstellung muss einer " +
-                "g\u00fcltigen " +
-                "Zeitdauerangabe im Format P[0-9]+Y[0-9]+M[0-9]+D entsprechen. Stattdessen wird die Vorgabe P1Y (1 " +
-                "Jahr) verwendet.");
-            this.retentionPolicy.years = 1;
+            logger.warn("Die Konfigurationseinstellung changeLogRetentionPolicy ist fehlerhaft. " +
+              "Die Einstellung muß einer gültigen Zeitdauerangabe im Format P[0-9]+Y[0-9]+M[0-9]+D " +
+              "entsprechen. Stattdessen wird die Vorgabe P1Y (1 Jahr) verwendet.");
+            retentionPolicy.years = 1;
         }
 
         // this setting is unconfigurable, by default, the maintenance routine
         // will be run once every two hours
-        this.waitMillis = 7200000;
+        waitMillis = 7200000;
 
         // Server status
         if (!keeprunning) {
-            this.keeprunning = true;
-            this.thread = new Thread(this);
-            this.thread.start();
+            keeprunning = true;
+            thread = new Thread(this);
+            thread.start();
         } else {
-            this.unload();
+            unload();
         }
     }
 
@@ -196,8 +188,8 @@ public class ChangeLogMaintenanceWorker implements Runnable {
      * Stops the background service.
      */
     public void unload() {
-        this.logger.info("ChangeLogMaintenanceWorker wird gestoppt.");
-        this.keeprunning = false;
+        logger.debug("Worker wird gestoppt");
+        keeprunning = false;
     }
 
     /**
@@ -205,33 +197,31 @@ public class ChangeLogMaintenanceWorker implements Runnable {
      */
     public void run() {
         try {
-            this.isworking = true;
-            while (this.keeprunning) {
+            isworking = true;
+            while (keeprunning) {
                 try {
-                    this.purgeChangeLog();
+                    purgeChangeLog();
                 } catch (Exception e) {
-                    this.logger.error("ChangeLogMaintenanceWorker: allgemeiner Fehler w\u00e4hrend der Wartungsarbeit.", e);
+                    logger.error("allgemeiner Fehler während der Wartungsarbeit", e);
                 }
                 try {
-                    Thread.sleep(this.waitMillis < 1000 ? 1000 : this.waitMillis);
+                    Thread.sleep(waitMillis < 1000 ? 1000 : waitMillis);
                 } catch (InterruptedException e) {
                     // just catch
                 }
             }
         } finally {
-            this.isworking = false;
-            this.thread = null;
+            isworking = false;
+            thread = null;
         }
     }
 
     /**
      * Purges the change log from old information.
      *
-     * @throws IOException   ioException
      * @throws BeanException beanException
-     * @throws SQLException  sqlException
      */
-    public void purgeChangeLog() throws SQLException, BeanException, IOException {
+    private void purgeChangeLog() throws BeanException {
         Calendar c = Calendar.getInstance();
         c.setTimeInMillis(System.currentTimeMillis());
         c.add(Calendar.YEAR, -1 * this.retentionPolicy.years);
@@ -247,7 +237,7 @@ public class ChangeLogMaintenanceWorker implements Runnable {
             transactionContext.commit();
         } catch (Throwable e) {
             transactionContext.rollBack();
-            logger.trace("Das Changelog konnte nicht gel\u00f6scht werden.", e);
+            logger.trace("Das Changelog konnte nicht gelöscht werden", e);
         }
     }
 }
