7beba7b6d60c827d3273e5fcef1fd4d10b18d248
mirabilos <t.glaser@tarent.de>
2019-11-22 21:17:12 +0100

    make the test “work”

--- vwor/src/test/groovy/org/evolvis/veraweb/onlinereg/rest/FileUploadResourceTest.groovy
+++ vwor/src/test/groovy/org/evolvis/veraweb/onlinereg/rest/FileUploadResourceTest.groovy
@@ -94,20 +94,18 @@ package org.evolvis.veraweb.onlinereg.rest
 
 import org.apache.commons.io.FileUtils
 import org.evolvis.veraweb.onlinereg.utils.VworPropertiesReader
-import spock.lang.Ignore
 import spock.lang.Specification
 
 /**
  * @author Atanas Alexandrov, tarent solutions GmbH
  */
 class FileUploadResourceTest extends Specification {
-
-    def propertiesReaderMock = Mock(VworPropertiesReader)
     def imageFile = new File("src/test/resources/imageData.txt")
     def IMAGE_DATA_STRING = new FileUtils().readFileToString(imageFile)
 
     void testSaveImageIntoDataSystem() {
         setup:
+            def propertiesReaderMock = Mock(VworPropertiesReader)
             def resource = new FileUploadResource(vworPropertiesReader: propertiesReaderMock)
 
         when:
@@ -117,9 +115,9 @@ class FileUploadResourceTest extends Specification {
             1 * propertiesReaderMock.getProperty("filesLocation")
     }
 
-    @Ignore('java.lang.OutOfMemoryError: Java heap space')
     void testGetImageByUUID() {
         setup:
+            def propertiesReaderMock = Mock(VworPropertiesReader)
             def resource = new FileUploadResource(vworPropertiesReader: propertiesReaderMock)
             resource.saveImageIntoDataSystem(IMAGE_DATA_STRING, "jpg", "19c3fdaf-8599-466c-b77e-435720528dcf")
 
@@ -127,6 +125,7 @@ class FileUploadResourceTest extends Specification {
             def image = resource.getImageByUUID("19c3fdaf-8599-466c-b77e-435720528dcf")
 
         then:
+            1 * propertiesReaderMock.getProperty("filesLocation")
             assert image == "data:image/jpg;base64," + IMAGE_DATA_STRING
     }
 }
